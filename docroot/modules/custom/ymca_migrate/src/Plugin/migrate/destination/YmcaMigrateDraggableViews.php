b3bc376422093bd05c74a5226d60951286dff2f2
Dmytro Danylevskyi <dmytro@danylevskyi.com>
2015-11-02 18:20:53 +0200

    Add Draggableviews migration

---
+++ docroot/modules/custom/ymca_migrate/src/Plugin/migrate/destination/YmcaMigrateDraggableViews.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * @file
+ * Contains destination plugin for Draggable Views database table.
+ *
+ * @TODO: Add DB connections as dependency injection.
+ */
+
+namespace Drupal\ymca_migrate\Plugin\migrate\destination;
+
+use Drupal\migrate\Entity\MigrationInterface;
+use Drupal\migrate\Plugin\migrate\destination\DestinationBase;
+use Drupal\migrate\Row;
+
+
+/**
+ * @MigrateDestination(
+ *   id = "ymca_migrate_draggableviews"
+ * )
+ */
+class YmcaMigrateDraggableViews extends DestinationBase {
+
+  /**
+   * Constructs an entity destination plugin.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param MigrationInterface $migration
+   *   The migration.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $migration);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function import(Row $row, array $old_destination_id_values = []) {
+    $record = [
+      'view_name' => $row->getDestinationProperty('view_name'),
+      'view_display' => $row->getDestinationProperty('view_display'),
+      'args' => $row->getDestinationProperty('args'),
+      'entity_id' => $row->getDestinationProperty('entity_id'),
+      'weight' => $row->getDestinationProperty('weight'),
+      'parent' => $row->getDestinationProperty('parent'),
+    ];
+    $result = db_insert('draggableviews_structure')->fields($record)->execute();
+    return array($result);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIds() {
+    return [
+      'dvid' => [
+        'type' => 'integer',
+      ],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields(MigrationInterface $migration = NULL) {
+    return [
+      'dvid' => $this->t('The primarty identifier'),
+      'view_name' => $this->t('The view name.'),
+      'view_display' => $this->t('The view display.'),
+      'args' => $this->t('The arguments.'),
+      'entity_id' => $this->t('The entity id.'),
+      'weight' => $this->t('The order weight.'),
+      'parent' => $this->t('The parent entity id.'),
+    ];
+  }
+
+}
---
+++ docroot/modules/custom/ymca_migrate/src/Plugin/migrate/source/YmcaMigrateDraggableViews.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\ymca_migrate\Plugin\migrate\source\YmcaMigrateDraggableViews.
+ */
+
+namespace Drupal\ymca_migrate\Plugin\migrate\source;
+
+use Drupal\migrate\Entity\Migration;
+use Drupal\migrate\Plugin\migrate\source\SqlBase;
+use Drupal\migrate\Row;
+
+/**
+ * Source plugin for Draggable Views data.
+ *
+ * @MigrateSource(
+ *   id = "ymca_migrate_draggableviews"
+ * )
+ */
+class YmcaMigrateDraggableviews extends SqlBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    $query = $this->select('legacy__node_article', 'b')
+      ->fields('b', ['id']);
+    return $query;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    $fields = [
+      'id' => $this->t('Article id'),
+      'view_name' => $this->t('The view name'),
+      'view_display' => $this->t('The view display'),
+      'args' => $this->t('The arguments'),
+      'entity_id' => $this->t('The entity id'),
+      'weight' => $this->t('The weight'),
+      'parent' => $this->t('The parent id'),
+    ];
+
+    return $fields;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+    // @TODO: set parent id here...
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIds() {
+    return [
+      'id' => [
+        'type' => 'integer',
+        'alias' => 'b',
+      ],
+    ];
+  }
+
+}
