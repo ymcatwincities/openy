24062a9b9ccf00ae5e2c80ac3c56d1cf45fce010
Dmytro Danylevskyi <dmytro@danylevskyi.com>
2015-11-06 13:05:18 +0200

    Add dragging, finaly

--- docroot/modules/custom/draggableviews/src/Plugin/views/field/DraggableViewsField.php
+++ docroot/modules/custom/draggableviews/src/Plugin/views/field/DraggableViewsField.php
@@ -48,14 +48,10 @@ class DraggableViewsField extends BulkForm {
    * {@inheritdoc}
    */
   public function viewsForm(&$form, FormStateInterface $form_state) {
-    // @todo d7 has $this->field_alias. How to make in d8?
-    $field_alias = 'nid';
-
     $form[$this->options['id']] = [
       '#tree' => TRUE,
     ];
 
-    $range = count($this->view->result);
     $draggableviews = new DraggableViews($this->view);
 
     foreach ($this->view->result as $row_index => $row) {
@@ -63,18 +59,10 @@ class DraggableViewsField extends BulkForm {
         '#tree' => TRUE,
       );
 
-      // Weight field select.
-      $form[$this->options['id']][$row_index]['weight'] = array(
-        '#type' => 'select',
-        '#options' => range(-$range, $range),
-        '#attributes' => array('class' => array('draggableviews-weight')),
-        '#default_value' => $row_index + $range,
-      );
-
       // Item to keep id of the entity.
       $form[$this->options['id']][$row_index]['id'] = array(
         '#type' => 'hidden',
-        '#value' => $row->$field_alias,
+        '#value' => $row->{$this->definition['entity field']},
         '#attributes' => array('class' => 'draggableviews-id'),
       );
 
@@ -84,23 +72,17 @@ class DraggableViewsField extends BulkForm {
         '#default_value' => $draggableviews->getParent($row_index),
         '#attributes' => array('class' => 'draggableviews-parent'),
       );
-
-      // Add depth.
-      $form[$this->options['id']][$row_index]['depth'] = array(
-        '#type' => 'hidden',
-        '#default_value' => $draggableviews->getDepth($row_index),
-        '#attributes' => array('class' => 'draggableviews-depth'),
-      );
     }
 
     $options = [
       'table_id' => $draggableviews->getHtmlId(),
-      'action' => 'depth',
-      'relationship' => 'group',
-      'group' => 'draggableviews-depth',
+      'action' => 'match',
+      'relationship' => 'parent',
+      'group' => 'draggableviews-parent',
+      'subgroup' => 'draggableviews-parent',
+      'source' => 'draggableviews-id'
     ];
     drupal_attach_tabledrag($form, $options);
-
   }
 
 }
