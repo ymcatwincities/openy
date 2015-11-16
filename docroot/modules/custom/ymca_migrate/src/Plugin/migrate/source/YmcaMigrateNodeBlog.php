af183b9a409023d4171237d5de098d01e36b58f1
Dmytro Danylevskyi <dmytro@danylevskyi.com>
2015-11-16 10:44:19 +0200

    Add source plugin for blog migration

---
+++ docroot/modules/custom/ymca_migrate/src/Plugin/migrate/source/YmcaMigrateNodeBlog.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\ymca_migrate\Plugin\migrate\source\YmcaMigrateNodeArticle.
+ */
+
+namespace Drupal\ymca_migrate\Plugin\migrate\source;
+
+use Drupal\migrate\Plugin\migrate\source\SqlBase;
+use Drupal\migrate\Row;
+
+/**
+ * Source plugin for node:blog content.
+ *
+ * @MigrateSource(
+ *   id = "ymca_migrate_node_blog"
+ * )
+ */
+class YmcaMigrateNodeBlog extends SqlBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    $query = $this->select('abe_blog_post', 'b')
+      ->fields(
+        'b',
+        [
+          'blog_post_id',
+          'title',
+          'created_on',
+          'modified_on',
+        ]
+      )
+      ->condition('blog_post_id', 856);
+    return $query;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    $fields = [
+      'blog_post_id' => $this->t('Blog post ID'),
+      'title' => $this->t('Blog title'),
+      'created_on' => $this->t('Creation time'),
+      'modified_on' => $this->t('Modification time'),
+    ];
+
+    return $fields;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+    // Get all component data.
+    $components = $this->select('abe_blog_post_component', 'c')
+      ->fields('c')
+      ->condition('blog_post_id', $row->getSourceProperty('blog_post_id'))
+      ->execute()
+      ->fetchAll();
+
+    return parent::prepareRow($row);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIds() {
+    return [
+      'blog_post_id' => [
+        'type' => 'integer',
+        'alias' => 'b',
+      ],
+    ];
+  }
+
+}
