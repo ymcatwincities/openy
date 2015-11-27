7f4ffea95ed26aed5772fa413378bde229f334d6
Andrey Maximov <savagge@gmail.com>
2015-11-27 14:32:10 +0300

    [YMCA-114] Introduce pagecontext.service

---
+++ docroot/modules/custom/ymca_page_context/src/PageContextService.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\ymca_blocks\AlertsService.
+ */
+
+namespace Drupal\ymca_page_context;
+
+/**
+ * Controls in what context page header should be rendered.
+ */
+class PageContextService {
+  private $context;
+
+  /**
+   * Constructs a new AlertsService.
+   */
+  public function __construct() {
+    $this->context = NULL;
+  }
+
+  /**
+   * Sets current Alert block.
+   *
+   * @param \Drupal\block_content\Entity\BlockContent $block
+   *   Alert Block content entity.
+   */
+  public function setContext(\Drupal\node\Entity\Node $node) {
+    $this->context = $node;
+  }
+
+  /**
+   * Returns current Alert Block.
+   *
+   * @return mixed
+   *   An instance of \Drupal\block_content\Entity\BlockContent or null.
+   */
+  public function getContext() {
+    return $this->context;
+  }
+
+}
