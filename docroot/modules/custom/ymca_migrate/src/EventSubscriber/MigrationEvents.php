d9ec83ae77697bfb65600ff6200176a4fa07e801
podarok <podarok@ua.fm>
2015-12-03 14:53:48 +0200

    Adding views

--- docroot/modules/custom/ymca_migrate/src/EventSubscriber/MigrationEvents.php
+++ docroot/modules/custom/ymca_migrate/src/EventSubscriber/MigrationEvents.php
@@ -8,13 +8,18 @@ namespace Drupal\ymca_migrate\EventSubscriber;
 
 
 use Drupal\Component\Serialization\Yaml;
+use Drupal\file\Entity\File;
 use Drupal\migrate\Event\MigrateEvents;
 use Drupal\migrate\Event\MigrateImportEvent;
-use Drupal\migrate\MigrateExecutable;
 use Drupal\views\Entity\View;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpKernel\Event\PostResponseEvent;
+use Symfony\Component\Validator\Mapping\Loader\YamlFileLoader;
 
+/**
+ * Class MigrationEvents.
+ *
+ * @package Drupal\ymca_migrate\EventSubscriber
+ */
 class MigrationEvents implements EventSubscriberInterface {
 
   /**
@@ -26,14 +31,42 @@ class MigrationEvents implements EventSubscriberInterface {
   public function onPostImport(MigrateImportEvent $event) {
     switch ($event->getMigration()->id()) {
       case 'ymca_migrate_taxonomy_term_tags':
-        /* \Drupal\Core\Config\ConfigManager $cm */
-        $cm = \Drupal::service('config.manager');
-        $data = $cm->getConfigFactory()->get('views.views.ymca_news.yml')->getRawData();
-        // View::create(Yaml::encode($data));
+        $loader = file_get_contents(drupal_get_path('module', 'ymca_migrate') . '/config/disabled/views.view.ymca_news.yml');
+        $data = Yaml::decode($loader);
+        $news_term_id = \Drupal::entityQuery('taxonomy_term')
+          ->condition('name', 'News')
+          ->condition('vid', 'tags')
+          ->execute();
+        $term_id = (int) array_shift($news_term_id);
+        $news_term = \Drupal::entityManager()->getStorage('taxonomy_term')->load($term_id);
+        $data['display']['default']['display_options']['filters']['field_tags_target_id']['value'] = array($term_id => $term_id);
+        $view = View::create($data);
+        $view->set('dependencies', array('content' => array('taxonomy_term:tags:' . $news_term->uuid())));
+        // @todo check if view is already within DB.
+        $view->save();
+
+        $loader = file_get_contents(drupal_get_path('module', 'ymca_migrate') . '/config/disabled/views.view.ymca_twin_cities_blog.yml');
+        $data = Yaml::decode($loader);
+        $news_term_id = \Drupal::entityQuery('taxonomy_term')
+          ->condition('name', 'News', '!=')
+          ->condition('vid', 'tags')
+          ->execute();
+        $terms = \Drupal::entityManager()->getStorage('taxonomy_term')->loadMultiple($news_term_id);
+        $data['display']['default']['display_options']['filters']['field_tags_target_id']['value'] = array();
+        $deps = array();
+        foreach ($terms as $term) {
+          $data['display']['default']['display_options']['filters']['field_tags_target_id']['value'][(int) $term->id()] = (int) $term->id();
+          $deps[] = 'taxonomy_term:tags:' . $term->uuid();
+        }
+
+        $view = View::create($data);
+        $view->set('dependencies', array('content' => $deps));
+        // @todo check if view is already within DB.
+        $view->save();
+
         break;
     }
-    $message = t('Event done: @echo', array('@echo' => var_export($data, TRUE)));
-    \Drupal::logger('ymca_migrate')->error($message);
+
   }
 
   /**
