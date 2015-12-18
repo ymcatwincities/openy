14d34ab035c4794fa557d52894ab80c44ab6db5e
Dmytro Danylevskyi <dmytro@danylevskyi.com>
2015-12-18 09:40:36 +0200

    Filtered out skipped pages

--- docroot/modules/custom/ymca_migrate/ymca_migrate_status/src/Controller/YmcaMigrateStatus.php
+++ docroot/modules/custom/ymca_migrate/ymca_migrate_status/src/Controller/YmcaMigrateStatus.php
@@ -8,12 +8,36 @@
 namespace Drupal\ymca_migrate_status\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Database\Connection;
 use Drupal\Core\Database\Database;
+use Drupal\ymca_migrate\Plugin\migrate\YmcaMigrateTrait;
 
+/**
+ * YmcaMigrateStatus controller.
+ */
 class YmcaMigrateStatus extends ControllerBase {
 
-  private $db_legacy = NULL;
+  use YmcaMigrateTrait;
 
+  /**
+   * Legacy DB connection.
+   *
+   * @var Connection
+   */
+  private $dbLegacy = NULL;
+
+  /**
+   * Indicates debugging or not.
+   *
+   * @var bool
+   */
+  private $isDebug = FALSE;
+
+  /**
+   * List of components.
+   *
+   * @var array
+   */
   private $components = [
     'complex' => [
       'content_block_join',
@@ -24,61 +48,88 @@ class YmcaMigrateStatus extends ControllerBase {
     ],
   ];
 
+  /**
+   * List of pages.
+   *
+   * @var array
+   */
   private $pages = [];
 
-
+  /**
+   * Get list of complex [1] pages.
+   *
+   * @return array
+   *   List of complex [1] pages.
+   */
   private function getComplexPages() {
     if (!array_key_exists(1, $this->pages)) {
-      $query = $this->db_legacy->select('amm_site_page', 'p');
+      $query = $this->dbLegacy->select('amm_site_page', 'p');
       $query->fields('p', ['site_page_id']);
       $query->addJoin('left', 'amm_site_page_component', 'c', 'p.site_page_id = c.site_page_id');
       $query->condition('c.component_type', $this->components['complex'], 'IN');
+      $query->condition('c.component_type', $this->getSkippedPages(), 'NOT IN');
       $query->distinct();
       $this->pages[1] = $query->execute()->fetchAllAssoc('site_page_id');
     }
     return $this->pages[1];
   }
 
+  /**
+   * Get list of simple [0] pages.
+   *
+   * @return array
+   *   List of simple [0] pages.
+   */
   private function getSimplePages() {
     if (!array_key_exists(0, $this->pages)) {
-      $query = $this->db_legacy->select('amm_site_page', 'p');
+      $query = $this->dbLegacy->select('amm_site_page', 'p');
       $query->fields('p', ['site_page_id']);
+      $query->addJoin('left', 'amm_site_page_component', 'c', 'p.site_page_id = c.site_page_id');
+      $query->condition('c.component_type', $this->getSkippedPages(), 'NOT IN');
       $all = $query->execute()->fetchAllAssoc('site_page_id');
       $this->pages[0] = array_diff_key($all, $this->getComplexPages());
     }
     return $this->pages[0];
   }
 
+  /**
+   * Show the page.
+   */
   public function pageView() {
     // Setup.
-    $this->db_legacy = Database::getConnection('default', 'legacy');
+    $this->dbLegacy = Database::getConnection('default', 'legacy');
 
-    $this->getComplexPages();
-    $this->getSimplePages();
-
-    // Get pages with complex components.
+    // Prepare table.
+    $data = [
+      0 => array_values($this->getSimplePages()),
+      1 => array_values($this->getComplexPages()),
+    ];
 
     $header = [
       sprintf('Simple [%d]', count($this->getSimplePages())),
       sprintf('Complex [%d]', count($this->getComplexPages())),
     ];
 
-    $rows = [];
+    $counters = [];
+    foreach ($data as $item => $value) {
+      $counters[] = count($value);
+    }
+
     $count = TRUE;
     $i = 0;
-    $data = [
-      0 => array_values($this->getSimplePages()),
-      1 => array_values($this->getComplexPages()),
-    ];
-
+    $rows = [];
     while ($count === TRUE) {
       foreach ($data as $key => $value) {
         $rows[$i][$key] = $value[$i]->site_page_id;
         $rows[$i][$key] = $value[$i]->site_page_id;
       }
       $i++;
-//      if (max([count($this->pages[0], $this->pages[1])]) == $i) {
-      if ($i == 10) {
+
+      $max = 10;
+      if (!$this->isDebug) {
+        $max = max($counters);
+      }
+      if ($max == $i) {
         $count = FALSE;
       }
     }
