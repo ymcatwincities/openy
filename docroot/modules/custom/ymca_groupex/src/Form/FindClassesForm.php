abfc527a54120f1946708ee1fabd734998ae6e64
Dmytro Danylevskyi <dmytro@danylevskyi.com>
2016-01-04 10:33:55 +0200

    Implement GroupEx search form

--- docroot/modules/custom/ymca_groupex/src/Form/FindClassesForm.php
+++ docroot/modules/custom/ymca_groupex/src/Form/FindClassesForm.php
@@ -15,6 +15,68 @@ use Drupal\Core\Form\FormStateInterface;
 class FindClassesForm extends FormBase {
 
   /**
+   * Uri to make requests.
+   */
+  const URI = 'http://api.groupexpro.com/schedule/embed';
+
+  /**
+   * Account ID.
+   */
+  const ACCOUNT = 3;
+
+  /**
+   * Make a request to GroupEx.
+   *
+   * @param $options
+   *   Request options.
+   *
+   * @return array
+   *   Data.
+   */
+  private function request($options) {
+    $client = \Drupal::httpClient();
+    $data = [];
+    $options_defaults = [
+      'query' => [
+        'a' => self::ACCOUNT,
+      ],
+    ];
+
+    try {
+      $response = $client->request('GET', self::URI, array_merge_recursive($options_defaults, $options));
+      $body = $response->getBody();
+      $data = json_decode($body->getContents());
+    }
+    catch(\Exception $e) {
+      watchdog_exception('ymca_groupex', $e);
+    }
+
+    return $data;
+  }
+
+  /**
+   * Get form item options.
+   *
+   * @param $data
+   *   Data to iterate.
+   * @param $key
+   *   Key name.
+   * @param $value
+   *   Value name.
+   *
+   * @return array
+   *   Array of options.
+   */
+  private function getOptions($data, $key, $value) {
+    $options = [];
+    foreach ($data as $item) {
+      $options[$item->{$key}] = $item->{$value};
+    }
+
+    return $options;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function getFormId() {
@@ -25,8 +87,63 @@ class FindClassesForm extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $form['hello'] = [
-      '#markup' => 'Hello world!',
+
+    $form['note'] = [
+      '#markup' => $this->t('(Please select a location, class or category)'),
+    ];
+
+    $form['locations'] = [
+      '#type' => 'checkboxes',
+      '#options' => $this->getOptions($this->request(['query' => ['locations' => TRUE]]), 'id', 'name'),
+      '#title' => t('Locations'),
+    ];
+
+    $form['class_name'] = [
+      '#type' => 'select',
+      '#options' => array_merge(
+        [0 => $this->t('All')],
+        $this->getOptions($this->request(['query' => ['classes' => TRUE]]), 'id', 'title')
+      ),
+      '#title' => $this->t('Class Name (optional)'),
+    ];
+
+    $form['categories'] = [
+      '#type' => 'select',
+      '#options' => array_merge(
+        [0 => $this->t('All')],
+        $this->getOptions($this->request(['query' => ['categories' => TRUE]]), 'id', 'name')
+      ),
+      '#title' => $this->t('Category (optional)'),
+    ];
+
+    $form['time'] = [
+      '#type' => 'select',
+      '#options' => [
+        'morning' => $this->t('Morning (6 a.m. - 12 p.m.'),
+        'afternoon' => $this->t('Afternoon (12 p.m. - 5 p.m.'),
+        'evening' => $this->t('Evening (5 p.m. - 10 p.m.'),
+      ],
+      '#title' => $this->t('Time of Day (optional)'),
+    ];
+
+    $form['view'] = [
+      '#type' => 'select',
+      '#options' => [
+        'day' => $this->t('Day'),
+        'week' => $this->t('Week'),
+      ],
+      '#title' => $this->t('View Day or Week'),
+      '#description' => $this->t('Selecting more than one location limits your search to one day.'),
+    ];
+
+    $form['date'] = [
+      '#type' => 'date',
+      '#title' => $this->t('Start Date'),
+    ];
+
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Find Class'),
     ];
 
     return $form;
@@ -36,7 +153,7 @@ class FindClassesForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    drupal_set_message('Submitted');
+    drupal_set_message($this->t('Yahoo!!!'));
   }
 
 }
