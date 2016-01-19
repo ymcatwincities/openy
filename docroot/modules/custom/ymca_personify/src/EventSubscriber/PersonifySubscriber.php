bd252bd8a194c4b9d7f3fe6893b25c42f7f6ec12
Dmytro Danylevskyi <dmytro@danylevskyi.com>
2016-01-19 09:55:29 +0200

    Login user in event subscriber

--- docroot/modules/custom/ymca_personify/src/EventSubscriber/PersonifySubscriber.php
+++ docroot/modules/custom/ymca_personify/src/EventSubscriber/PersonifySubscriber.php
@@ -6,6 +6,7 @@
 
 namespace Drupal\ymca_personify\EventSubscriber;
 
+use Drupal\Core\Database\Database;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -21,6 +22,19 @@ class PersonifySubscriber implements EventSubscriberInterface {
    */
   public function checkForLogin(GetResponseEvent $event) {
     if ($event->getRequest()->query->get('login-me')) {
+      if (\Drupal::getContainer()->get('current_user')->id() == 0) {
+        $account = \Drupal::getContainer()
+          ->get('entity.manager')
+          ->getStorage('user')
+          ->load(1);
+        user_login_finalize($account);
+
+        // Update the user table timestamp noting user has logged in.
+        Database::getConnection()->update('users_field_data')
+          ->fields(array('login' => time()))
+          ->condition('uid', 1)
+          ->execute();
+      }
     }
   }
 
