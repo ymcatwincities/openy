b2dd276700fa0215727eeed2ccae9b168581bb70
Andrey Maximov <savagge@gmail.com>
2016-01-27 17:47:02 +0300

    [YMCA-275] Theme All Y Schedules search results page

--- docroot/modules/custom/ymca_groupex/src/Controller/AllSearchResultsController.php
+++ docroot/modules/custom/ymca_groupex/src/Controller/AllSearchResultsController.php
@@ -20,12 +20,21 @@ class AllSearchResultsController extends ControllerBase {
   public function pageView() {
     $query = \Drupal::request()->query->all()['query'];
 
-    $schedule = ymca_groupex_schedule_layout($query);
+    // Get classes schedules.
+    $fetcher = new \Drupal\ymca_groupex\GroupexScheduleFetcher($query);
+    $schedule = $fetcher->getSchedule();
+
+    // Are results empty?
+    $empty_results = empty($schedule['days']) && empty($schedule['locations']) && empty($schedule['classes']);
+
+    $formatted_results = ymca_groupex_schedule_layout($query, $schedule);
     $form = $this->formBuilder()->getForm('Drupal\ymca_groupex\Form\GroupexFormFullRefine', $query);
 
     return [
-      'form' => $form,
-      'schedule' => $schedule,
+      '#form' => $form,
+      '#schedule' => $formatted_results,
+      '#empty_results' => $empty_results,
+      '#theme' => 'groupex_all_search_results',
     ];
   }
 
