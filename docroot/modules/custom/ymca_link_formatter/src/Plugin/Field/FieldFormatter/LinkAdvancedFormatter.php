b3c175e828ddf578a0af32df2ebb8c719b477866
Dmytro Danylevskyi <dmytro@danylevskyi.com>
2016-02-05 15:58:02 +0200

    Fix link formatter to output the options

--- docroot/modules/custom/ymca_link_formatter/src/Plugin/Field/FieldFormatter/LinkAdvancedFormatter.php
+++ docroot/modules/custom/ymca_link_formatter/src/Plugin/Field/FieldFormatter/LinkAdvancedFormatter.php
@@ -7,10 +7,11 @@
 
 namespace Drupal\ymca_link_formatter\Plugin\Field\FieldFormatter;
 
-use Drupal\Component\Utility\SafeMarkup;
-use Drupal\Core\Field\FieldItemInterface;
+use Drupal\Component\Utility\Unicode;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+use Drupal\link\LinkItemInterface;
 use Drupal\link\Plugin\Field\FieldFormatter\LinkFormatter;
 
 /**
@@ -68,6 +69,7 @@ class LinkAdvancedFormatter extends LinkFormatter {
    * {@inheritdoc}
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
+    // Add custom attributes.
     foreach ($items as &$item) {
       if (!empty($item->_attributes['class'])) {
         $item->_attributes['class'][] = $this->getSetting('classes');
@@ -77,7 +79,90 @@ class LinkAdvancedFormatter extends LinkFormatter {
       }
     }
 
-    return parent::viewElements($items, $langcode);
+    $element = array();
+    $entity = $items->getEntity();
+    $settings = $this->getSettings();
+
+    foreach ($items as $delta => $item) {
+      // By default use the full URL as the link text.
+      $url = $this->buildUrl($item);
+      $link_title = $url->toString();
+
+      // If the title field value is available, use it for the link text.
+      if (empty($settings['url_only']) && !empty($item->title)) {
+        // Unsanitized token replacement here because the entire link title
+        // gets auto-escaped during link generation in
+        // \Drupal\Core\Utility\LinkGenerator::generate().
+        $link_title = \Drupal::token()->replace($item->title, [$entity->getEntityTypeId() => $entity], ['clear' => TRUE]);
+      }
+
+      // Trim the link text to the desired length.
+      if (!empty($settings['trim_length'])) {
+        $link_title = Unicode::truncate($link_title, $settings['trim_length'], FALSE, TRUE);
+      }
+
+      if (!empty($settings['url_only']) && !empty($settings['url_plain'])) {
+        $element[$delta] = array(
+          '#plain_text' => $link_title,
+        );
+
+        if (!empty($item->_attributes)) {
+          // Piggyback on the metadata attributes, which will be placed in the
+          // field template wrapper, and set the URL value in a content
+          // attribute.
+          // @todo Does RDF need a URL rather than an internal URI here?
+          // @see \Drupal\rdf\Tests\Field\LinkFieldRdfaTest.
+          $content = str_replace('internal:/', '', $item->uri);
+          $item->_attributes += array('content' => $content);
+        }
+      }
+      else {
+        $element[$delta] = array(
+          '#type' => 'link',
+          '#title' => $link_title,
+          '#options' => $url->getOptions(),
+        );
+        $element[$delta]['#url'] = $url;
+
+        if (!empty($item->_attributes)) {
+          $element[$delta]['#options'] += array ('attributes' => array());
+          $element[$delta]['#options']['attributes'] += $item->_attributes;
+          // Unset field item attributes since they have been included in the
+          // formatter output and should not be rendered in the field template.
+          unset($item->_attributes);
+        }
+      }
+    }
+
+    return $element;
+  }
+
+  /**
+   * Builds the \Drupal\Core\Url object for a link field item.
+   *
+   * @param \Drupal\link\LinkItemInterface $item
+   *   The link field item being rendered.
+   *
+   * @return \Drupal\Core\Url
+   *   An Url object.
+   */
+  protected function buildUrl(LinkItemInterface $item) {
+    $url = $item->getUrl() ?: Url::fromRoute('<none>');
+
+    $settings = $this->getSettings();
+    $options = $url->getOptions();
+
+    // Add optional 'rel' attribute to link options.
+    if (!empty($settings['rel'])) {
+      $options['attributes']['rel'] = $settings['rel'];
+    }
+    // Add optional 'target' attribute to link options.
+    if (!empty($settings['target'])) {
+      $options['attributes']['target'] = $settings['target'];
+    }
+    $url->setOptions($options);
+
+    return $url;
   }
 
 }
