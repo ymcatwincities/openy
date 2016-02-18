3be06eb214b5e8775a901ca0bb43ed4ec06e289e
Andrey Maximov <savagge@gmail.com>
2016-02-18 22:05:12 +0300

    [YMCA-368] Membership page module

--- docroot/modules/custom/ymca_membership/src/Controller/YMCAMembershipPage.php
+++ docroot/modules/custom/ymca_membership/src/Controller/YMCAMembershipPage.php
@@ -3,6 +3,8 @@
 namespace Drupal\ymca_membership\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Url;
+use Drupal\Core\Render\RendererInterface;
 
 /**
  * Implements YMCAMembershipPage.
@@ -13,15 +15,25 @@ class YMCAMembershipPage extends ControllerBase {
    * Show the page.
    */
   public function pageView() {
-
     $assets = \Drupal::config('ymca_membership.assets')->get();
-    dpm($assets, '$assets');
 
-    $block = '<div style="border: 10px solid block;">block</div>';
+    $module_path = drupal_get_path('module', 'ymca_membership');
+    foreach ($assets as &$asset) {
+      $asset = Url::fromUri('base:' . $module_path . '/assets/' . $asset);
+    }
+    $assets_path = Url::fromUri('base:' . $module_path . '/assets');
+
+    $form = $this->getForm('membership_form');
+
+    $block_form_fee = $this->getBlock('Membership page: Form Enrollment Fee');
+    $block_cost = $this->getBlock('Membership page: What Does It Cost?');
 
     return [
-      '#block_price_top' => $block,
-      '#block_price_bottom' => $block,
+      '#assets' => $assets,
+      '#assets_path' => $assets_path,
+      '#block_form_fee' => $block_form_fee,
+      '#block_cost' => $block_cost,
+      '#form' => $form,
       '#theme' => 'membership_page',
       '#cache' => [
         'max-age' => 0,
@@ -32,6 +44,62 @@ class YMCAMembershipPage extends ControllerBase {
         ],
       ],
     ];
+  }
+
+  /**
+   * Retrieves form.
+   *
+   * @param string $form_id
+   *   Contact form machine name.
+   *
+   * @return
+   *   Form build array.
+   */
+  private function getForm($form_id) {
+    $message = $this->entityManager()
+      ->getStorage('contact_message')
+      ->create(array(
+        'contact_form' => $form_id,
+      ));
+
+    $form = $this->entityFormBuilder()->getForm($message);
+    $form['#cache']['contexts'][] = 'user.permissions';
+
+    return $form;
+  }
+
+  /**
+   * Retrieves block.
+   *
+   * @param string $block_description
+   *   Block description.
+   *
+   * @return
+   *   Form build array.
+   */
+  private function getBlock($block_description) {
+    $block = $this->entityManager()
+      ->getStorage('block_content')
+      ->loadByProperties([
+        'info' => $block_description,
+      ]);
+
+    $block_build = [
+      '#markup' => '<div>No block named «<em>' . $block_description . '</em>» found.'
+        . \Drupal::l('Add block', Url::fromUri('base:block/add')) .'</div>'
+    ];
+    if ($block) {
+      $block_content_entity = reset($block);
+      $view_builder = \Drupal::entityTypeManager()->getViewBuilder('block_content');
+      $block_content = $view_builder->view($block_content_entity);
+      $block_build = [
+        '#theme' => ['datebased_block_with_cl'],
+        '#content' => $block_content,
+        '#contextual_links' => $block_content['#contextual_links'],
+      ];
+    }
+
+    return $block_build;
   }
 
 }
