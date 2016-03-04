4fd213fae799a646e21766791c51b6dde9cc2d34
Andrey Maximov <savagge@gmail.com>
2016-03-04 14:20:50 +0300

    [YMCA-535] Allow drupal-entity tag to be a content of any tag

---
+++ docroot/modules/custom/ymca_ckeditor/js/ymca_ckeditor.js
@@ -0,0 +1,28 @@
+/**
+ * @file
+ * Alters CKEditor DTD.
+ */
+
+(function ($) {
+
+  'use strict';
+
+  /**
+   * Alter CKEditor.dtd, allowing drupal-entity to be a content of any tag.
+   */
+  Drupal.behaviors.ymca_ckeditor = {
+
+    /**
+     * Editor attach callback.
+     */
+    attach: function (context, settings) {
+      for (var i in window.CKEDITOR.dtd) {
+        if (i.substring(0, 1) != '$') {
+          window.CKEDITOR.dtd[i]['drupal-entity'] = 1;
+        }
+      }
+    }
+
+  };
+
+})(jQuery);
