a6d8b9f9b7ae488b23ce5259ecb5e883044b9972
Andrii TIupa <andrii.tiupa@ffw.co>
2016-03-22 12:03:11 +0200

    [YMCA-677] Add missed pages

---
+++ docroot/modules/custom/ymca_frontend/src/Controller/YMCA_2014_annual_report_leaders_Controller.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\ymca_frontend\Controller;
+
+/**
+ * Controller for "Cause-driven Leaders" page.
+ */
+class YMCA_2014_annual_report_leaders_Controller {
+
+  /**
+   * Set page's content.
+   */
+  public function content() {
+    return array(
+      '#markup' => '',
+    );
+  }
+
+  /**
+   * Set Title.
+   */
+  public function setTitle() {
+    return t('Cause-driven Leaders');
+  }
+
+}
