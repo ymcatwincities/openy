4844a7a31b4b2af92a1ee08b4f9fc8b41790eb3d
Dmytro Danylevskyi <dmytro@danylevskyi.com>
2016-03-28 11:50:44 +0300

    Add parent_404 module

---
+++ docroot/modules/custom/parent_404/src/EventSubscriber/Parent404Handler.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Drupal\parent_404\EventSubscriber;
+
+use Drupal\Core\EventSubscriber\HttpExceptionSubscriberBase;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+
+/**
+ * Parent 404 exception handler.
+ */
+class Parent404Handler extends HttpExceptionSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static function getPriority() {
+    return 201;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getHandledFormats() {
+    return ['html'];
+  }
+
+  /**
+   * Redirect user to parent page.
+   *
+   * @param GetResponseForExceptionEvent $event
+   *   Event.
+   */
+  public function on404(GetResponseForExceptionEvent $event) {
+    $request_path = $event->getRequest()->getPathInfo();
+    $items = explode('/', $request_path);
+    $dir = $items[1];
+    $to_direct = ['blog', 'news'];
+    if (in_array($dir, $to_direct)) {
+      $redirect = new RedirectResponse('/' . $dir, 303);
+      $event->setResponse($redirect);
+    }
+  }
+
+}
