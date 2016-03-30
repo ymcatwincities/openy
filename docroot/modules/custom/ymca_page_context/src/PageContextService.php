889547276456240477dd8bbb87eed7c71d9079cd
Andrii TIupa <andrii.tiupa@ffw.co>
2016-03-30 18:18:34 +0300

    [YMCA-764] fix regressions of preview functionality

--- docroot/modules/custom/ymca_page_context/src/PageContextService.php
+++ docroot/modules/custom/ymca_page_context/src/PageContextService.php
@@ -34,28 +34,24 @@ class PageContextService {
    *   An instance of \Drupal\node\Entity\Node or null.
    */
   public function getContext() {
-    if (\Drupal::routeMatch()->getRouteName() == 'entity.node.canonical') {
-      $node = \Drupal::routeMatch()->getParameter('node');
-    }
+    $node = \Drupal::routeMatch()->getParameter('node');
     if (\Drupal::routeMatch()->getRouteName() == 'entity.node.preview') {
       $node = \Drupal::routeMatch()->getParameter('node_preview');
     }
-    if (isset($node)) {
-      if (in_array($node->bundle(), ['camp', 'location'])) {
-        return $node;
-      }
-      if ($node->hasField('field_related')) {
-        if ($value = $node->field_related->getValue()) {
-          if ($id = $value[0]['target_id']) {
-            return \Drupal::entityTypeManager()->getStorage('node')->load($id);
-          }
+    if (in_array($node->bundle(), ['camp', 'location'])) {
+      return $node;
+    }
+    if ($node->hasField('field_related')) {
+      if ($value = $node->field_related->getValue()) {
+        if ($id = $value[0]['target_id']) {
+          return \Drupal::entityTypeManager()->getStorage('node')->load($id);
         }
       }
-      if ($node->hasField('field_site_section')) {
-        if ($value = $node->field_site_section->getValue()) {
-          if ($id = $value[0]['target_id']) {
-            return \Drupal::entityTypeManager()->getStorage('node')->load($id);
-          }
+    }
+    if ($node->hasField('field_site_section')) {
+      if ($value = $node->field_site_section->getValue()) {
+        if ($id = $value[0]['target_id']) {
+          return \Drupal::entityTypeManager()->getStorage('node')->load($id);
         }
       }
     }
