9b3033d937dab70bc2711111b3a3ca2ef092d67d
Andrey Maximov <savagge@gmail.com>
2016-04-01 17:42:07 +0300

    [YMCA-383] Override menu list, hidding main menu

---
+++ docroot/modules/custom/ymca_menu/src/MenuListBuilder.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\menu_ui\MenuListBuilder.
+ */
+
+namespace Drupal\ymca_menu;
+
+use Drupal\menu_ui\MenuListBuilder as CoreMenuListBuilder;
+
+/**
+ * Overrides core menu list builder.
+ *
+ * Hides main menu from menu list.
+ *
+ * @see \Drupal\system\Entity\Menu
+ * @see menu_entity_info()
+ */
+class MenuListBuilder extends CoreMenuListBuilder {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render() {
+    $build = parent::render();
+    $build['table']['#rows']['main'] = [
+      'data' => $build['table']['#rows']['main'],
+      'class' => 'hidden',
+    ];
+    return $build;
+  }
+
+}
