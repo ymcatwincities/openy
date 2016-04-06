07a9b154deb4e246c2bd226b6f3605b1fc465117
Dmytro Danylevskyi <dmytro@danylevskyi.com>
2016-04-06 07:22:35 +0300

    Add working prototype of config importer

--- docroot/modules/custom/config_import/src/ConfigImporterService.php
+++ docroot/modules/custom/config_import/src/ConfigImporterService.php
@@ -2,9 +2,12 @@
 
 namespace Drupal\config_import;
 
-use Drupal\Core\Config\InstallStorage;
 use Drupal\Core\Config\ConfigManagerInterface;
 use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Config\FileStorage;
+use Exception;
+use Drupal\Core\Config\StorageComparer;
+use Drupal\Core\Config\ConfigImporter;
 
 /**
  * Implements Config Importer Service.
@@ -39,49 +42,88 @@ class ConfigImporterService {
   /**
    * Import config.
    *
-   * @param array $filenames
+   * @param array $files
    *   Array of strings, each item is a path to config file.
    */
-  public function importConfigs(array $filenames) {
-    foreach ($filenames as $filename) {
-      $contents = @file_get_contents($filename);
-      if (!$contents) {
-        continue;
-      }
-      $data = (new InstallStorage())->decode($contents);
-      $config_name = basename($filename, '.yml');
-      $entity_type_id = $this->configManager->getEntityTypeIdByName($config_name);
-      if ($entity_type_id) {
-        $entity_storage = $this->getStorage($entity_type_id);
-        $entity_id = $entity_storage::getIDFromConfigName($config_name, $entity_storage->getEntityType()->getConfigPrefix());
-        $entity_type = $entity_storage->getEntityType();
-        $id_key = $entity_type->getKey('id');
-        $data[$id_key] = $entity_id;
-        $entity = $entity_storage->create($data);
-        if ($existing_entity = $entity_storage->load($entity_id)) {
-          $entity
-            ->set('uuid', $existing_entity->uuid())
-            ->enforceIsNew(FALSE);
-        }
-        $entity_storage->save($entity);
-      }
-      else {
-        $this->configFactory->getEditable($config_name)->setData($data)->save();
-      }
+  public function importConfigs(array $files) {
+    // @todo The next string doesn't work during installation. Hardcode it.
+    // $uri = 'temporary://confi_tmp_' . \Drupal::service('uuid')->generate();
+    $tmp_dir = '/tmp/confi_tmp_' . \Drupal::service('uuid')->generate();
+
+    try {
+      $this->export($tmp_dir);
+    }
+    catch (Exception $e) {
+      watchdog_exception('config_import', $e);
+      return;
+    }
+
+    foreach ($files as $source) {
+      file_unmanaged_copy($source, $tmp_dir, FILE_EXISTS_REPLACE);
+    }
+
+    $this->import($tmp_dir);
+  }
+
+  public function import($dir) {
+    $active_storage = \Drupal::service('config.storage');
+    $source_storage = new FileStorage($dir);
+    $config_manager = \Drupal::service('config.manager');
+    $storage_comparer = new StorageComparer($source_storage, $active_storage, $config_manager);
+
+    if (!$storage_comparer->createChangelist()->hasChanges()) {
+      return;
+    }
+
+    $config_importer = new ConfigImporter(
+      $storage_comparer,
+      \Drupal::service('event_dispatcher'),
+      \Drupal::service('config.manager'),
+      \Drupal::lock(),
+      \Drupal::service('config.typed'),
+      \Drupal::moduleHandler(),
+      \Drupal::service('module_installer'),
+      \Drupal::service('theme_handler'),
+      \Drupal::service('string_translation')
+    );
+
+
+    try {
+      $config_importer->import();
+    }
+    catch (Exception $e) {
+      watchdog_exception('config_import', $e);
     }
   }
 
   /**
-   * Return storage.
+   * Export active configuration to temporary directory.
    *
-   * @param string $entity_type_id
-   *   Entity type id.
+   * @param string $dir
+   *   Uri of a temporary directory.
    *
-   * @return \Drupal\Core\Entity\EntityStorageInterface
-   *   The entity storage.
+   * @throws Exception
+   *   When fails to create a directory.
    */
-  private function getStorage($entity_type_id) {
-    return $this->configManager->getEntityManager()->getStorage($entity_type_id);
+  public function export($dir) {
+    $result = \Drupal::service('file_system')->mkdir($dir);
+    if (!$result) {
+      throw new \Exception('Failed to create temporary directory: ' . $dir);
+    }
+    $source_storage = \Drupal::service('config.storage');
+    $destination_storage = new FileStorage($dir);
+
+    $filters = [
+      'devel.settings',
+      'config_devel.settings',
+    ];
+
+    foreach ($source_storage->listAll() as $name) {
+      if (in_array($name, $filters)) {
+        continue;
+      }
+      $destination_storage->write($name, $source_storage->read($name));
+    }
   }
 
 }
