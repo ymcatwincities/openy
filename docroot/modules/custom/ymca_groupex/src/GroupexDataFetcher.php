25c9c6c9ddbbc780be5a545507a687283bb0a1bc
Dmytro Danylevskyi <dmytro@danylevskyi.com>
2016-05-24 11:03:02 +0300

    [YMCA-923] Add GropexDataFetcher service

---
+++ docroot/modules/custom/ymca_groupex/src/GroupexDataFetcher.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\ymca_groupex;
+
+/**
+ * Class GroupexDataFetcher.
+ *
+ * @package Drupal\ymca_groupex
+ */
+class GroupexDataFetcher implements GroupexDataFetcherInterface {
+  
+  use GroupexRequestTrait;
+
+  /**
+   * GroupexDataFetcher constructor.
+   */
+  public function __construct() {
+    
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fetch($start, $end) {
+    $options = [
+      'query' => [
+        'schedule' => TRUE,
+        'desc' => 'true',
+        'start' => $start,
+        'end' => $end,
+      ],
+    ];
+
+    $data = $this->request($options);
+    return $data;
+  }
+
+}
