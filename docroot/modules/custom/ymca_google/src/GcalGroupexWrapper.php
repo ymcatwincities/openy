73cd6ac5d8468c78cb9f2d006bb0548683cdd406
podarok <podarok@ua.fm>
2016-05-25 13:03:56 +0300

    Initial Syncer

---
+++ docroot/modules/custom/ymca_google/src/GcalGroupexWrapper.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Drupal\ymca_google;
+
+
+/**
+ * Class GcalGroupexWrapper.
+ *
+ * @package Drupal\ymca_google
+ */
+class GcalGroupexWrapper implements GcalGroupexWrapperInterface {
+
+  /**
+   * Constructor.
+   */
+  public function __construct() {
+
+  }
+
+}
