321400c9da1c8c272601c85607f317f4ffbdf3e2
podarok <podarok@ua.fm>
2016-05-25 14:57:03 +0300

    Init architecture checked

--- docroot/modules/custom/ymca_google/src/GcalGroupexWrapper.php
+++ docroot/modules/custom/ymca_google/src/GcalGroupexWrapper.php
@@ -11,10 +11,79 @@ namespace Drupal\ymca_google;
 class GcalGroupexWrapper implements GcalGroupexWrapperInterface {
 
   /**
+   * Raw source data from source system.
+   *
+   * @var array
+   */
+  protected $sourceData;
+
+  /**
+   * Prepared data for proxy system.
+   *
+   * @var array
+   */
+  protected $proxyData;
+
+  /**
+   * Prepared data for destination system.
+   *
+   * @var array
+   */
+  protected $destinationData;
+  /**
    * Constructor.
    */
   public function __construct() {
     $i = 0;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDrupalEntitiesFromSource() {
+    // TODO: Implement getDrupalEntitiesFromSource() method.
+    $this->sourceData = [
+      0 => ['title' => 'Event1', 'id' => 123],
+      1 => ['title' => 'Event2', 'id' => 124],
+    ];
+    foreach ($this->sourceData as $id => &$sourceItem) {
+      $this->proxyData[$id] = new \stdClass();
+      $this->proxyData[$id]->title = $sourceItem['title'];
+      $this->proxyData[$id]->entity_id = $sourceItem['id'];
+    }
+    return $this->sourceData;
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDestinationEntitiesFromProxy() {
+    // TODO: Implement getDestinationEntitiesFromProxy() method.
+
+    foreach ($this->proxyData as $id => &$hostEntity) {
+      $this->destinationData[] = array(
+        'summary' => $hostEntity->title,
+        'location' => 'Kyiv, Ukraine, 01042',
+        'description' => 'Description for test event from code.',
+        'start' => array(
+          'dateTime' => '2016-05-24T09:00:00-07:00',
+          'timeZone' => 'UTC',
+        ),
+        'end' => array(
+          'dateTime' => '2016-05-24T17:00:00-07:00',
+          'timeZone' => 'UTC',
+        ),
+      );
+    }
+    return $this->destinationData;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getProxyData() {
+    return $this->proxyData;
   }
 
 }
--- docroot/modules/custom/ymca_google/src/GcalGroupexWrapperInterface.php
+++ docroot/modules/custom/ymca_google/src/GcalGroupexWrapperInterface.php
@@ -9,5 +9,24 @@ namespace Drupal\ymca_google;
  */
 interface GcalGroupexWrapperInterface {
 
+  /**
+   * Array of raw entities from source system.
+   * 
+   * @return array
+   */
+  public function getDrupalEntitiesFromSource();
 
+  /**
+   * Array of entities, prepared for destination system push.
+   *
+   * @return array
+   */
+  public function getDestinationEntitiesFromProxy();
+
+  /**
+   * Array of entities to be cached and enriched on host system.
+   *
+   * @return array
+   */
+  public function getProxyData();
 }
