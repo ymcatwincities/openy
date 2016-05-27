666ee9a3727e2e31f6877be64416e828d27cc561
Dmytro Danylevskyi <dmytro@danylevskyi.com>
2016-05-27 09:10:14 +0300

    Add mapping fields update logic

--- docroot/modules/custom/ymca_groupex/src/DrupalProxy.php
+++ docroot/modules/custom/ymca_groupex/src/DrupalProxy.php
@@ -3,9 +3,11 @@
 namespace Drupal\ymca_groupex;
 
 use Drupal\Core\Entity\Query\QueryFactory;
+use Drupal\Core\Logger\LoggerChannelFactory;
 use Drupal\ymca_google\GcalGroupexWrapper;
 use Drupal\Core\Datetime\DrupalDateTime;
 use Drupal\ymca_mappings\Entity\Mapping;
+use Drupal\ymca_mappings\MappingInterface;
 
 /**
  * Class DrupalProxy.
@@ -36,16 +38,26 @@ class DrupalProxy implements DrupalProxyInterface {
   protected $queryFactory;
 
   /**
+   * Logger factory.
+   *
+   * @var QueryFactory
+   */
+  protected $loggerFactory;
+
+  /**
    * DrupalProxy constructor.
    *
    * @param GcalGroupexWrapper $data_wrapper
    *   Data wrapper.
    * @param QueryFactory $query_factory
    *   Query factory.
+   * @param LoggerChannelFactory $logger_factory
+   *   Logger factory.
    */
-  public function __construct(GcalGroupexWrapper $data_wrapper, QueryFactory $query_factory) {
+  public function __construct(GcalGroupexWrapper $data_wrapper, QueryFactory $query_factory, LoggerChannelFactory $logger_factory) {
     $this->dataWrapper = $data_wrapper;
     $this->queryFactory = $query_factory;
+    $this->loggerFactory = $logger_factory;
 
     $this->timezone = new \DateTimeZone('UTC');
   }
@@ -58,10 +70,35 @@ class DrupalProxy implements DrupalProxyInterface {
 
     foreach ($this->dataWrapper->getSourceData() as $item) {
       // Parse time to create timestamps.
+      $all_day = FALSE;
       preg_match("/(.*)-(.*)/i", $item->time, $output);
-      $item->timestamp_start = $this->extractTimestamp($item->date, $output[1]);
-      $item->timestamp_end = $this->extractTimestamp($item->date, $output[2]);
+      if (isset($output[1]) && isset($output[2])) {
+        $time_start = $output[1];
+        $time_end = $output[2];
+      }
+      else {
+        // If we can't fetch exact time, assume it as all day event.
+        $all_day = TRUE;
+        $time_start = '12:00pm';
+        $time_end = '12:00pm';
 
+        // Log exception for unknown values.
+        if ($item->time != "All Day") {
+          $message = 'DrupalProxy: Got unknown time value (%value) for the class ID %ID';
+          $this->loggerFactory->get('ymca_sync')->error(
+            $message,
+            ['%value' => $item->time, '%ID' => $item->id]
+          );
+        }
+      }
+
+      $item->timestamp_start = $this->extractTimestamp($item->date, $time_start);
+      $item->timestamp_end = $this->extractTimestamp($item->date, $time_end);
+
+      // Just add 24 hours for All day events.
+      if ($all_day) {
+        $item->timestamp_end = $item->timestamp_start + (60 * 60 * 24);
+      }
 
       // Try to find existing mapping.
       $existing = $this->findByGroupexId($item->id);
@@ -86,12 +123,85 @@ class DrupalProxy implements DrupalProxyInterface {
         ]);
         $mapping->setName($item->location . ' [' . $item->id . ']');
         $mapping->save();
+        $entities['insert'][] = $mapping;
+      }
+      else {
+        // Entity exists. Check for the diff.
+        $diff = $this->diff($existing, $item);
+
+        // Proceed only with changed entities.
+        if (!empty($diff['date']) || !empty($diff['fields'])) {
+
+          // Update fields.
+          foreach ($diff['fields'] as $field_name => $value) {
+            $existing->set($field_name, $value);
+          }
+
+          // Add recurring event.
+          if (!empty($diff['date'])) {
+
+          }
+
+          $existing->save();
+          $entities['update'][] = $existing;
+        }
       }
 
-      $entities[] = $mapping;
     }
 
     $this->dataWrapper->setProxyData($entities);
+  }
+
+  /**
+   * Diffs entity saved in DB and groupex class item.
+   *
+   * @param MappingInterface $entity
+   *   Mapping entity.
+   *
+   * @param $class
+   *   Class item (enriched with timestamps).
+   *
+   * @return mixed
+   *   Diff array.
+   */
+  protected function diff(MappingInterface $entity, $class) {
+    $diff['fields'] = [];
+    $diff['date'] = [];
+
+    // Compare simple fields.
+    $compare = [
+      'field_groupex_category' => 'category',
+      'field_groupex_description' => 'desc',
+      'field_groupex_instructor' => 'instructor',
+      'field_groupex_location' => 'location',
+      'field_groupex_orig_instructor' => 'original_instructor',
+      'field_groupex_studio' => 'studio',
+      'field_groupex_sub_instructor' => 'sub_instructor',
+      'field_groupex_title' => 'title',
+      'field_groupex_time' => 'time',
+    ];
+
+    foreach ($compare as $drupal_field => $groupex_field) {
+      $drupal_value = $entity->{$drupal_field}->value;
+      $groupex_value = $class->{$groupex_field};
+      if (strcmp($drupal_value, $groupex_value) != 0) {
+        $diff['fields'][$drupal_field] = $groupex_value;
+      }
+    }
+
+    // Check timestamps.
+    if (
+      $entity->field_timestamp_start->value == $class->timestamp_start &&
+      $entity->field_timestamp_end->value == $class->timestamp_end
+    ) {
+      return $diff;
+    }
+
+    // The event is recurring.
+    $diff['date']['date'] = $class->date;
+    $diff['date']['time'] = $class->time;
+
+    return $diff;
   }
 
   /**
