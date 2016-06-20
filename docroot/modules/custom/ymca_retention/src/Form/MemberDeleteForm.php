df295d5ee2b9ae3aee3578c24f15a3505b630dcc
Dmitry Drozdik <dmitriydrozdik@gmail.com>
2016-06-20 17:37:25 +0300

    [YSR-24] fix sniffer comments

--- docroot/modules/custom/ymca_retention/src/Form/MemberDeleteForm.php
+++ docroot/modules/custom/ymca_retention/src/Form/MemberDeleteForm.php
@@ -17,7 +17,9 @@ class MemberDeleteForm extends ContentEntityConfirmFormBase {
    * {@inheritdoc}
    */
   public function getQuestion() {
-    return $this->t('Are you sure you want to delete entity %name?', ['%name' => $this->entity->label()]);
+    return $this->t('Are you sure you want to delete entity %name?', [
+      '%name' => $this->entity->label(),
+    ]);
   }
 
   /**
@@ -46,9 +48,9 @@ class MemberDeleteForm extends ContentEntityConfirmFormBase {
     $entity->delete();
 
     \Drupal::logger('ymca_retention_member')->notice('@type: deleted %title.', [
-        '@type' => $this->entity->bundle(),
-        '%title' => $this->entity->label(),
-      ]);
+      '@type' => $this->entity->bundle(),
+      '%title' => $this->entity->label(),
+    ]);
     $form_state->setRedirect('entity.ymca_retention_member.collection');
   }
 
