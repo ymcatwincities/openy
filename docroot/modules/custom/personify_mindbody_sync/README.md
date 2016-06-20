805b6bb081707ac3bc1a8afa4ef611df3a60fa02
Dmytro Danylevskyi <dmytro@danylevskyi.com>
2016-06-20 16:54:34 +0300

    Add methods to save proxy data

--- docroot/modules/custom/personify_mindbody_sync/README.md
+++ docroot/modules/custom/personify_mindbody_sync/README.md
@@ -8,3 +8,8 @@ To run the process use the next code:
   * With Drush:
   `drush ev 'ymca_sync_run("personify_mindbody_sync.syncer", "proceed");'`
 
+## Help methods
+
+### Clear cached entities
+
+  `drush ev '\Drupal::service("personify_mindbody_sync.proxy")->clearCache();'`
--- docroot/modules/custom/personify_mindbody_sync/src/PersonifyMindbodySyncProxy.php
+++ docroot/modules/custom/personify_mindbody_sync/src/PersonifyMindbodySyncProxy.php
@@ -2,8 +2,11 @@
 
 namespace Drupal\personify_mindbody_sync;
 
+use Drupal\Core\Entity\EntityTypeManager;
+use Drupal\Core\Entity\Query\QueryFactory;
 use Drupal\Core\Logger\LoggerChannel;
 use Drupal\Core\Logger\LoggerChannelFactory;
+use Drupal\personify_mindbody_sync\Entity\PersonifyMindbodyCache;
 
 /**
  * Class PersonifyMindbodySyncProxy.
@@ -27,23 +30,64 @@ class PersonifyMindbodySyncProxy implements PersonifyMindbodySyncProxyInterface 
   protected $logger;
 
   /**
+   * Query factory.
+   *
+   * @var QueryFactory
+   */
+  protected $query;
+
+  /**
+   * Entity type manager.
+   *
+   * @var EntityTypeManager
+   */
+  protected $entityTypeManager;
+
+  /**
    * PersonifyMindbodySyncProxy constructor.
    *
    * @param PersonifyMindbodySyncWrapper $wrapper
    *   Wrapper.
    * @param LoggerChannelFactory $logger_factory
    *   Logger factory.
+   * @param QueryFactory $query_factory
+   *   Query factory.
+   * @param EntityTypeManager $entity_type_manager
+   *   Entity type manager.
    */
-  public function __construct(PersonifyMindbodySyncWrapper $wrapper, LoggerChannelFactory $logger_factory) {
+  public function __construct(PersonifyMindbodySyncWrapper $wrapper, LoggerChannelFactory $logger_factory, QueryFactory $query_factory, EntityTypeManager $entity_type_manager) {
     $this->wrapper = $wrapper;
     $this->logger = $logger_factory->get(PersonifyMindbodySyncWrapper::CHANNEL);
+    $this->query = $query_factory;
+    $this->entityTypeManager = $entity_type_manager;
   }
 
   /**
    * {@inheritdoc}
    */
   public function saveEntities() {
-    $a = 10;
+    foreach ($this->wrapper->getSourceData() as $item) {
+      $cache_item = PersonifyMindbodyCache::create([
+        'field_pmc_data' => serialize($item),
+        'field_pmc_order_num' => $item->OrderNo,
+        'field_pmc_order_line_num' => $item->OrderLineNo,
+      ]);
+      $cache_item->setName($item->OrderNo . ' (' . $item->OrderLineNo . ')');
+      $cache_item->save();
+    }
+  }
+
+  /**
+   * Clear cached entities.
+   */
+  public function clearCache() {
+    $ids = $this->query->get(PersonifyMindbodySyncWrapper::CACHE_ENTITY)->execute();
+    $chunks = array_chunk($ids, 10);
+    $storage = $this->entityTypeManager->getStorage(PersonifyMindbodySyncWrapper::CACHE_ENTITY);
+    foreach ($chunks as $chunk) {
+      $cache = PersonifyMindbodyCache::loadMultiple($chunk);
+      $storage->delete($cache);
+    }
   }
 
 }
