6ae35eb33ab1550a92ad25b7a96c60aa9c1ed2af
podarok <podarok@ua.fm>
2016-06-23 15:55:17 +0300

    Change field type from numeric to string

--- docroot/modules/custom/personify_mindbody_sync/src/PersonifyMindbodySyncPusher.php
+++ docroot/modules/custom/personify_mindbody_sync/src/PersonifyMindbodySyncPusher.php
@@ -77,33 +77,62 @@ class PersonifyMindbodySyncPusher implements PersonifyMindbodySyncPusherInterfac
     foreach ($this->wrapper->getProxyData() as $id => $entity) {
       $personifyData = unserialize($entity->get('field_pmc_data')->getValue()[0]['value']);
       if ($entity->get('field_pmc_mindbody_data')->isEmpty()) {
-        $this->clientIds[$entity->get('field_user_id')->getValue()[0]['value']] = [
-          'NewID' => $entity->get('field_user_id')->getValue()[0]['value'],
+        $this->clientIds[$entity->get('field_pmc_user_id')->getValue()[0]['value']] = new \SoapVar([
+          'NewID' => $entity->get('field_pmc_user_id')->getValue()[0]['value'],
           'FirstName' => $personifyData->FirstName,
           'LastName' => $personifyData->LastName,
           'Email' => $personifyData->PrimaryEmail,
-          'AddressLine1' => '',
-          'City' => '',
-          'PostalCode' => '',
-          'ReferredBy' => '',
+          //'AddressLine1' => '',
+          //'City' => '',
+          //'PostalCode' => '',
+          //'ReferredBy' => '',
           'BirthDate' => $personifyData->BirthDate,
-          'State' => 'Nevada',
+          //'State' => '',
           'MobilePhone' => $personifyData->PrimaryPhone,
-        ];
+        ],
+          SOAP_ENC_OBJECT,
+          'Client',
+          'http://clients.mindbodyonline.com/api/0_5');
       }
     }
-    $result = $this->client->call('ClientService', 'GetClients', ['ClientIDs' => array_keys($this->clientIds)], FALSE);
-    $count = $result->GetClientsResult->ResultCount;
-    if ($count != 0) {
-      // @todo We've found a few clients already. Let's filter them out.
-      $this->logger->error('We\'ve found a few clients aleady. Let\'s filter them out');
-    }
-    else {
-      // @todo Save new clients to MindBody.
-      $result = $this->client->call('ClientService', 'AddOrUpdateClients', ['ClientIDs' => array_values($this->clientIds)], FALSE);
-      return $this->clientIds;
-    }
 
+    $result = $this->client->call('ClientService', 'GetClients', ['ClientIDs' => [array_shift(array_keys($this->clientIds))]], FALSE);
+
+    if ($result->GetClientsResult->ErrorCode == 200 && $result->GetClientsResult->ResultCount != 0) {
+      $remote_clients = [];
+      if ($result->GetClientsResult->ResultCount == 1) {
+        $remote_clients[] = $result->GetClientsResult->Clients->Client;
+      }
+      else {
+        $remote_clients = $result->GetClientsResult->Clients->Client;
+      }
+
+      // We've found a few clients already. Let's filter them out.
+      foreach ($remote_clients as $client) {
+        // Skip users already saved into cache.
+        // @todo I'm guessing ID is not unique.
+        unset($this->clientIds[$client->ID]);
+        $cache_id = \Drupal::entityQuery('personify_mindbody_cache')
+          ->condition('field_pmc_user_id', $client->ID)
+          ->execute();
+        /** @var PersonifyMindbodyCache $cache_entity */
+        $cache_entity = $this->wrapper->getProxyData()[array_shift($cache_id)];
+        // Updating local storage about MindBody client's data if first time.
+        if ($cache_entity->get('field_pmc_mindbody_data')->isEmpty()) {
+          // @todo make it more smart via diff with old data for getting actual.
+          $cache_entity->set('field_pmc_mindbody_data', serialize($client));
+          $cache_entity->save();
+        }
+      }
+    }
+    elseif ($result->GetClientsResult->ErrorCode != 200) {
+      $this->logger->critical('[DEV] Error from MindBody: %error', ['%error' => print_r($result, TRUE)]);
+      return;
+    }
+    // @todo Save new clients to MindBody.
+    $test = array_values($this->clientIds);
+    $result = $this->client->call('ClientService', 'AddOrUpdateClients', ['Clients' => $test], FALSE);
+    return $this->clientIds;
   }
 
 }
