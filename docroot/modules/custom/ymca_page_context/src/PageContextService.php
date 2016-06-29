5c2f76286cf55a9dec01dcf96b9e177b4a99e3b5
Andrii TIupa <andrii.tiupa@ffw.co>
2016-06-29 17:37:32 +0300

    [YPTF-48] Context on Forms related to Locations/Trainers

--- docroot/modules/custom/ymca_page_context/src/PageContextService.php
+++ docroot/modules/custom/ymca_page_context/src/PageContextService.php
@@ -14,6 +14,11 @@ class PageContextService {
    * Constructs a new PageContextService.
    */
   public function __construct() {
+    $container = \Drupal::getContainer();
+    $this->proxy = $container->get('mindbody_cache_proxy.client');
+    $this->entityQuery = $container->get('entity.query');
+    $this->entityTypeManager = $container->get('entity_type.manager');
+    $this->currentRouteMatch = $container->get('current_route_match');
     $this->context = NULL;
   }
 
@@ -34,9 +39,10 @@ class PageContextService {
    *   An instance of \Drupal\node\Entity\Node or null.
    */
   public function getContext() {
-    $node = \Drupal::routeMatch()->getParameter('node');
-    if (\Drupal::routeMatch()->getRouteName() == 'entity.node.preview') {
-      $node = \Drupal::routeMatch()->getParameter('node_preview');
+    $query = \Drupal::request()->query->all();
+    $node = $this->currentRouteMatch->getParameter('node');
+    if ($this->currentRouteMatch->getRouteName() == 'entity.node.preview') {
+      $node = $this->currentRouteMatch->getParameter('node_preview');
     }
     if (isset($node) && is_object($node)) {
       if (in_array($node->bundle(), ['camp', 'location'])) {
@@ -45,15 +51,30 @@ class PageContextService {
       if ($node->hasField('field_related')) {
         if ($value = $node->field_related->getValue()) {
           if ($id = $value[0]['target_id']) {
-            return \Drupal::entityTypeManager()->getStorage('node')->load($id);
+            return $this->entityTypeManager->getStorage('node')->load($id);
           }
         }
       }
       if ($node->hasField('field_site_section')) {
         if ($value = $node->field_site_section->getValue()) {
           if ($id = $value[0]['target_id']) {
-            return \Drupal::entityTypeManager()->getStorage('node')->load($id);
+            return $this->entityTypeManager->getStorage('node')->load($id);
           }
+        }
+      }
+    }
+    if (isset($query['location']) && is_numeric($query['location'])) {
+      $mapping_id = $this->entityQuery
+        ->get('mapping')
+        ->condition('type', 'location')
+        ->condition('field_mindbody_id', $query['location'])
+        ->execute();
+      $mapping_id = reset($mapping_id);
+      if ($mapping = $this->entityTypeManager->getStorage('mapping')->load($mapping_id)) {
+        $field_location_ref = $mapping->field_location_ref->getValue();
+        $location_id = isset($field_location_ref[0]['target_id']) ? $field_location_ref[0]['target_id'] : FALSE;
+        if ($location_node = $this->entityTypeManager->getStorage('node')->load($location_id)) {
+          return $location_node;
         }
       }
     }
