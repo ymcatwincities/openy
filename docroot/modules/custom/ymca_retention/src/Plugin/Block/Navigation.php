15428cb74e514587fb377e3638c2ffd200115f93
Dmitry Drozdik <dmitriydrozdik@gmail.com>
2016-07-05 17:39:20 +0300

    [YSR-3] Add some changes

--- docroot/modules/custom/ymca_retention/src/Plugin/Block/Navigation.php
+++ docroot/modules/custom/ymca_retention/src/Plugin/Block/Navigation.php
@@ -22,27 +22,38 @@ class Navigation extends BlockBase {
    */
   public function build() {
     $links = [
-      'registration' => Link::fromTextAndUrl(t('Registration'), Url::fromRoute('<current>', [], ['fragment' => 'register-and-report'])),
-      'track_my_activity' => Link::fromTextAndUrl(t('Track My Activities'), Url::fromRoute('<current>', [], ['fragment' => 'register-and-report'])),
-      'leader_board' => Link::fromTextAndUrl(t('Leaderboard'), Url::fromRoute('<current>', [], ['fragment' => 'leaderboard'])),
-      'rules' => Link::fromTextAndUrl(t('Prizes/Rules Details'), Url::fromRoute('<current>', [], ['fragment' => 'rules']))
-        ->toString(),
+      'registration' => Link::fromTextAndUrl(t('Registration'), Url::fromRoute('<current>', [], [
+        'fragment' => 'register-and-report',
+      ])),
+      'track_my_activity' => Link::fromTextAndUrl(t('Track My Activities'), Url::fromRoute('<current>', [], [
+        'fragment' => 'register-and-report',
+      ])),
+      'leader_board' => Link::fromTextAndUrl(t('Leaderboard'), Url::fromRoute('<current>', [], [
+        'fragment' => 'leaderboard',
+      ])),
+      'rules' => Link::fromTextAndUrl(t('Prizes/Rules Details'), Url::fromRoute('<current>', [], [
+        'fragment' => 'rules',
+      ]))->toString(),
     ];
-    /** @var \Drupal\user\SharedTempStore $temp_store */
-    $temp_store = \Drupal::service('user.shared_tempstore')
-      ->get('ymca_retention');
-    $member = $temp_store->get('member');
-
-    if (!empty($member)) {
-      unset($links['registration']);
-    }
+//    /** @var \Drupal\user\SharedTempStore $temp_store */
+//    $temp_store = \Drupal::service('user.shared_tempstore')
+//      ->get('ymca_retention');
+//    $member = $temp_store->get('member');
+//
+//    if (!empty($member)) {
+//      unset($links['registration']);
+//    }
 
     return [
       '#theme' => 'ymca_retention_navigation',
       '#content' => [
-        'back_url' => Url::fromRoute('<front>', [], ['absolute' => TRUE])
-          ->toString(),
+        'back_url' => Url::fromRoute('<front>', [], [
+          'absolute' => TRUE,
+        ])->toString(),
         'links' => $links,
+      ],
+      '#cache' => [
+        'max-age' => 0,
       ],
     ];
   }
