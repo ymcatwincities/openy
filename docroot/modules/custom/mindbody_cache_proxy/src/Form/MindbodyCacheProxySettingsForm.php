a9e80a4afae4d966ad3ac7c60fc06a2b6b69702a
Dmytro Danylevskyi <dmytro@danylevskyi.com>
2016-07-05 12:07:11 +0300

    Implement settings form

--- docroot/modules/custom/mindbody_cache_proxy/src/Form/MindbodyCacheProxySettingsForm.php
+++ docroot/modules/custom/mindbody_cache_proxy/src/Form/MindbodyCacheProxySettingsForm.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\mindbody_cache_proxy\Form;
 
+use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\mindbody_cache_proxy\Entity\MindbodyCache;
@@ -11,7 +12,7 @@ use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 /**
  * Settings form.
  */
-class MindbodyCacheProxySettingsForm extends FormBase implements ContainerAwareInterface {
+class MindbodyCacheProxySettingsForm extends ConfigFormBase implements ContainerAwareInterface {
 
   use ContainerAwareTrait;
 
@@ -28,9 +29,28 @@ class MindbodyCacheProxySettingsForm extends FormBase implements ContainerAwareI
   public function buildForm(array $form, FormStateInterface $form_state) {
     $state = $this->container->get('state');
     $stats = $state->get('mindbody_cache_proxy');
+    $config = $this->config('mindbody_cache_proxy.settings');
+
+    $form['settings'] = [
+      '#title' => $this->t('Settings'),
+      '#type' => 'fieldset',
+    ];
+
+    $form['settings']['calls'] = [
+      '#title' => $this->t('Calls available'),
+      '#type' => 'textfield',
+      '#description' => $this->t('Free calls available within MindBody agreement.'),
+      '#default_value' => !empty($config->get('calls')) ? $config->get('calls') : 1000,
+    ];
+
+    $form['settings']['submit'] = [
+      '#name' => 'submit',
+      '#type' => 'submit',
+      '#value' => $this->t('Save'),
+    ];
 
     $form['stats'] = [
-      '#title' => t('Cache statistics'),
+      '#title' => $this->t('Cache statistics'),
       '#type' => 'fieldset',
     ];
 
@@ -63,7 +83,14 @@ class MindbodyCacheProxySettingsForm extends FormBase implements ContainerAwareI
       '#value' => $this->t('Clear the cache'),
     ];
 
-    return $form;
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['mindbody_cache_proxy.settings'];
   }
 
   /**
@@ -72,14 +99,22 @@ class MindbodyCacheProxySettingsForm extends FormBase implements ContainerAwareI
   public function submitForm(array &$form, FormStateInterface $form_state) {
     // Remove button and internal Form API values from submitted values.
     $form_state->cleanValues();
-
     $triggering_element = $form_state->getTriggeringElement();
+
     switch ($triggering_element['#name']) {
       case 'reset':
         $manager = $this->container->get('mindbody_cache_proxy.manager');
         $manager->resetCache();
         break;
+
+      case 'submit':
+        $this->config('mindbody_cache_proxy.settings')
+          ->set('calls', $form_state->getValue('calls'))
+          ->save();
+        break;
     }
+
+    parent::submitForm($form, $form_state);
   }
 
 }
