b25702d0f8a9fbfb721c36b0eb39549775e41d7a
vlikin <viktor.likin@gmail.com>
2016-07-06 10:25:54 +0300

    YMCA-984: BaseUrl fix.

--- docroot/modules/contrib/rel_to_abs/src/Plugin/Filter/RelToAbs.php
+++ docroot/modules/contrib/rel_to_abs/src/Plugin/Filter/RelToAbs.php
@@ -2,9 +2,12 @@
 
 namespace Drupal\rel_to_abs\Plugin\Filter;
 
+use Drupal\Core\Routing\UrlGeneratorInterface;
+use Drupal\Core\Url;
 use Drupal\filter\FilterProcessResult;
 use Drupal\filter\Plugin\FilterBase;
-use Drupal\Core\Url;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a filter to convert relative paths to absolute URLs.
@@ -15,16 +18,57 @@ use Drupal\Core\Url;
  *   type = Drupal\filter\Plugin\FilterInterface::TYPE_TRANSFORM_REVERSIBLE
  * )
  */
-class RelToAbs extends FilterBase {
+class RelToAbs extends FilterBase implements ContainerFactoryPluginInterface {
+//class RelToAbs extends FilterBase {
+
+
+  /**
+   * @var UrlGeneratorInterface $requestStack
+   */
+  protected $urlGenerator;
+
+  /**
+   * Constructs a \Drupal\editor\Plugin\Filter\EditorFileReference object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Routing\UrlGeneratorInterface $urlGenerator
+   *   An entity manager object.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, UrlGeneratorInterface $urlGenerator) {
+    $this->urlGenerator = $urlGenerator;
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  static public function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('url_generator')
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
   public function process($text, $langcode) {
     $resultText = preg_replace_callback('/(href|background|src)=["\']([\/#][^"\']*)["\']/', function($matches) {
-      $url = preg_replace('/\/{2,}/', '/', $matches[2]);
+      $baseUrl = $this->urlGenerator->getContext()->getBaseUrl();
+      $relativeUrl = $rawUrl = $matches[2];
+      if (!empty($baseUrl) && strpos($rawUrl, $baseUrl) === 0) {
+        $relativeUrl = '/' . substr($rawUrl, strlen($baseUrl));
+      }
+      $relativeUrl = preg_replace('/\/{2,}/', '/', $relativeUrl);
       try {
-        $url = Url::fromUserInput($url)->setAbsolute(true)->toString();
+        $url = Url::fromUserInput($relativeUrl)->setAbsolute(true)->toString();
       }
       catch(\InvalidArgumentException $e) {
         drupal_set_message($e->getMessage(), 'error');
