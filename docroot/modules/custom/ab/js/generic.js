56eb163b3f9e1ad521287fb30e0035e78538b129
podarok <podarok@ua.fm>
2016-07-07 17:45:45 +0300

    Add JS cookie setter

--- docroot/modules/custom/ab/js/generic.js
+++ docroot/modules/custom/ab/js/generic.js
@@ -11,6 +11,10 @@
   Drupal.behaviors.aB = {
     attach: function (context) {
       var cookie = $.cookie('ab');
+      if (cookie !== 'a' && cookie !== 'b') {
+        cookie = Math.round(Math.random()) == 1 ? 'b' : 'a';
+        $.cookie('ab', cookie);
+      }
       if (cookie == 'b') {
         $.each(drupalSettings['ab'], function (index, value) {
           $(context).find(value.selector).once('ab').each(function () {
