57686b9bbb1321f31808188f10145544ed1e1eab
Andrii TIupa <andrii.tiupa@ffw.co>
2016-07-11 12:38:47 +0300

    [YMCA-1023] Add class filter to the Schedule form

--- docroot/modules/custom/ymca_groupex/src/GroupexScheduleFetcher.php
+++ docroot/modules/custom/ymca_groupex/src/GroupexScheduleFetcher.php
@@ -91,27 +91,42 @@ class GroupexScheduleFetcher {
     }
 
     $filter_date = DrupalDateTime::createFromTimestamp($this->parameters['filter_timestamp'], $this->timezone);
+    // Define end date of shown items as 1 week.
+    $end_date = DrupalDateTime::createFromTimestamp(REQUEST_TIME + 86400 * 7, $this->timezone);
 
     // Prepare classes items.
     $items = [];
 
     foreach ($this->processedData as $item) {
+      // If item date more than 1 week in future skip it.
+      $item_date = DrupalDateTime::createFromTimestamp($item->timestamp, $this->timezone);
+      if ($item_date > $end_date) {
+        continue;
+      }
       $class_url_options = $this->parameters;
       $class_url_options['class'] = $item->class_id;
+      $class_url_options['filter_date'] = date('m/d/y', strtotime($item->date));
       $class_url_options['filter_length'] = 'week';
       $class_url_options['groupex_class'] = 'groupex_table_class_individual';
+      $class_url_options['view_mode'] = 'class';
       unset($class_url_options['instructor']);
 
       $instructor_url_options = $this->parameters;
+      $instructor_filter_date = DrupalDateTime::createFromTimestamp(REQUEST_TIME, $this->timezone)->format(GroupexRequestTrait::$dateFilterFormat);
+      $instructor_url_options['filter_date'] = $instructor_filter_date;
       $instructor_url_options['filter_length'] = 'week';
       $instructor_url_options['instructor'] = $item->instructor;
+      $instructor_url_options['class'] = 'any';
       $instructor_url_options['groupex_class'] = 'groupex_table_instructor_individual';
+      unset($instructor_url_options['view_mode']);
 
       $date_url_options = $this->parameters;
+      $date_url_options['filter_date'] = date('m/d/y', strtotime($item->date));
       $date_url_options['filter_length'] = 'day';
       $date_url_options['class'] = 'any';
       $date_url_options['groupex_class'] = 'groupex_table_class';
       unset($date_url_options['instructor']);
+      unset($date_url_options['view_mode']);
 
       $items[$item->id] = [
         '#theme' => isset($this->parameters['groupex_class']) ? $this->parameters['groupex_class'] : 'groupex_class',
@@ -173,8 +188,7 @@ class GroupexScheduleFetcher {
         $schedule['days'] = [];
         foreach ($items as $id => $class) {
           $schedule['days'][$this->enrichedData[$id]->day]['classes'][] = $class;
-          $schedule['days'][$this->enrichedData[$id]->day]['date_link'] = Url::fromRoute('ymca_groupex.all_schedules_search_results', [], array('query' => $date_url_options));
-
+          $schedule['days'][$this->enrichedData[$id]->day]['date_link'] = $class['#class']['date_link'];
         }
         // Pass 'View This Week’s PDF' href if some location selected.
         if (!empty($this->parameters['location'])) {
@@ -451,6 +465,12 @@ class GroupexScheduleFetcher {
       preg_match('/<span class=\"subbed\".*><br>(.*)<\/span>/', $item->address_1, $test);
       if (!empty($test)) {
         $item->address_1 = str_replace($test[0], ' ' . $test[1], $item->address_1);
+      }
+      preg_match('/<span class=\"subbed\".*><br>(.*)<\/span>/', $item->instructor, $test1);
+      if (!empty($test1)) {
+        $test1[1] = str_replace('(sub for ', '', $test1[1]);
+        $test1[1] = str_replace(')', '', $test1[1]);
+        $item->instructor = str_replace($test1[0], '<br><span class="icon icon-loop2"></span><span class="sub">' . $test1[1] . '</span>', $item->instructor);
       }
     }
 
