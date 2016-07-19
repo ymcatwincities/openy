bfed0851770b486edc7e82cc69087c08b16a9e66
Dmytro Danylevskyi <dmytro@danylevskyi.com>
2016-07-19 15:12:04 +0300

    Add logging of status for Fast pusher

--- docroot/modules/custom/personify_mindbody_sync/src/PersonifyMindbodySyncPusherFast.php
+++ docroot/modules/custom/personify_mindbody_sync/src/PersonifyMindbodySyncPusherFast.php
@@ -33,6 +33,19 @@ class PersonifyMindbodySyncPusherFast extends PersonifyMindbodySyncPusherBase {
       return $this;
     }
 
+    if ($debug) {
+      // Let's check only 2 clients for debug.
+      $clients = [];
+      $i = 1;
+      foreach ($this->clientIds as $client_id => $client_data) {
+        if ($i < 3) {
+          $clients[$client_id] = $client_data;
+          $i++;
+        }
+      }
+      $this->clientIds = $clients;
+    }
+
     // Let's push new clients to MindBody.
     $push_clients = array_values($this->clientIds);
     if (!empty($push_clients)) {
@@ -46,6 +59,8 @@ class PersonifyMindbodySyncPusherFast extends PersonifyMindbodySyncPusherBase {
         );
       }
       catch (MindbodyException $e) {
+        $this->updateStatusByClients(array_keys($this->clientIds), $e->getMessage());
+
         $msg = 'Failed to push the clients: %error';
         $this->logger->critical($msg, ['%error' => $e->getMessage()]);
         return $this;
@@ -64,9 +79,16 @@ class PersonifyMindbodySyncPusherFast extends PersonifyMindbodySyncPusherBase {
           /* Note, the data will not be pushed if the client ID was
           overridden for the testing purposes. */
           $this->updateClientData($client->ID, $client);
+
+          // Reset status.
+          $this->updateStatusByClients([$client->ID], '');
         }
       }
       else {
+        // A bunch of clients may failed with different reasons.
+        // The only way is to debug the problem is to look in the system log.
+        $this->updateStatusByClients(array_values($this->clientIds), 'Bulk user update fail. Look in the system log.');
+
         $msg = 'Failed to push the clients: %error';
         $this->logger->critical($msg, ['%error' => serialize($result)]);
         return $this;
