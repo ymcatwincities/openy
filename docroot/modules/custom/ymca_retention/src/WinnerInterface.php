9d840c537296a20a0bf561dac591d6a55e235995
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2016-07-28 14:28:04 +0300

    [YSR-8] Created storage for winners

---
+++ docroot/modules/custom/ymca_retention/src/WinnerInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\ymca_retention;
+
+use Drupal\Core\Entity\ContentEntityInterface;
+
+/**
+ * Provides an interface defining a Winner entity.
+ *
+ * @ingroup ymca_retention
+ */
+interface WinnerInterface extends ContentEntityInterface {
+
+}
