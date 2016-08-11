ec25f891f4bf74dd7ee1daede2240c06966c1c09
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2016-08-11 14:45:38 +0300

    [YSR-8] implemented winners draw logic.

--- docroot/modules/custom/ymca_retention/src/Controller/WinnersController.php
+++ docroot/modules/custom/ymca_retention/src/Controller/WinnersController.php
@@ -57,33 +57,117 @@ class WinnersController extends ControllerBase {
 
     $branch_id = $context['sandbox']['branches'][$context['sandbox']['progress']];
 
-    // Select 12 members from the branch.
-    // TODO: implement selection logic.
-    $member_ids = \Drupal::entityQuery('ymca_retention_member')
+    // Select winner candidates - all with the same or better result as the
+    // 12th place in each track.
+    $tracks = [
+      'total_visits' => 'visits',
+      'activity_track_swimming' => 'swimming',
+      'activity_track_fitness' => 'fitness',
+      'activity_track_groupx' => 'groupex',
+    ];
+    $candidates = [];
+
+    // Count registered members in the branch.
+    $count = \Drupal::entityQueryAggregate('ymca_retention_member')
       ->condition('branch', $branch_id)
       ->condition('is_employee', FALSE)
-      ->range(NULL, 12)
+      ->aggregate('id', 'COUNT')
       ->execute();
-    $members = Member::loadMultiple($member_ids);
+    $limit = min($count[0]['id_count'], 12);
 
-    // Create winners from selected members.
-    $member = current($members);
-    foreach (['visits', 'swimming', 'fitness', 'groupex'] as $track) {
-      foreach ([1, 2, 3] as $place) {
-        /** @var Member $member */
-        if (!$member) {
-          break 2;
+    foreach ($tracks as $field => $track) {
+      // Determine limit-th member.
+      $member_ids = \Drupal::entityQuery('ymca_retention_member')
+        ->condition('branch', $branch_id)
+        ->condition('is_employee', FALSE)
+        ->sort($field, 'DESC')
+        ->range($limit - 1, 1)
+        ->execute();
+      if (empty($member_ids)) {
+        continue;
+      }
+      $member_id = reset($member_ids);
+      $member = Member::load($member_id);
+      $member_ids = \Drupal::entityQuery('ymca_retention_member')
+        ->condition('branch', $branch_id)
+        ->condition('is_employee', FALSE)
+        ->condition($field, $member->get($field)->value, '>=')
+        ->sort($field, 'DESC')
+        ->execute();
+      $members = Member::loadMultiple($member_ids);
+      /** @var Member $member */
+      foreach ($members as $member) {
+        $candidates[$track][$member->getId()] = $member->get($field)->value;
+      }
+    }
+
+    // Draw candidates ranking - preliminary winners.
+    $preliminary_winners = [];
+    foreach ($tracks as $track) {
+      $count = count($candidates[$track]);
+      for ($i = 1; $i <= $count; $i++) {
+        // Select candidates for this place.
+        $candidates_place = [];
+        $cut_value = 0;
+        foreach($candidates[$track] as $id => $value) {
+          if ($value >= $cut_value) {
+            $candidates_place[$id] = $value;
+            $cut_value = $value;
+          }
+          else {
+            break;
+          }
         }
+
+        // Select random winner from candidates for this place.
+        $winner_id = array_rand($candidates_place, 1);
+        $preliminary_winners[$track][] = $winner_id;
+        // Remove winner from candidates.
+        unset($candidates[$track][$winner_id]);
+      }
+    }
+
+    $winners = $preliminary_winners;
+    // Filter preliminary winners so that winner gets only one best possible
+    // reward. First filter by place, then by track.
+    foreach ([0, 1, 2] as $place) {
+      foreach ($tracks as $track) {
+        $id_to_remove = $winners[$track][$place];
+
+        foreach ($tracks as $track_to_filter) {
+          if ($track === $track_to_filter) {
+            continue;
+          }
+
+          $key_to_remove = array_search($id_to_remove, $winners[$track_to_filter]);
+          if ($key_to_remove === FALSE) {
+            continue;
+          }
+
+          unset($winners[$track_to_filter][$key_to_remove]);
+          $winners[$track_to_filter] = array_values($winners[$track_to_filter]);
+        }
+      }
+    }
+
+    // Save winners - create winner entities.
+    foreach ($tracks as $track) {
+      $place = 1;
+      foreach ($winners[$track] as $member_id) {
         $winner = Winner::create([
           'branch' => $branch_id,
-          'member' => $member->getId(),
+          'member' => $member_id,
           'track' => $track,
           'place' => $place,
         ]);
         $winner->save();
-        $context['results'][] = $member->getId();
+        $context['results'][] = $member_id;
 
-        $member = next($members);
+
+        if ($place >= 3) {
+          break;
+        }
+        $place++;
       }
     }
 
