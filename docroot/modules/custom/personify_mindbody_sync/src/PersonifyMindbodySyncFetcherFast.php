2ecf74b2f4ccea81a8a3029ee69ac0dec6b4209c
Dmytro Danylevskyi <dmytro@danylevskyi.com>
2016-08-31 14:24:42 +0300

    [YPTF-116] Remove debug line

--- docroot/modules/custom/personify_mindbody_sync/src/PersonifyMindbodySyncFetcherFast.php
+++ docroot/modules/custom/personify_mindbody_sync/src/PersonifyMindbodySyncFetcherFast.php
@@ -15,7 +15,6 @@ class PersonifyMindbodySyncFetcherFast extends PersonifyMindbodySyncFetcherBase 
   public function fetch() {
     // For testing use: "PersonifyMindbodySyncWrapper::INITIAL_DATE".
     $date = $this->wrapper->timestampToPersonifyDate(REQUEST_TIME - PersonifyMindbodySyncWrapper::DATE_OFFSET);
-    $date = PersonifyMindbodySyncWrapper::INITIAL_DATE;
     $orders = $this->getData($date);
     $this->wrapper->setSourceData($orders);
   }
