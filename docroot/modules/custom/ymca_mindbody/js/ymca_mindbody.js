029ae1ccbeead3975b14c06873e15e46b7534bdb
Andrii TIupa <andrii.tiupa@ffw.co>
2016-09-01 13:48:55 +0300

    [YPTF-150] Display list of products on Healthy Living Page

---
+++ docroot/modules/custom/ymca_mindbody/js/ymca_mindbody.js
@@ -0,0 +1,20 @@
+(function($) {
+
+  Drupal.behaviors.ymca_mindbody = {
+    attach: function (context, settings) {
+      var data = {};
+      $('.mindbody-products-list a').on('click', function() {
+        data.session_length = $(this).data('session_length');
+        data.package = $(this).data('package');
+      });
+      $('#modal').on('shown.bs.modal', function() {
+        $('.personify_location_list a').on('click', function(e) {
+          e.preventDefault();
+          var product_code = $(this).data('id')+ '_PT_' + data.package + '_SESS_' + data.session_length + '_MIN';
+          window.location.href = settings.personify_product_url + settings.products_codes[product_code];
+        });
+      });
+    }
+  };
+
+})(jQuery);
