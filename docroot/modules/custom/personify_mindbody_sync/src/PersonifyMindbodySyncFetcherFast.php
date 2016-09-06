844b35b9f02e1703763564b6c67a56342c939e6b
Alexander Schedrov <alexander.schedrov@gmail.com>
2016-09-06 14:12:00 +0300

    Fix timezone for Personify endpoint

--- docroot/modules/custom/personify_mindbody_sync/src/PersonifyMindbodySyncFetcherFast.php
+++ docroot/modules/custom/personify_mindbody_sync/src/PersonifyMindbodySyncFetcherFast.php
@@ -14,7 +14,7 @@ class PersonifyMindbodySyncFetcherFast extends PersonifyMindbodySyncFetcherBase 
    */
   public function fetch() {
     // For testing use: "PersonifyMindbodySyncWrapper::INITIAL_DATE".
-    $date = $this->wrapper->timestampToPersonifyDate(REQUEST_TIME - PersonifyMindbodySyncWrapper::DATE_OFFSET);
+    $date = $this->wrapper->getCurrentTime();
     $orders = $this->getData($date);
     $this->wrapper->setSourceData($orders);
   }
