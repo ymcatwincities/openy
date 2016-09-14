18c240bfd268de61ff8b2ba281917fd372a30051
podarok <podarok@ua.fm>
2016-09-14 15:41:03 +0300

    Fix YMCA-1154 - old date now should be refreshed automatically

--- docroot/modules/custom/ymca_alters/js/sd.js
+++ docroot/modules/custom/ymca_alters/js/sd.js
@@ -1,20 +1,29 @@
 ;(function ($) {
-  'use strict';
-  Drupal.behaviors.schedules_date = {
-    attach: function(context, settings) {
-      var d = new Date(),
-          out = [];
+    'use strict';
+    Drupal.behaviors.schedules_date = {
+        attach: function (context, settings) {
+            var d = new Date(),
+                out = [];
 
-      out[0] = d.getMonth() + 1;
-      out[1] = d.getDate();
-      out[2] = d.getFullYear().toString().slice(-2);
+            out[0] = d.getMonth() + 1;
+            out[1] = d.getDate();
+            out[2] = d.getFullYear().toString().slice(-2);
 
-      // Add leading zero if needed.
-      if (out[1].toString().length == 1) {
-        out[1] = '0' + out[1].toString();
-      }
+            // Add leading zero if needed.
+            if (out[1].toString().length == 1) {
+                out[1] = '0' + out[1].toString();
+            }
 
-      $('#edit-filter-date-date').val(out.join('/'));
-    }
-  };
+            // @todo check if this it still needed.
+            // $('#edit-filter-date-date').val(out.join('/'));
+            // Set current date and reload a form.
+            var date_element = $('#edit-date-select');
+            var old_d = new Date(date_element.val());
+            if (old_d < new Date()) {
+                date_element.val(out.join('/'));
+                date_element.change();
+            }
+
+        }
+    };
 })(jQuery);
