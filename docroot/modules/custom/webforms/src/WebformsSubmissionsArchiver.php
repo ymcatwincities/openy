a0ca8250d03421384a18217c115f2f0b887ebfec
podarok <podarok@ua.fm>
2016-10-07 14:33:42 +0300

    Add todos, Add service

--- docroot/modules/custom/webforms/src/WebformsSubmissionsArchiver.php
+++ docroot/modules/custom/webforms/src/WebformsSubmissionsArchiver.php
@@ -2,6 +2,10 @@
 
 namespace Drupal\webforms;
 
+use Drupal\contact\Entity\Message;
+use Drupal\Core\Entity\Query\QueryFactory;
+use Drupal\Core\Entity\EntityTypeManager;
+use Drupal\csv_serialization\Encoder\CsvEncoder;
 
 /**
  * Class WebformsSubmissionsArchiver
@@ -10,13 +14,62 @@ namespace Drupal\webforms;
 class WebformsSubmissionsArchiver {
 
   /**
+   * Query Factory to work with.
+   *
+   * @var \Drupal\Core\Entity\Query\QueryFactory
+   */
+  private $queryFactory;
+
+  /**
+   * Entity Type Manager to work with.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManager
+   */
+  private $entityTypeManager;
+
+  /**
+   * Encoder for converting entities into CSV file.
+   *
+   * @var \Drupal\csv_serialization\Encoder\CsvEncoder
+   */
+  private $csvEncoder;
+
+  public function __construct(QueryFactory $queryFactory, EntityTypeManager $entityTypeManager, CsvEncoder $csvEncoder) {
+    $this->queryFactory = $queryFactory;
+    $this->entityTypeManager = $entityTypeManager;
+    $this->csvEncoder = $csvEncoder;
+  }
+
+  /**
    * Archiving loop, should be run from cron.
    */
   public function archive() {
-    // @todo Get list of contact_storage entities.
-    // @todo Loop through all of them to find the data, older than a month.
+    // Get first from list of contact_storage entities.
+    $message_ids = $this->queryFactory->get('contact_message')
+      ->condition('created', strtotime('last day of previous month'), '<=')
+      ->sort('created', 'ASC')
+      ->range(0,1)
+      ->execute();
+    // Loop through all of them to find the data, older than a month.
+    $entities = $this->entityTypeManager->getStorage('contact_message')->loadMultiple($message_ids);
+    /** @var Message $entity */
+    $entity = array_shift($entities);
+    $form_name = $entity->bundle();
+    $created = $entity->created->get(0)->getValue()['value'];
+    $end = strtotime('last day of this month', $created);
+    $start = strtotime('first day of this month', $created);
+    $month_ids = $this->queryFactory->get('contact_message')
+      ->condition('created', $end, '<=')
+      ->condition('created', $start, '>=')
+      ->condition('contact_form', $form_name)
+      ->sort('created', 'ASC')
+      ->execute();
+
     // @todo Archive a single month data, store to local Archive entity.
+    $month_entities = $this->entityTypeManager->getStorage('contact_message')->loadMultiple($month_ids);
+    $csv = $this->csvEncoder->encode($month_entities, 'csv');
     // @todo Check if the file is greater than a zero, remove archived data.
     // @todo finish a loop.
+    $i = 0;
   }
 }
