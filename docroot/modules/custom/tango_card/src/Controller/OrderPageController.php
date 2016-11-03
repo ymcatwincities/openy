c10852ad785bc9e7a9505e045be9b3c6d3f5befe
Tiago Bember <tiago.bember@ffwagency.com>
2016-11-03 10:31:41 -0400

    [YMCA-1198] Adding Tango Card integration module.

---
+++ docroot/modules/custom/tango_card/src/Controller/OrderPageController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Drupal\tango_card\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Datetime\DateFormatter;
+use Drupal\Core\Url;
+use Drupal\tango_card\TangoCardWrapper;
+use Drupal\tango_card\Entity\Account;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Provides Tango Card order page display.
+ */
+class OrderPageController extends ControllerBase {
+
+  /**
+   * The Tango Card wrapper.
+   *
+   * @var \Drupal\tango_card\TangoCardWrapper
+   */
+  protected $tangoCardWrapper;
+
+  /**
+   * Date formatter.
+   *
+   * @var \Drupal\Core\Datetime\DateFormatter
+   */
+  protected $dateFormatter;
+
+  /**
+   * Constructs the OrdersPageController object.
+   *
+   * @param \Drupal\tango_card\TangoCardWrapper $tango_card_wrapper
+   *   The Tango Card wrapper.
+   * @param Drupal\Core\Datetime\DateFormatter $date_formatter
+   *   The date formatter.
+   */
+  public function __construct(TangoCardWrapper $tango_card_wrapper, DateFormatter $date_formatter) {
+    $this->tangoCardWrapper = $tango_card_wrapper;
+    $this->dateFormatter = $date_formatter;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('tango_card.tango_card_wrapper'),
+      $container->get('date.formatter')
+    );
+  }
+
+  /**
+   * Return Tango Card order details page.
+   *
+   * @return array
+   *   A renderable array.
+   */
+  public function pageView(Request $request, Account $tango_card_account, $order_id) {
+    $build = array();
+    $this->tangoCardWrapper->setAccount($tango_card_account);
+
+    try {
+      $order = $this->tangoCardWrapper->getOrderInfo($order_id);
+      $success = $order !== FALSE;
+    }
+    catch (Exception $e) {
+      $success = FALSE;
+    }
+
+    if (!$success) {
+      drupal_set_message($this->t('An error occurred. Please try again later or contact support.'), 'error');
+      return $build;
+    }
+
+    $order = (array) $order + (array) $order->reward;
+
+    $fields = array(
+      'order_id' => 'ID',
+      'sku' => 'Name',
+      'delivered_at' => 'Date',
+      'amount' => 'amount',
+      'reward_from' => 'From email',
+      'reward_subject' => 'Email subject',
+      'reward_message' => 'Email message',
+      'token' => 'Token',
+      'number' => 'Number',
+      'pin' => 'Pin',
+    );
+
+    foreach ($fields as $field => $title) {
+      $build[$field] = array(
+        '#type' => 'item',
+        '#title' => $this->t($title),
+        '#markup' => empty($order[$field]) ? '-' : $order[$field],
+      );
+    }
+
+    $reward = $this->tangoCardWrapper->getRewardInfo($order['sku']);
+
+    $build['sku']['#markup'] = $reward->description . ' (' . $build['sku']['#markup'] . ')';
+    $build['delivered_at']['#markup'] = $this->dateFormatter->format(strtotime($build['delivered_at']['#markup']));
+    $build['amount']['#markup'] = '$' . number_format($build['amount']['#markup'] / 100, 2);
+
+    return $build;
+  }
+
+}
---
+++ docroot/modules/custom/tango_card/src/Entity/Controller/AccountListBuilder.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Drupal\tango_card\Entity\Controller;
+
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityListBuilder;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+use Drupal\tango_card\TangoCardWrapper;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a list controller for tango_card_campaign entity.
+ *
+ * @ingroup tango_card_account
+ */
+class AccountListBuilder extends EntityListBuilder {
+
+  /**
+   * The Tango Card wrapper.
+   *
+   * @var \Drupal\tango_card\TangoCardWrapper
+   */
+  protected $tangoCardWrapper;
+
+  /**
+   * Construct AccountListBuilder object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
+   *   The entity type definition.
+   * @param \Drupal\Core\Entity\EntityStorageInterface $storage
+   *   The entity storage class.
+   * @param \Drupal\tango_card\TangoCardWrapper $tango_card_wrapper
+   *   The Tango Card wrapper.
+   */
+  public function __construct(EntityTypeInterface $entity_type, EntityStorageInterface $storage, TangoCardWrapper $tango_card_wrapper) {
+    parent::__construct($entity_type, $storage);
+    $this->tangoCardWrapper = $tango_card_wrapper;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function createInstance(ContainerInterface $container, EntityTypeInterface $entity_type) {
+    return new static(
+      $entity_type,
+      $container->get('entity.manager')->getStorage($entity_type->id()),
+      $container->get('tango_card.tango_card_wrapper')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildHeader() {
+    $header = array(
+      'id' => $this->t('ID'),
+      'remote_id' => $this->t('Remote ID'),
+      'mail' => $this->t('Email'),
+      'balance' => $this->t('Balance'),
+      'cc_number' => $this->t('CC Number'),
+    );
+    $header += parent::buildHeader();
+    $header['orders'] = '';
+
+    return $header;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildRow(EntityInterface $entity) {
+    $this->tangoCardWrapper->setAccount($entity);
+
+    $balance = $this->tangoCardWrapper->getAccountBalance();
+    if ($balance === FALSE) {
+      return array();
+    }
+
+    $row = array(
+      'id' => $entity->id(),
+      'remote_id' => $entity->label(),
+      'mail' => $entity->mail->value,
+      'balance' => '$' . number_format($balance / 100, 2),
+      'cc_number' => '************' . $entity->cc_number->value,
+    );
+
+    $row += parent::buildRow($entity);
+    unset($row['operations']['data']['#links']['clone']);
+
+    $row['orders'] = new Link($this->t('see orders'), Url::fromRoute('tango_card.orders', array('tango_card_account' => $entity->id())));
+
+    return $row;
+  }
+
+}
