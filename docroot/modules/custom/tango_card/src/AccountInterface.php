8ad9f7392c05d4fcd3c049ad10d21fcff57281c2
Tiago Bember <tiago.bember@ffwagency.com>
2016-11-03 10:31:41 -0400

    [YMCA-1198] Adding Tango Card integration module.

---
+++ docroot/modules/custom/tango_card/src/AccountInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\tango_card;
+
+use Drupal\Core\Entity\ContentEntityInterface;
+
+/**
+ * Provides an interface defining a Tango Card account entity.
+ *
+ * @ingroup tango_card_account
+ */
+interface AccountInterface extends ContentEntityInterface {
+
+}
