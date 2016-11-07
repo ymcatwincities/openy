4a3df86813dfe793fe82d8151c616c839e427a01
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2016-11-07 13:12:30 +0300

    [YMCA-1215] Added angular application initialization library.

---
+++ docroot/modules/custom/ymca_retention/js/app.js
@@ -0,0 +1,9 @@
+(function($) {
+
+  Drupal.behaviors.ymca_retention_app = {};
+  Drupal.behaviors.ymca_retention_app.attach = function (context, settings) {
+    Drupal.ymca_retention = Drupal.ymca_retention || {};
+    Drupal.ymca_retention.app = angular.module('Retention', []);
+  };
+
+})(jQuery);
