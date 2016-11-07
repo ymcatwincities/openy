d7fabbdcc8f90270c8264f9442fe3f1924d11cd4
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2016-11-08 01:22:27 +0300

    [YMCA-1215] Added modal theming for register and login forms.

--- docroot/modules/custom/ymca_retention/js/user-menu.js
+++ docroot/modules/custom/ymca_retention/js/user-menu.js
@@ -3,7 +3,35 @@
   Drupal.behaviors.ymca_retention_user_menu = {};
   Drupal.behaviors.ymca_retention_user_menu.attach = function (context, settings) {
     // Drupal.ymca_retention.app;
-    console.log('test!');
+
+    $('#ymca-retention-modal', context).once('ymca-retention-user-menu')
+      .on('show.bs.modal', function (event) {
+        var $button = $(event.relatedTarget),
+          type = $button.data('type'),
+          $modal = $(this),
+          $modal_body = $modal.find('.modal-body');
+
+        // Assign modal title.
+        $modal.find('.modal-title').text($button.text());
+
+        // Check for already existing content and save it.
+        if ($.trim($modal_body.html())) {
+          $modal_body.find('.ysr-user-menu__form').appendTo($('.ysr-user-menu__forms'));
+        }
+
+        // Add requested form to the modal body.
+        if (type === 'register') {
+          $('#ymca-retention-user-menu-register-form').appendTo($modal_body);
+        }
+        else if (type === 'login') {
+          $('#ymca-retention-user-menu-login-form').appendTo($modal_body);
+        }
+      }).
+      on('hidden.bs.modal', function (event) {
+        var $modal = $(this);
+        // Save the form back.
+        $modal.find('.modal-body .ysr-user-menu__form').appendTo($('.ysr-user-menu__forms'));
+      });
   };
 
 })(jQuery);
