1649e501e3d0c047aa7c340f2972b6ba8dd23226
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2016-11-07 13:12:30 +0300

    [YMCA-1215] Added angular application initialization library.

--- docroot/modules/custom/ymca_retention/js/tabs-selector.js
+++ docroot/modules/custom/ymca_retention/js/tabs-selector.js
@@ -8,8 +8,8 @@
    * @prop {Drupal~behaviorAttach} attach
    *   Attaches the behavior.
    */
-  Drupal.behaviors.tabs_selector = {};
-  Drupal.behaviors.tabs_selector.attach = function (context, settings) {
+  Drupal.behaviors.ymca_retention_tabs_selector = {};
+  Drupal.behaviors.ymca_retention_tabs_selector.attach = function (context, settings) {
     $('.yfr-tabs', context)
       .once('tab-collapse')
       .tabCollapse({
