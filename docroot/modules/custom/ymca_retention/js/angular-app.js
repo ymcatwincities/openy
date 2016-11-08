f1ea7b328cbdc1938e3e8b166f43e04014c7d059
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2016-11-08 22:06:01 +0300

    [YMCA-1215] Added service to communicate with backend.

--- docroot/modules/custom/ymca_retention/js/angular-app.js
+++ docroot/modules/custom/ymca_retention/js/angular-app.js
@@ -5,32 +5,41 @@
     Drupal.ymca_retention = Drupal.ymca_retention || {};
     Drupal.ymca_retention.angular_app = Drupal.ymca_retention.angular_app || angular.module('Retention', ['ngCookies']);
 
-    Drupal.ymca_retention.angular_app.controller('RetentionController', function ($scope, $log, $cookies, $timeout, $interval) {
+    Drupal.ymca_retention.angular_app.controller('RetentionController', function ($scope, $log, $cookies, $timeout, $interval, fetcher) {
       $scope.$watch(function () {
         return $cookies.get('Drupal.visitor.ymca_retention_member');
       }, angular.bind(this, function (newVal, oldVal) {
         this.getUserData(newVal);
       }));
-      // Force to check cookie value every second.
+      // Force to check cookie value.
       $interval(function() {
         $cookies.get('Drupal.visitor.ymca_retention_member');
-      }, 1000);
+      }, 500);
 
-      this.getUserData = function(value) {
-        if (typeof value != 'undefined') {
-          this.user = {
-            firstName: 'Andrew'
-          };
-        }
-        else {
-          this.user = '';
-        }
+      this.getUserData = function(id) {
+        this.user = fetcher.getUserData(id);
       };
 
       this.cookieRemove = function() {
         $cookies.remove('Drupal.visitor.ymca_retention_member');
       };
 
+    });
+
+    // Service to communicate with backend.
+    Drupal.ymca_retention.angular_app.factory('fetcher', function() {
+      return {
+        getUserData: function(id) {
+          if (typeof id != 'undefined') {
+            return {
+              firstName: 'Andrew'
+            };
+          }
+          else {
+            return '';
+          }
+        }
+      };
     });
   };
 
