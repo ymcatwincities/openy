623c62bed6d3b6b7bca91ccf5a59707861277f45
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2016-11-08 18:51:40 +0300

    [YMCA-1215] Started Angular application.

--- docroot/modules/custom/ymca_retention/js/angular-app.js
+++ docroot/modules/custom/ymca_retention/js/angular-app.js
@@ -1,9 +1,15 @@
 (function($) {
 
-  Drupal.behaviors.ymca_retention_app = {};
-  Drupal.behaviors.ymca_retention_app.attach = function (context, settings) {
+  Drupal.behaviors.ymca_retention_angular_app = {};
+  Drupal.behaviors.ymca_retention_angular_app.attach = function (context, settings) {
     Drupal.ymca_retention = Drupal.ymca_retention || {};
-    Drupal.ymca_retention.app = Drupal.ymca_retention.app || angular.module('Retention', []);
+    Drupal.ymca_retention.angular_app = Drupal.ymca_retention.angular_app || angular.module('Retention', []);
+
+    Drupal.ymca_retention.angular_app.controller('RetentionController', function () {
+      this.user = {
+        firstName: 'Andrew'
+      };
+    });
   };
 
 })(jQuery);
