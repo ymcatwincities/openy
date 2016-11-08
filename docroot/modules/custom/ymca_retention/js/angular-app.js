220910c14d27ecf1057321e6ee63776bfb33ea28
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2016-11-08 20:47:10 +0300

    [YMCA-1215] Added cookie checking by angular.

--- docroot/modules/custom/ymca_retention/js/angular-app.js
+++ docroot/modules/custom/ymca_retention/js/angular-app.js
@@ -3,12 +3,34 @@
   Drupal.behaviors.ymca_retention_angular_app = {};
   Drupal.behaviors.ymca_retention_angular_app.attach = function (context, settings) {
     Drupal.ymca_retention = Drupal.ymca_retention || {};
-    Drupal.ymca_retention.angular_app = Drupal.ymca_retention.angular_app || angular.module('Retention', []);
+    Drupal.ymca_retention.angular_app = Drupal.ymca_retention.angular_app || angular.module('Retention', ['ngCookies']);
 
-    Drupal.ymca_retention.angular_app.controller('RetentionController', function () {
-      this.user = {
-        firstName: 'Andrew'
+    Drupal.ymca_retention.angular_app.controller('RetentionController', function ($scope, $log, $cookies, $timeout, $interval) {
+      $scope.$watch(function () {
+        return $cookies.get('Drupal.visitor.ymca_retention_member');
+      }, angular.bind(this, function (newVal, oldVal) {
+        this.getUserData(newVal);
+      }));
+      // Force to check cookie value every second.
+      $interval(function() {
+        $cookies.get('Drupal.visitor.ymca_retention_member');
+      }, 1000);
+
+      this.getUserData = function(value) {
+        if (typeof value != 'undefined') {
+          this.user = {
+            firstName: 'Andrew'
+          };
+        }
+        else {
+          this.user = '';
+        }
       };
+
+      this.cookieRemove = function() {
+        $cookies.remove('Drupal.visitor.ymca_retention_member');
+      };
+
     });
   };
 
