85fdb0bbf730eb83f469319d60323be8b7bbdd84
Dmytro Danylevskyi <dmytro@danylevskyi.com>
2016-11-08 18:51:48 +0200

    Add the first PHPUnit test here

---
+++ docroot/modules/custom/personify_mindbody_sync/tests/src/Unit/PusherTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\Tests\personify_mindbody_sync\Unit;
+
+/**
+ * Wrapper test.
+ *
+ * @group ymca
+ */
+class PusherTest extends \PHPUnit_Framework_TestCase {
+
+  /**
+   * Backup globals.
+   *
+   * @see https://github.com/sebastianbergmann/phpunit/issues/451
+   * @see https://github.com/silverstripe/silverstripe-behat-extension/commit/7ef575c961ef8a42646b9a30d5a37ad125290dce
+   *
+   * @var bool
+   */
+  protected $backupGlobals = FALSE;
+
+  /**
+   * Test sending notifications.
+   */
+  public function testSendNotification() {
+    $pusher = \Drupal::service('personify_mindbody_sync.pusher_fast');
+    $wrapper = \Drupal::service('personify_mindbody_sync.wrapper');
+
+    $order = $wrapper->mockOrder();
+    $pusher->sendNotification($order, 10);
+  }
+
+}
