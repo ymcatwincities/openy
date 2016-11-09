d758e961f0033e766a5b5dbc81ec9827b4f04293
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2016-11-09 19:28:02 +0300

    [YMCA-1215] Created user menu controller.

--- docroot/modules/custom/ymca_retention/js/angular-app.js
+++ docroot/modules/custom/ymca_retention/js/angular-app.js
@@ -2,55 +2,22 @@
 
   Drupal.behaviors.ymca_retention_angular_app = {};
   Drupal.behaviors.ymca_retention_angular_app.attach = function (context, settings) {
-    Drupal.ymca_retention = Drupal.ymca_retention || {};
-    Drupal.ymca_retention.angular_app = Drupal.ymca_retention.angular_app || angular.module('Retention', ['ngCookies']);
+    if ($('body').hasClass('ymca-retention-angular-app-processed')) {
+      return;
+    }
+    $('body').addClass('ymca-retention-angular-app-processed');
 
-    Drupal.ymca_retention.angular_app.controller('RetentionController', function ($scope, $log, $cookies, $timeout, $interval, fetcher) {
-      $scope.$watch(function () {
-        return $cookies.get('Drupal.visitor.ymca_retention_member');
-      }, angular.bind(this, function (newVal, oldVal) {
-        this.getMemberData(newVal);
-      }));
+    Drupal.ymca_retention = Drupal.ymca_retention || {};
+    Drupal.ymca_retention.angular_app = Drupal.ymca_retention.angular_app || angular.module('Retention', ['ngCookies', 'ajoslin.promise-tracker']);
+
+    Drupal.ymca_retention.angular_app.controller('RetentionController', function ($cookies, $interval) {
       // Force to check cookie value.
       $interval(function() {
         $cookies.get('Drupal.visitor.ymca_retention_member');
       }, 500);
 
-      this.getMemberData = angular.bind(this, function(id) {
-        fetcher.getMemberData(id).then(angular.bind(this, function(data) {
-          this.member = data;
-        }));
-      });
-
-      this.cookieRemove = function() {
+      this.memberCookieRemove = function() {
         $cookies.remove('Drupal.visitor.ymca_retention_member', { path: '/' });
-      };
-
-    });
-
-    // Service to communicate with backend.
-    Drupal.ymca_retention.angular_app.factory('fetcher', function($http, $q) {
-      function getMemberData(id) {
-        var deferred = $q.defer();
-        if (typeof id === 'undefined') {
-          deferred.resolve(null);
-        }
-        else {
-          $http.get(settings.ymca_retention.user_menu.member_url).then(function(response) {
-            if ($.isEmptyObject(response.data)) {
-              deferred.resolve(null);
-              return;
-            }
-
-            deferred.resolve(response.data);
-          });
-        }
-
-        return deferred.promise;
-      }
-
-      return {
-        getMemberData: getMemberData
       };
     });
   };
