6b8b6dbf41335c571ab8fd3b9d054f944e54726a
Dmytro Danylevskyi <dmytro@danylevskyi.com>
2016-11-09 16:29:50 +0200

    Add call magic method

--- src/DaxkoClientInterface.php
+++ src/DaxkoClientInterface.php
@@ -9,19 +9,4 @@ namespace Drupal\daxko;
  */
 interface DaxkoClientInterface {
 
-  /**
-   * Wrapper for 'request' method.
-   *
-   * @param string $method
-   *   Method.
-   * @param string $uri
-   *   URI.
-   * @param array $parameters
-   *   Parameters.
-   *
-   * @return mixed
-   *   Data.
-   */
-  public function makeRequest($method, $uri, array $parameters);
-
 }
