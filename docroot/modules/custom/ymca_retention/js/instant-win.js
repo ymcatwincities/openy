a46b4b0908822e6aa3371ed150832507890e9fbf
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2016-11-11 13:08:29 +0300

    [YMCa-1196] Use member chances JSON and display message with chances.

--- docroot/modules/custom/ymca_retention/js/instant-win.js
+++ docroot/modules/custom/ymca_retention/js/instant-win.js
@@ -8,7 +8,22 @@
     $('body').addClass('ymca-retention-instant-win-processed');
 
 
+    Drupal.ymca_retention.angular_app.controller('InstantWinController', function ($scope, $cookies, promiseTracker, courier) {
+      // Initiate the promise tracker to track submissions.
+      $scope.progress = promiseTracker();
 
+      // Watch cookie value and update member chances data on change.
+      $scope.$watch(function () {
+        return $cookies.get('Drupal.visitor.ymca_retention_member');
+      }, function (newVal, oldVal) {
+        $scope.getMemberChances(newVal);
+      });
+      $scope.getMemberChances = function(id) {
+        courier.getMemberChances(id).then(function(data) {
+          $scope.member_chances = data;
+        });
+      };
+    });
   };
 
 })(jQuery);
