5f14b281af69fc1419b166ad31a4e91e1e456986
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2016-11-12 23:45:08 +0300

    [YMC-1215] Started implementing angularjs logic for progress report tab.

--- docroot/modules/custom/ymca_retention/js/progress.js
+++ docroot/modules/custom/ymca_retention/js/progress.js
@@ -1,7 +1,7 @@
 (function ($) {
 
-  Drupal.behaviors.ymca_retention_activity = {};
-  Drupal.behaviors.ymca_retention_activity.attach = function (context, settings) {
+  Drupal.behaviors.ymca_retention_progress = {};
+  Drupal.behaviors.ymca_retention_progress.attach = function (context, settings) {
     if ($('body').hasClass('ymca-retention-progress-processed')) {
       return;
     }
@@ -11,6 +11,24 @@
       var self = this;
       // Shared information.
       self.storage = storage;
+
+      self.activityGroupClass = function (index) {
+        var classes = [];
+        if (self.storage.activity_groups[index].name === 'Swim') {
+          classes.push('item-activity-type__a');
+        }
+        if (self.storage.activity_groups[index].name === 'Fitness') {
+          classes.push('item-activity-type__b');
+        }
+        if (self.storage.activity_groups[index].name === 'Free Group X') {
+          classes.push('item-activity-type__c');
+        }
+        if (self.storage.activity_groups[index].name === 'Community') {
+          classes.push('item-activity-type__d');
+        }
+
+        return classes.join(' ');
+      };
     });
   };
 
