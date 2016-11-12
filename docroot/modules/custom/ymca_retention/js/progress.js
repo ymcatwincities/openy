6c8bc8b76acb985dca91cc35fb7570f86e822197
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2016-11-13 02:32:41 +0300

    [YMCA-1215] Added activities counting to progress report.

--- docroot/modules/custom/ymca_retention/js/progress.js
+++ docroot/modules/custom/ymca_retention/js/progress.js
@@ -12,22 +12,49 @@
       // Shared information.
       self.storage = storage;
 
-      self.activityGroupClass = function (index) {
+      self.dateClass = function (date) {
         var classes = [];
-        if (self.storage.activity_groups[index].name === 'Swim') {
+        if (date.future) {
+          classes.push('progress-row__disabled');
+        }
+        return classes.join(' ');
+      };
+
+      self.activityGroupClass = function (timestamp, activity_group) {
+        var classes = [];
+        if (activity_group.name === 'Swim') {
           classes.push('item-activity-type__a');
         }
-        if (self.storage.activity_groups[index].name === 'Fitness') {
+        if (activity_group.name === 'Fitness') {
           classes.push('item-activity-type__b');
         }
-        if (self.storage.activity_groups[index].name === 'Free Group X') {
+        if (activity_group.name === 'Free Group X') {
           classes.push('item-activity-type__c');
         }
-        if (self.storage.activity_groups[index].name === 'Community') {
+        if (activity_group.name === 'Community') {
           classes.push('item-activity-type__d');
         }
 
+        if (self.activitiesCount(timestamp, activity_group)) {
+          classes.push('active');
+        }
+
         return classes.join(' ');
+      };
+
+      self.activitiesCount = function(timestamp, activity_group) {
+        if (typeof self.storage.member_activities === 'undefined') {
+          return 0;
+        }
+
+        var count = 0;
+        for (var activity in activity_group.activities) {
+          if (self.storage.member_activities[timestamp][activity_group.activities[activity].id]) {
+            count++;
+          }
+        }
+
+        return count;
       };
     });
   };
