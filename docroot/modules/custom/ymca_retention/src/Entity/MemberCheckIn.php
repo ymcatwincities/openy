f0a8750918858a711c9374bb5bd8848ede91ca3a
Dmitry Drozdik <dmitriydrozdik@gmail.com>
2016-11-14 19:56:44 +0300

    [YMCA-1223] Update visits per a list of members

---
+++ docroot/modules/custom/ymca_retention/src/Entity/MemberCheckIn.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Drupal\ymca_retention\Entity;
+
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\EntityTypeInterface;
+
+use Drupal\ymca_retention\MemberCheckInInterface;
+
+/**
+ * Defines the Member Chance entity.
+ *
+ * @ingroup ymca_retention
+ *
+ * @ContentEntityType(
+ *   id = "ymca_retention_member_checkin",
+ *   label = @Translation("Member Check-in entity"),
+ *   handlers = {
+ *     "views_data" = "Drupal\views\EntityViewsData",
+ *   },
+ *   base_table = "ymca_retention_member_checkin",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "label" = "id",
+ *   },
+ * )
+ */
+class MemberCheckIn extends ContentEntityBase implements MemberCheckInInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+
+    // Standard field, used as unique if primary index.
+    $fields['id'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('ID'))
+      ->setDescription(t('The ID of the Member Check-in entity.'))
+      ->setReadOnly(TRUE);
+
+    $fields['created'] = BaseFieldDefinition::create('created')
+      ->setLabel(t('Created'))
+      ->setDescription(t('The time of the day when check-in was logged.'));
+
+    $fields['member'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Member ID'))
+      ->setDescription(t('The member ID.'))
+      ->setSettings([
+        'target_type' => 'ymca_retention_member',
+        'default_value' => 0,
+      ]);
+
+    $fields['checkin'] = BaseFieldDefinition::create('boolean')
+      ->setLabel(t('Status of check-in for the day.'))
+      ->setDefaultValue(FALSE)
+      ->setDisplayOptions('view', [
+        'label' => 'above',
+        'type' => 'boolean',
+        'weight' => -1,
+      ]);
+
+    return $fields;
+  }
+
+}
---
+++ docroot/modules/custom/ymca_retention/src/MemberCheckInInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\ymca_retention;
+
+use Drupal\Core\Entity\ContentEntityInterface;
+
+/**
+ * Provides an interface defining a Member Check-in entity.
+ *
+ * @ingroup ymca_retention
+ */
+interface MemberCheckInInterface extends ContentEntityInterface {
+
+}
