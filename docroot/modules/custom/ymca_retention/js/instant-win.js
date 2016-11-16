7b6575ca59b1c5cf822fa56433f491bb2151aadd
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2016-11-16 18:50:43 +0300

    [YMCA-1196] Instant win history.

--- docroot/modules/custom/ymca_retention/js/instant-win.js
+++ docroot/modules/custom/ymca_retention/js/instant-win.js
@@ -8,7 +8,7 @@
     $('body').addClass('ymca-retention-instant-win-processed');
 
 
-    Drupal.ymca_retention.angular_app.controller('InstantWinController', function ($timeout, $sce, storage) {
+    Drupal.ymca_retention.angular_app.controller('InstantWinController', function ($filter, $timeout, $sce, storage) {
       var self = this;
       // Shared information.
       self.storage = storage;
@@ -44,6 +44,34 @@
             self.storage.state = 'result.win';
           }, 3000);
         });
+      };
+
+      self.cardsWonStatus = function() {
+        var message = $sce.trustAsHtml('<p>' + Drupal.t("You didn't win any cards yet.") + '</p>');
+        if (typeof self.storage.member_chances === 'undefined' || !self.storage.member_chances) {
+          return message;
+        }
+
+        var count = $filter('filter')(self.storage.member_chances, {'played': '!0', 'winner': '1'}, true).length;
+        if (!count) {
+          return message;
+        }
+
+        message = $sce.trustAsHtml('<p>' + Drupal.formatPlural(
+            count,
+            'You have already won 1 card so far.',
+            'You have already won @count cards so far.'
+          ) + '</p>' + '<p>Please, check your emails for further instructions.</p>');
+
+        return message;
+      };
+
+      self.gameHistoryMessageClass = function(chance) {
+        var classes = [];
+        if (chance.winner) {
+          classes.push('active');
+        }
+        return classes.join(' ');
       };
     });
   };
