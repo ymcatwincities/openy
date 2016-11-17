41a9135ccd36824b0847a59b2633413fbaeea29c
Tiago Bember <tiago.bember@ffwagency.com>
2016-11-16 19:42:39 -0500

    [YMCA-1337] Display login popup when accessing private tabs.

--- docroot/modules/custom/ymca_retention/js/ajax-commands.js
+++ docroot/modules/custom/ymca_retention/js/ajax-commands.js
@@ -5,4 +5,21 @@
   Drupal.AjaxCommands.prototype.ymcaRetentionModalHide = function(ajax, response, status) {
     $('#ymca-retention-modal').modal('hide');
   };
+  Drupal.AjaxCommands.prototype.ymcaRetentionSetTab = function(ajax, response, status) {
+    var tab_id = response.arguments.tabId;
+
+    if ($('.yfr-tabs').is(':visible')) {
+      var $link = $('.yfr-tabs a[href="#' + tab_id + '"]');
+
+      // Displaying selected tab after login.
+      $link.tab('show');
+      $link.parent().addClass('active');
+    }
+    else {
+      // Expanding selected accordion item after login.
+      $('.yfr-accordion a[href="#' + tab_id + '-collapse"]').removeClass('collapsed');
+      $('.yfr-accordion #' + tab_id + '-collapse').addClass('in');
+    }
+
+  };
 })(jQuery, Drupal);
