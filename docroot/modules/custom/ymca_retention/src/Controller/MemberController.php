d040dc9f8874e218b75b338d7d79b79bac5616a0
Dmitry Drozdik <dmitriydrozdik@gmail.com>
2016-11-17 17:32:36 +0300

    [YMCA-1338] Admin page for prizes

--- docroot/modules/custom/ymca_retention/src/Controller/MemberController.php
+++ docroot/modules/custom/ymca_retention/src/Controller/MemberController.php
@@ -33,8 +33,7 @@ class MemberController extends ControllerBase {
       'firstName' => $member->getFirstName(),
     ];
 
-    $response = new JsonResponse($member_values);
-    return $response;
+    return new JsonResponse($member_values);
   }
 
   /**
@@ -72,8 +71,7 @@ class MemberController extends ControllerBase {
     $service = \Drupal::service('ymca_retention.activity_manager');
     $member_activities = $service->getMemberActivitiesModel();
 
-    $response = new JsonResponse($member_activities);
-    return $response;
+    return new JsonResponse($member_activities);
   }
 
   /**
@@ -110,8 +108,9 @@ class MemberController extends ControllerBase {
       }
     }
 
-    $storage = \Drupal::entityTypeManager()->getStorage('ymca_retention_member_chance');
-    $chances = $storage->loadByProperties(['member' => $member_id]);
+    $chances = \Drupal::entityTypeManager()
+      ->getStorage('ymca_retention_member_chance')
+      ->loadByProperties(['member' => $member_id]);
 
     $chances_values = [];
     foreach ($chances as $chance) {
@@ -123,8 +122,7 @@ class MemberController extends ControllerBase {
       ];
     }
 
-    $response = new JsonResponse($chances_values);
-    return $response;
+    return new JsonResponse($chances_values);
   }
 
   /**
@@ -145,9 +143,10 @@ class MemberController extends ControllerBase {
     $checkin_ids = \Drupal::entityQuery('ymca_retention_member_checkin')
       ->condition('member', $member_id)
       ->execute();
-    $storage = \Drupal::entityTypeManager()
-      ->getStorage('ymca_retention_member_checkin');
-    $checkins = $storage->loadMultiple($checkin_ids);
+
+    $checkins = \Drupal::entityTypeManager()
+      ->getStorage('ymca_retention_member_checkin')
+      ->loadMultiple($checkin_ids);
 
     $checkin_values = [];
     foreach ($checkins as $checkin) {
