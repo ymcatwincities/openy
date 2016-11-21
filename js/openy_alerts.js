62e6208786e9b4d2237bfccb1d04817b33f3f6aa
Andriy Prokopenko <anprok@users.noreply.github.com>
2016-11-21 15:59:59 +0200

    [YGH-63] Add js logic and theming for alerts

--- js/openy_alerts.js
+++ js/openy_alerts.js
@@ -1,1 +1,59 @@
-// @todo Place the code from Seattle site.
+(function ($) {
+    "use strict";
+
+    Drupal.behaviors.alert_dismiss = {
+        attach: function (context, settings) {
+            $('.header-alerts-list', context).once('header-alert-list-arrows').each(function () {
+                $('.slick__arrow', this).wrap('<div class="container"></div>');
+            });
+
+            var dismissed = Drupal.behaviors.alert_dismiss.getDismissed();
+            $('.site-alert', context).once('alert-dismiss').each(function () {
+                var self = $(this);
+                var nid = parseInt(self.data('nid'));
+                var slick = self.parents('.slick__slider').first();
+                // Remove dismissed alerts.
+                if ($.inArray(nid, dismissed) != -1) {
+                    if (slick.size() > 0) {
+                        slick.slick('slickRemove', self.parents('.slick__slide').eq(0).index());
+                    }
+                    else {
+                        self.remove();
+                    }
+                }
+                $('.site-alert__dismiss', self).on('click', function () {
+                    if (slick.size() > 0) {
+                        slick.slick('slickRemove', self.parents('.slick__slide').eq(0).index());
+                    }
+                    else {
+                        self.remove();
+                    }
+                    // Store dimsmissed alerts ids into cookie.
+                    dismissed = Drupal.behaviors.alert_dismiss.getDismissed();
+                    if ($.inArray(nid, dismissed) == -1) {
+                        dismissed.push(nid);
+                    }
+                    Drupal.behaviors.alert_dismiss.setDismissed(dismissed);
+                    return false;
+                });
+            });
+            $('.header-alerts-list').addClass('header-alerts-list-processed');
+        },
+
+        getDismissed: function () {
+            var jQueryCookieJson = $.cookie.json;
+            $.cookie.json = true;
+            var dismissed = $.cookie('alerts_dismiss') || [];
+            $.cookie.json = jQueryCookieJson;
+            return dismissed;
+        },
+
+        setDismissed: function (dismissed) {
+            var jQueryCookieJson = $.cookie.json;
+            $.cookie.json = true;
+            $.cookie('alerts_dismiss', dismissed, {expires: 7});
+            $.cookie.json = jQueryCookieJson;
+        }
+    };
+
+})(jQuery);
