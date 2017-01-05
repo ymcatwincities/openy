a2e26c3a4d6d5d9170fd379dff2ac94227067164
Dmytro Danylevskyi <dmytro@danylevskyi.com>
2017-01-05 12:57:59 +0200

    [YMCA-1426] Upgrade simple_sitemap module

--- docroot/modules/contrib/simple_sitemap/js/simple_sitemap.fieldsetSummaries.js
+++ docroot/modules/contrib/simple_sitemap/js/simple_sitemap.fieldsetSummaries.js
@@ -8,16 +8,13 @@
 
   Drupal.behaviors.simple_sitemapFieldsetSummaries = {
     attach: function(context) {
-      $(context).find('#edit-simple-sitemap').drupalSetSummary(function (context) {
+      $(context).find('#edit-simple-sitemap').drupalSetSummary(function(context) {
         var vals = [];
         if ($(context).find('#edit-simple-sitemap-index-content-1').is(':checked')) {
-
-          // Display summary of the settings in tabs.
           vals.push(Drupal.t('Included in sitemap'));
           vals.push(Drupal.t('Priority') + ' ' + $('#edit-simple-sitemap-priority option:selected', context).text());
         }
         else {
-          // Display summary of the settings in tabs.
           vals.push(Drupal.t('Excluded from sitemap'));
         }
         return vals.join('<br />');
