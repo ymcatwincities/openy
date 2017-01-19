50ed34376f2312cc58a87bd2bf6e6b10fbb5c939
Dmytro Danylevskyi <dmytro@danylevskyi.com>
2017-01-19 17:19:15 +0200

    Fix interface

--- src/DataWrapperBase.php
+++ src/DataWrapperBase.php
@@ -13,7 +13,10 @@ abstract class DataWrapperBase implements DataWrapperInterface {
   abstract public function getMembershipPriceMatrix();
 
   /**
-   * {@inheritdoc}
+   * Get list of membership types.
+   *
+   * @return array
+   *   The list of membership types keyed by type ID.
    */
   public function getMembershipTypes() {
     $types = [];
@@ -29,7 +32,13 @@ abstract class DataWrapperBase implements DataWrapperInterface {
   }
 
   /**
-   * {@inheritdoc}
+   * Get the list of locations.
+   *
+   * @param string $membership_type
+   *   Membership type.
+   *
+   * @return array
+   *   The list of locations keyed by location ID.
    */
   public function getLocations($membership_type) {
     $locations = [];
@@ -52,7 +61,15 @@ abstract class DataWrapperBase implements DataWrapperInterface {
   }
 
   /**
-   * {@inheritdoc}
+   * Get price.
+   *
+   * @param int $location_id
+   *   Location ID.
+   * @param string $membership_type
+   *   Membership type ID.
+   *
+   * @return string
+   *   Price.
    */
   public function getPrice($location_id, $membership_type) {
     foreach ($this->getMembershipPriceMatrix() as $membership_type_item) {
