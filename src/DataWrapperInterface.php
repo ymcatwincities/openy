309532205b7e271c4227c569f4e3376df7b3cd8e
Dmytro Danylevskyi <dmytro@danylevskyi.com>
2017-01-19 17:19:15 +0200

    Fix interface

--- src/DataWrapperInterface.php
+++ src/DataWrapperInterface.php
@@ -8,38 +8,6 @@ namespace Drupal\openy_calc;
 interface DataWrapperInterface {
 
   /**
-   * Get list of membership types.
-   *
-   * @return array
-   *   The list of membership types keyed by type ID.
-   */
-  public function getMembershipTypes();
-
-  /**
-   * Get the list of locations.
-   *
-   * @param string $membership_type
-   *   Membership type.
-   *
-   * @return array
-   *   The list of locations keyed by location ID.
-   */
-  public function getLocations($membership_type);
-
-  /**
-   * Get price.
-   *
-   * @param int $location_id
-   *   Location ID.
-   * @param string $membership_type
-   *   Membership type ID.
-   *
-   * @return string
-   *   Price.
-   */
-  public function getPrice($location_id, $membership_type);
-
-  /**
    * Return price matrix.
    *
    * @return array
