e4110dbf8684d7e5c793bc55165307647ac77a38
Dmytro Danylevskyi <dmytro@danylevskyi.com>
2017-01-19 17:03:19 +0200

    Add working matrix

--- src/DataWrapper.php
+++ src/DataWrapper.php
@@ -10,45 +10,90 @@ class DataWrapper implements DataWrapperInterface {
   /**
    * {@inheritdoc}
    */
-  public function getMembershipTypes() {
-    return [
-      'youth' => [
+  public function getMembershipPriceMatrix() {
+    $matrix = [
+      [
+        'id' => 'youth',
         'title' => 'Youth',
-        'description' => 'Short description of membership type',
-      ],
-      'adult' => [
-        'title' => 'Adult',
-        'description' => 'Short description of membership type',
-      ],
-      'family' => [
-        'title' => 'Family',
-        'description' => 'Short description of membership type',
+        'description' => 'Here short description',
+        'locations' => [
+          [
+            'title' => 'Loc #1',
+            'id' => 12,
+            'price' => 100,
+          ],
+          [
+            'title' => 'Loc #2',
+            'id' => 13,
+            'price' => 200,
+          ],
+          [
+            'title' => 'Loc #3',
+            'id' => 14,
+            'price' => 300,
+          ],
+        ],
       ],
     ];
+
+    return $matrix;
   }
 
   /**
    * {@inheritdoc}
    */
-  public function getLocations() {
-    return [
-      1 => [
-        'title' => 'Location #1',
-      ],
-      2 => [
-        'title' => 'Location #2',
-      ],
-      3 => [
-        'title' => 'Location #3',
-      ],
-    ];
+  public function getMembershipTypes() {
+    $types = [];
+
+    foreach ($this->getMembershipPriceMatrix() as $membership_type) {
+      $types[$membership_type['id']] = [
+        'title' => $membership_type['title'],
+        'description' => $membership_type['description'],
+      ];
+    }
+
+    return $types;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getLocations($membership_type) {
+    $locations = [];
+    $membership_definition = FALSE;
+
+    foreach ($this->getMembershipPriceMatrix() as $membership_type_item) {
+      if ($membership_type_item['id'] == $membership_type) {
+        $membership_definition = $membership_type_item;
+        break;
+      }
+    }
+
+    foreach ($membership_definition['locations'] as $location) {
+      $locations[$location['id']] = [
+        'title' => $location['title'],
+      ];
+    }
+
+    return $locations;
   }
 
   /**
    * {@inheritdoc}
    */
   public function getPrice($location_id, $membership_type) {
-    return '100$/Month';
+    foreach ($this->getMembershipPriceMatrix() as $membership_type_item) {
+      if ($membership_type_item['id'] == $membership_type) {
+        foreach ($membership_type_item['locations'] as $location) {
+          if ($location['id'] == $location_id) {
+            return $location['price'];
+          }
+        }
+        break;
+      }
+    }
+
+    return FALSE;
   }
 
 }
