74f4a7c63aea55e45dc823fdcc2468829494a80b
Dmytro Danylevskyi <dmytro@danylevskyi.com>
2017-01-24 11:48:37 +0200

    Add initial openy_map form element

---
+++ js/main.js
@@ -0,0 +1,55 @@
+/**
+ * @file main.js
+ */
+(function ($, Drupal, drupalSettings) {
+
+  "use strict";
+
+  Drupal.openyMap = function() {
+    return {
+      drawMap: function (config) {
+        $(config.element).once('openyMap').each(function () {
+
+          // Let's center map on the first point.
+          var map = new google.maps.Map($(config.element).get(0), {
+            zoom: 13,
+            center: config.data[0]
+          });
+
+
+          // Add markers.
+          $.each(config.data, function(index, value) {
+            var marker = new google.maps.Marker({
+              position: { lat: value.lat, lng: value.lng },
+              map: map,
+              title: value.title
+            });
+
+            var info = new google.maps.InfoWindow({
+              content: value.markup
+            });
+
+            marker.addListener('click', function() {
+              info.open(map, marker);
+            });
+          });
+
+        });
+      }
+    }
+  };
+
+  Drupal.behaviors.openyMap = {
+    attach: function (context, settings) {
+      var data = settings.openyMap;
+      var map = new Drupal.openyMap();
+
+      map.drawMap({
+          element: '#openy-map-canvas',
+          data: data
+      });
+
+    }
+  };
+
+}(jQuery, Drupal, drupalSettings));
---
+++ src/Element/OpenYMap.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\openy_map\Element;
+
+use Drupal\Core\Render\Element\RenderElement;
+
+/**
+ * Class OpenYMap.
+ *
+ * @RenderElement("openy_map")
+ */
+class OpenYMap extends RenderElement {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getInfo() {
+    $class = get_class($this);
+    return [
+      '#theme' => 'openy_map',
+      '#pre_render' => [
+        [$class, 'processElement'],
+      ],
+    ];
+  }
+
+  /**
+   * Prepare render array for template.
+   *
+   * @param array $element
+   *   Element.
+   *
+   * @return array
+   *   Element
+   */
+  public static function processElement($element) {
+
+    $element['#attached']['drupalSettings']['openyMap'] = $element['#element_variables'];
+
+    return $element;
+  }
+
+}
