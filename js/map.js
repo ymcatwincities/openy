ea1c502c00f535461695eadf7ddab086d07a7bf3
Dmytro Danylevskyi <dmytro@danylevskyi.com>
2017-01-24 13:30:15 +0200

    Fix openy_map JS

--- js/map.js
+++ js/map.js
@@ -5,35 +5,31 @@
 
   "use strict";
 
+  // pass context
   Drupal.openyMap = function() {
     return {
       drawMap: function (config) {
-        $(config.element).once('openyMap').each(function () {
+        // Let's center map on the first point.
+        var map = new google.maps.Map(config.element, {
+          zoom: 13,
+          center: config.data[0]
+        });
 
-          // Let's center map on the first point.
-          var map = new google.maps.Map($(config.element).get(0), {
-            zoom: 13,
-            center: config.data[0]
+        // Add markers.
+        $.each(config.data, function(index, value) {
+          var marker = new google.maps.Marker({
+            position: { lat: value.lat, lng: value.lng },
+            map: map,
+            title: value.title
           });
 
-
-          // Add markers.
-          $.each(config.data, function(index, value) {
-            var marker = new google.maps.Marker({
-              position: { lat: value.lat, lng: value.lng },
-              map: map,
-              title: value.title
-            });
-
-            var info = new google.maps.InfoWindow({
-              content: value.markup
-            });
-
-            marker.addListener('click', function() {
-              info.open(map, marker);
-            });
+          var info = new google.maps.InfoWindow({
+            content: value.markup
           });
 
+          marker.addListener('click', function() {
+            info.open(map, marker);
+          });
         });
       }
     }
@@ -44,9 +40,20 @@
       var data = settings.openyMap;
       var map = new Drupal.openyMap();
 
-      map.drawMap({
-          element: '.openy-map-canvas',
-          data: data
+      $('.openy-map-canvas', context).once().each(function () {
+        var self = this;
+        var timer = setInterval(function () {
+          if (typeof window.google == 'undefined') {
+            return;
+          }
+
+          map.drawMap({
+            element: self,
+            data: data
+          });
+
+          clearInterval(timer);
+        }, 100);
       });
 
     }
