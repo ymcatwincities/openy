478ab1411c7a66212baae7dd16735d7b4ee5e4b6
AlexNetman <alex.for.ffw@gmail.com>
2017-01-27 14:48:17 +0300

    Added quick function to add branch

--- tests/features/bootstrap/FeatureContext.php
+++ tests/features/bootstrap/FeatureContext.php
@@ -1,15 +1,12 @@
 <?php
 
-/**
- * @file
- * FeatureContent for OpenY project.
- */
-
 use Drupal\DrupalExtension\Context\RawDrupalContext;
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
 use Behat\Behat\Tester\Exception\PendingException;
+use Symfony\Component\Process\PhpExecutableFinder;
+use Symfony\Component\Process\Process;
 
 /**
  * Defines application features from the specific context.
@@ -24,6 +21,23 @@ class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext
    * context constructor through behat.yml.
    */
   public function __construct() {
+  }
+  /**
+   * @Given /^I create a branch$/
+   */
+  public function iCreateABranch()
+  {
+    $this->getSession()->visit($this->locatePath('/node/add/branch'));
+    $element = $this->getSession()->getPage();
+    $element->fillField('Title', 'Test Branch');
+    $element->fillField('Street address', 'Main road 10');
+    $element->fillField('City', 'Seattle');
+    $element->fillField('State', 'WA');
+    $element->fillField('Zip code', '98101');
+    $element->fillField('Latitude', '47.293433');
+    $element->fillField('Longitude', '-122.238717');
+    $element->fillField('Phone', '+1234567890');
+    $element->findButton('Save and publish')->click();
   }
 
 }
