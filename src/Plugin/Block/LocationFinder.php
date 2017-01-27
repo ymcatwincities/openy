3be33b140b211ff30480391a5cf90123ae9731fc
Andrii TIupa <andrii.tiupa@ffw.co>
2017-01-27 18:42:28 +0200

    [YGH-227] Implementation of Location Finder functionality with theming

--- src/Plugin/Block/LocationFinder.php
+++ src/Plugin/Block/LocationFinder.php
@@ -3,6 +3,9 @@
 namespace Drupal\location_finder\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\openy_socrates\OpenySocratesFacade;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a block with location finder.
@@ -13,19 +16,53 @@ use Drupal\Core\Block\BlockBase;
  *   category = @Translation("Paragraph Blocks")
  * )
  */
-class LocationFinder extends BlockBase {
+class LocationFinder extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * @var OpenySocratesFacade
+   */
+  protected $socrates;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, OpenySocratesFacade $socrates) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->socrates = $socrates;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('socrates')
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
     return [
-      '#type' => 'location_finder_element',
-      '#attached' => [
-        'library' => [
-          'location_finder/location_finder',
-        ],
+      [
+        '#type' => 'openy_map',
+        '#show_controls' => TRUE,
+        '#element_variables' => $this->socrates->getLocationPins(),
       ],
+      [
+       '#type' => 'view',
+        '#name' => 'locations',
+        '#display_id' => 'locations_branches_block',
+      ],
+      [
+        '#type' => 'view',
+        '#name' => 'locations',
+        '#display_id' => 'locations_camps_block',
+      ]
     ];
   }
 
