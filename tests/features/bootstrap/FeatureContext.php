968b5c5ffbc96e5e63bb55717476034804a114bd
AlexNetman <amolchano@gmail.com>
2017-01-30 11:34:35 +0200

    [OYPD-161] Behat tests for Blog and support functions (#67)
    * drush configured
    * Added quick function to add branch
    * Add blog post feature with adding taxonomy terms
    * Removed call for import. Added term creation during test
    * fixed URL
    * Fix for codesniffers
    * More code-style fixes
    * Update Test according to codestyle

--- tests/features/bootstrap/FeatureContext.php
+++ tests/features/bootstrap/FeatureContext.php
@@ -10,6 +10,8 @@ use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
 use Behat\Behat\Tester\Exception\PendingException;
+use Symfony\Component\Process\PhpExecutableFinder;
+use Symfony\Component\Process\Process;
 
 /**
  * Defines application features from the specific context.
@@ -24,6 +26,37 @@ class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext
    * context constructor through behat.yml.
    */
   public function __construct() {
+  }
+
+  /**
+   * Create a node of Branch CT with name "Test Branch".
+   *
+   * @Given /^I create a branch$/
+   */
+  public function iCreateBranch() {
+    $this->getSession()->visit($this->locatePath('/node/add/branch'));
+    $element = $this->getSession()->getPage();
+    $element->fillField('Title', 'Test Branch');
+    $element->fillField('Street address', 'Main road 10');
+    $element->fillField('City', 'Seattle');
+    $element->fillField('State', 'WA');
+    $element->fillField('Zip code', '98101');
+    $element->fillField('Latitude', '47.293433');
+    $element->fillField('Longitude', '-122.238717');
+    $element->fillField('Phone', '+1234567890');
+    $element->findButton('Save and publish')->click();
+  }
+
+  /**
+   * Creates a term "Category One" in Category taxonomy.
+   *
+   * @Given /^I create a category term$/
+   */
+  public function iCreateCategoryTerm() {
+    $this->getSession()->visit($this->locatePath('/admin/structure/taxonomy/manage/blog_category/add'));
+    $element = $this->getSession()->getPage();
+    $element->fillField('Name', 'Category One');
+    $element->findButton('Save')->click();
   }
 
 }
