22f3c2f7b0e445149c770485138abdc287e4bb6b
Tiago Bember <tiago.bember@ffwagency.com>
2017-02-23 09:52:59 -0500

    [YMCA-1492] Adjustments on introduction tab - part II.

--- docroot/themes/custom/ymca/scripts/ymca.js
+++ docroot/themes/custom/ymca/scripts/ymca.js
@@ -319,16 +319,4 @@
     }
   };
 
-  Drupal.behaviors.blockHeight = {
-    attach: function (context, settings) {
-      $('.compain-info-block .content-text', context)
-        .once('content-text').matchHeight({
-          byRow: true,
-          property: 'height',
-          target: null,
-          remove: false
-        });
-    }
-  };
-
 })(jQuery);
