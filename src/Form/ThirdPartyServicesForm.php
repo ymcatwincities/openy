7da854c956a3eb93274f21236bd36906d12ab980
Dave Bezuidenhout <dave@davethebrave.com>
2017-03-15 14:58:37 -0700

    [OYPD-20a] Analytics - Google Tag Manager

--- src/Form/ThirdPartyServicesForm.php
+++ src/Form/ThirdPartyServicesForm.php
@@ -26,6 +26,8 @@ class ThirdPartyServicesForm extends FormBase {
     $config_factory = \Drupal::service('config.factory');
     // Get Google Maps API settings container.
     $geo_loc_config = $config_factory->get('geolocation.settings');
+    // Get Google Tag Manager Account settings container.
+    $gtm_config = $config_factory->get('google_tag.settings');
     // Get Optimizely settings container.
     $optimizely_config = $config_factory->get('optimizely.settings');
 
@@ -37,6 +39,16 @@ class ThirdPartyServicesForm extends FormBase {
       '#title' => $this->t('Google Maps API key'),
       '#default_value' => $geo_loc_config->get('google_map_api_key'),
       '#description' => $this->t('Google Maps requires users to use a valid API key. Using the <a href="https://console.developers.google.com/apis" target="_blank">Google API Manager</a>, you can enable the <em>Google Maps JavaScript API</em>. That will create (or reuse) a <em>Browser key</em> which you can paste here.'),
+    ];
+
+    // Google Tag Manager Account ID.
+    $form['google_tag_manager_id'] = [
+      '#title' => $this->t('Container ID'),
+      '#description' => $this->t('The ID assigned by Google Tag Manager (GTM) for this website container. To get a container ID, <a href="https://tagmanager.google.com/">sign up for GTM</a> and create a container for your website.'),
+      '#default_value' => $gtm_config->get('container_id'),
+      '#attributes' => ['placeholder' => ['GTM-xxxxxx']],
+      '#maxlength' => 20,
+      '#type' => 'textfield',
     ];
 
     // Optimizely ID Number.
@@ -70,6 +82,14 @@ class ThirdPartyServicesForm extends FormBase {
     $geo_loc_config->set('google_map_api_key', $form_state->getValue('google_map_api_key'));
     $geo_loc_config->save();
 
+    // Set Google Tag Manager Container ID & create snippets.
+    if (!empty($form_state->getValue('google_tag_manager_id'))) {
+      $gtm_config = $config_factory->getEditable('google_tag.settings');
+      $gtm_config->set('container_id', $form_state->getValue('google_tag_manager_id'));
+      $gtm_config->save();
+      $this->saveSnippets();
+    }
+
     $optimizely_id = $form_state->getValue('optimizely_id');
     $optimizely_config->set('optimizely_id', $optimizely_id);
     $optimizely_config->save();
@@ -80,6 +100,32 @@ class ThirdPartyServicesForm extends FormBase {
       ))
       ->condition('oid', '1')
       ->execute();
+  }
+
+  /**
+   * Saves JS snippet files based on current settings.
+   *
+   * @return bool
+   *   Whether the files were saved.
+   */
+  public function saveSnippets() {
+    // Save the altered snippets after hook_google_tag_snippets_alter().
+    module_load_include('inc', 'google_tag', 'includes/snippet');
+    $result = TRUE;
+    $snippets = google_tag_snippets();
+    foreach ($snippets as $type => $snippet) {
+      $path = file_unmanaged_save_data($snippet, "public://js/google_tag.$type.js", FILE_EXISTS_REPLACE);
+      $result = !$path ? FALSE : $result;
+    }
+    if (!$result) {
+      drupal_set_message(t('An error occurred saving one or more snippet files. Please try again or contact the site administrator if it persists.'));
+    }
+    else {
+      drupal_set_message(t('Created three snippet files based on configuration.'));
+      \Drupal::service('asset.js.collection_optimizer')->deleteAll();
+      _drupal_flush_css_js();
+    }
+    return TRUE;
   }
 
 }
