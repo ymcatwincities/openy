cb17370e1a3ee559246fa5ca9e2c2ca40bf1751e
Dave Bezuidenhout <dave@davethebrave.com>
2017-03-14 21:56:10 -0700

    [YMCATCOY-1] Add Google Analytics to installation - add config to installation process

--- src/Form/ThirdPartyServicesForm.php
+++ src/Form/ThirdPartyServicesForm.php
@@ -5,6 +5,7 @@ namespace Drupal\openy\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Database\Database;
+use Drupal\Core\Url;
 
 /**
  * Defines a form for setting Google Maps API Key during install.
@@ -26,6 +27,8 @@ class ThirdPartyServicesForm extends FormBase {
     $config_factory = \Drupal::service('config.factory');
     // Get Google Maps API settings container.
     $geo_loc_config = $config_factory->get('geolocation.settings');
+    // Get Google Analytics Account settings container.
+    $ga_config = $config_factory->get('google_analytics.settings');
     // Get Optimizely settings container.
     $optimizely_config = $config_factory->get('optimizely.settings');
 
@@ -37,6 +40,16 @@ class ThirdPartyServicesForm extends FormBase {
       '#title' => $this->t('Google Maps API key'),
       '#default_value' => $geo_loc_config->get('google_map_api_key'),
       '#description' => $this->t('Google Maps requires users to use a valid API key. Using the <a href="https://console.developers.google.com/apis" target="_blank">Google API Manager</a>, you can enable the <em>Google Maps JavaScript API</em>. That will create (or reuse) a <em>Browser key</em> which you can paste here.'),
+    ];
+
+    // Google Analytics Account ID.
+    $form['google_analytics_account'] = [
+      '#default_value' => $ga_config->get('account'),
+      '#description' => $this->t('This ID is unique to each site you want to track separately, and is in the form of UA-xxxxxxx-yy. To get a Web Property ID, <a href=":analytics">register your site with Google Analytics</a>, or if you already have registered your site, go to your Google Analytics Settings page to see the ID next to every site profile. <a href=":webpropertyid">Find more information in the documentation</a>.', [':analytics' => 'http://www.google.com/analytics/', ':webpropertyid' => Url::fromUri('https://developers.google.com/analytics/resources/concepts/gaConceptsAccounts', ['fragment' => 'webProperty'])->toString()]),
+      '#maxlength' => 20,
+      '#placeholder' => 'UA-',
+      '#title' => $this->t('Google Analytics Web Property ID'),
+      '#type' => 'textfield',
     ];
 
     // Optimizely ID Number.
@@ -69,6 +82,11 @@ class ThirdPartyServicesForm extends FormBase {
 
     $geo_loc_config->set('google_map_api_key', $form_state->getValue('google_map_api_key'));
     $geo_loc_config->save();
+
+    // Set Google Analytics Account TODO: Add other values?
+    $ga_config = $config_factory->getEditable('google_analytics.settings');
+    $ga_config->set('account', $form_state->getValue('google_analytics_account'));
+    $ga_config->save();
 
     $optimizely_id = $form_state->getValue('optimizely_id');
     $optimizely_config->set('optimizely_id', $optimizely_id);
