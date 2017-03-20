4c8f88516280105efddc34cabf79736d7b9856b0
Vasyl Grygoryev <vasyl.grygoryev@ffwagency.com>
2017-03-20 15:51:23 +0200

    [YMCA-1574] redirect to winners page with event subscriber; sorting for winners on winner page; fix bug with path for images; fix codesniffer error

--- docroot/modules/custom/ymca_retention/src/Plugin/Layout/YmcaRetentionWinnersLayout.php
+++ docroot/modules/custom/ymca_retention/src/Plugin/Layout/YmcaRetentionWinnersLayout.php
@@ -81,12 +81,11 @@ class YmcaRetentionWinnersLayout extends LayoutBase {
 
     $members = Member::loadMultiple($member_ids);
 
-
     foreach ($winners as $winner) {
       $branch_id = $winner->getBranchId();
       $member = $members[$winner->getMemberId()];
       $member_id = $member->getMemberId();
-      $hidden_member_id = str_repeat('*', strlen($member_id) - 4) . substr($member_id, - 4);
+      $hidden_member_id = str_repeat('*', strlen($member_id) - 4) . substr($member_id, -4);
       $member_first_name = $member->getFirstName();
       $member_last_name = $member->getLastName();
       $member_name = $member_first_name . ' ' . $member_last_name[0] . '.';
@@ -103,6 +102,18 @@ class YmcaRetentionWinnersLayout extends LayoutBase {
         'id' => $hidden_member_id,
         'name' => $member_name,
       ];
+    }
+
+    // Define compare function.
+    $winnersCompare = function($winner1, $winner2) {
+      return strcmp($winner1['name'], $winner2['name']);
+    };
+
+    foreach ($winnersData['winners'] as $branch_id => $branch_winners) {
+      foreach ($branch_winners as $place => $winners_array) {
+        uasort($winnersData['winners'][$branch_id][$place], $winnersCompare);
+        $winnersData['winners'][$branch_id][$place] = array_values($winnersData['winners'][$branch_id][$place]);
+      }
     }
 
     $branch_ids = array_keys($winnersData['winners']);
