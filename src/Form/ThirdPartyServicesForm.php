9ea02c756267612a1b701a6816e022878f032bab
Dave Bezuidenhout <dave@davethebrave.com>
2017-03-20 12:46:21 -0700

    [OYPD-20] GA validation only if not empty

--- src/Form/ThirdPartyServicesForm.php
+++ src/Form/ThirdPartyServicesForm.php
@@ -77,14 +77,20 @@ class ThirdPartyServicesForm extends FormBase {
   public function validateForm(array &$form, FormStateInterface $form_state) {
     parent::validateForm($form, $form_state);
 
-    // Trim some text values.
-    $form_state->setValue('google_analytics_account', trim($form_state->getValue('google_analytics_account')));
+    if (!empty(trim($form_state->getValue('google_analytics_account')))) {
+      // Trim some text values.
+      $form_state->setValue('google_analytics_account', trim($form_state->getValue('google_analytics_account')));
 
-    // Replace all type of dashes (n-dash, m-dash, minus) with normal dashes.
-    $form_state->setValue('google_analytics_account', str_replace(['–', '—', '−'], '-', $form_state->getValue('google_analytics_account')));
+      // Replace all type of dashes (n-dash, m-dash, minus) with normal dashes.
+      $form_state->setValue('google_analytics_account', str_replace([
+        '–',
+        '—',
+        '−'
+      ], '-', $form_state->getValue('google_analytics_account')));
 
-    if (!preg_match('/^UA-\d+-\d+$/', $form_state->getValue('google_analytics_account'))) {
-      $form_state->setErrorByName('google_analytics_account', t('A valid Google Analytics Web Property ID is case sensitive and formatted like UA-xxxxxxx-yy.'));
+      if (!preg_match('/^UA-\d+-\d+$/', $form_state->getValue('google_analytics_account'))) {
+        $form_state->setErrorByName('google_analytics_account', t('A valid Google Analytics Web Property ID is case sensitive and formatted like UA-xxxxxxx-yy.'));
+      }
     }
   }
 
