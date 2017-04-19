1de6339ab81e49c05815ea452e03fa744a8f8ead
Georgiy Golovenko <geogoleo@gmail.com>
2017-04-19 18:27:34 +0300

    [YPTF-252]: ClientMindbody delete debug mode for every request.

--- docroot/modules/custom/mindbody/src/MindbodyClient.php
+++ docroot/modules/custom/mindbody/src/MindbodyClient.php
@@ -100,7 +100,6 @@ class MindbodyClient implements MindbodyClientInterface {
     $endpointUrl = "https://" . $this->hostname . "/0_5/" . $service . ".asmx";
     $wsdlUrl = $endpointUrl . "?wsdl";
 
-    $this->debug = TRUE;
 
     $option = [];
     if ($this->debug) {
