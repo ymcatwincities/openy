82456cd416bb3cfa508b0c31612359f7b80f4fc1
John Kerbaugh <john.kerbaugh@ffwagency.com>
2017-05-11 09:50:22 -0400

    [OYPD-363] Update post-install and post-update action to remove docroot from the profile folder.

--- remove_openy_docroot_folder.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Remove docroot folder from openy folder to avoid collision with drupal core.
-echo Removing .git folders from vendor folder to avoid possible git issues.
-rm -rf docroot/ > /dev/null 2>&1
