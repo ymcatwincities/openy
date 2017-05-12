66506c6735b3a74d8e84aeffa334c017f488e985
John Kerbaugh <john.kerbaugh@ffwagency.com>
2017-05-12 11:44:56 -0400

    [OYPD-333] Move git cleanup script into scripts folders, and include docroot/library and docroot/modules/contrib folders.

--- remove_vendor_git_folders.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Remove .git folders from vendor folder to avoid git submodule issues for some
-# workflows.
-echo Removing .git folders from vendor folder to avoid possible git issues.
-find vendor -name ".git" -exec rm -rf {} \; > /dev/null 2>&1
