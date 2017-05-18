30598a28ec070a3da780ce803bf3694a3ed765a3
Andrii TIupa <andrii.tiupa@ffwagency.com>
2017-05-18 14:41:33 +0300

    [YMCA-1618][YMCA-1619] fix issues with html inside desctiption and ajax reload

--- docroot/modules/custom/ymca_groupex/src/GroupexScheduleFetcher.php
+++ docroot/modules/custom/ymca_groupex/src/GroupexScheduleFetcher.php
@@ -449,7 +449,7 @@ class GroupexScheduleFetcher {
         'atc_date_end' => $date_end,
         'atc_timezone' => drupal_get_user_timezone(),
         'atc_title' => $item->title,
-        'atc_description' => 'Visit ' . $item->category . ' with ' . $item->instructor . PHP_EOL . 'Class will take place at ' . $item->studio . '.',
+        'atc_description' => 'Visit ' . $item->category . ' with ' . strip_tags($item->instructor) . PHP_EOL . 'Class will take place at ' . $item->studio . '.',
         'atc_location' => $address,
         'atc_organizer' => $item->instructor,
       ];
