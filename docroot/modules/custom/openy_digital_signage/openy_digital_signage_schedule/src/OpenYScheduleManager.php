d7da6206c57b7d18cabdd7175f0c0babe18eb4bf
Andrey Maximov <savagge@gmail.com>
2017-05-22 15:11:53 +0300

    [YGB-47] Update OpenYScheduleManager

--- docroot/modules/custom/openy_digital_signage/openy_digital_signage_schedule/src/OpenYScheduleManager.php
+++ docroot/modules/custom/openy_digital_signage/openy_digital_signage_schedule/src/OpenYScheduleManager.php
@@ -7,6 +7,7 @@ use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Entity\Query\QueryFactory;
 use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Drupal\openy_digital_signage_schedule\Entity\OpenYSchedule;
 
 /**
  * Class OpenYScheduleManager.
@@ -66,6 +67,58 @@ class OpenYScheduleManager implements OpenYScheduleManagerInterface {
    */
   public function dummy() {
     // Intentionally empty.
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getUpcomingScreenContents(OpenYSchedule $schedule, $timespan, $now = NULL) {
+    if (!$now) {
+      $now = time();
+    }
+
+    $query = $this->entityQuery->get('openy_digital_signage_sch_item');
+    $query->condition('schedule', $schedule->id());
+    $entity_ids = $query->execute();
+
+    if (!$entity_ids) {
+      return [];
+    }
+
+    $schedule_items = $this->entityTypeManager
+      ->getStorage('openy_digital_signage_sch_item')
+      ->loadMultiple($entity_ids);
+
+    $schedule = [];
+    $now_formatted = date('H:i:s', $now);
+    $today = date('Y-m-d', $now);
+    $tomorrow = date('Y-m-d', strtotime($today . ' +1day'));
+    foreach ($schedule_items as $schedule_item) {
+      $from_ts = strtotime($schedule_item->get('time_slot')->value . 'z');
+      $to_ts = strtotime($schedule_item->get('time_slot')->end_value . 'z');
+      $from_time = date('H:i:s', $from_ts);
+      $to_time = date('H:i:s', $to_ts);
+      $date = $today;
+      if ($to_time < $now_formatted) {
+        $date = $tomorrow;
+      }
+      $from = $date . 'T' . $from_time;
+      $to = $date . 'T' . $to_time;
+      $from_ts = strtotime($from);
+      $to_ts = strtotime($to);
+      $schedule[$from] = [
+        'item' => $schedule_item,
+        'from' => $from,
+        'to' => $to,
+        'from_ts' => $from_ts,
+        'to_ts' => $to_ts,
+      ];
+
+    }
+
+    ksort($schedule);
+
+    return $schedule;
   }
 
 }
