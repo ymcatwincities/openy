6b70d78d93eb2ae44315006ccef64a008fb63573
Andrey Maximov <savagge@gmail.com>
2017-05-22 15:30:00 +0300

    Fix code style

--- docroot/modules/custom/openy_digital_signage/openy_digital_signage_screen/src/OpenYDigitalSignageScreenServiceProvider.php
+++ docroot/modules/custom/openy_digital_signage/openy_digital_signage_screen/src/OpenYDigitalSignageScreenServiceProvider.php
@@ -6,16 +6,17 @@ use Drupal\Core\DependencyInjection\ContainerBuilder;
 use Drupal\Core\DependencyInjection\ServiceProviderBase;
 
 /**
-* Modifies the IPE Access manager service.
-*/
+ * Modifies the IPE Access manager service.
+ */
 class OpenyDigitalSignageScreenServiceProvider extends ServiceProviderBase {
 
   /**
-  * {@inheritdoc}
-  */
+   * {@inheritdoc}
+   */
   public function alter(ContainerBuilder $container) {
     // Overrides plugin.manager.ipe_access class to disable panelizer on a screen page.
     $definition = $container->getDefinition('plugin.manager.ipe_access');
     $definition->setClass('Drupal\openy_digital_signage_screen\Plugin\OpenYScreenIPEAccessManager');
   }
+
 }
