b4a09dcfd4d4ec571b4bcc1cfe2a59478f28c93e
Anatoliy Vorobyov <anatoliy.vorobyov@ffwagency.com>
2017-06-02 18:58:51 +0300

    if there are more then 3 items in a camp menu that as the screen size gets smaller menu items disappear #196

--- themes/openy_themes/openy_rose/scripts/openy_rose.js
+++ themes/openy_themes/openy_rose/scripts/openy_rose.js
@@ -106,4 +106,71 @@
     }
   };
 
+  // Horisontal scroll for camp menu.
+  Drupal.behaviors.scrollableList = {
+      attach: function (context, settings) {
+        $('.camp-menu-wrapper').once().each(function() {
+          var $this = $(this),
+              $list = $this.find('ul'),
+              $items = $list.find('li'),
+              listWidth = 0,
+              listPadding = 40;
+
+          setTimeout(function() {
+            $items.each(function() {
+              listWidth += $(this).outerWidth();
+          });
+
+          $list.css('width', listWidth + listPadding + "px");
+
+          var scroll = new IScroll($this[0], {
+            scrollX: true,
+            scrollY: false,
+            momentum: false,
+            snap: false,
+            bounce: true,
+            touch: true,
+            eventPassthrough: true
+          });
+
+            // GRADIENT BEHAVIOUR SCRIPT.
+            console.log('rrr');
+            var obj = $('.camp-menu');
+            var objWrap = $this.append( '<div class="columns-gradient gradient-right" onclick="void(0)"></div>' );
+            objWrap = document.querySelector('.columns-gradient');
+            var sliderLength = listWidth - objWrap.offsetWidth + 40;
+            var firstGap = 20;
+
+            if (window.innerWidth > 768) {
+              sliderLength = listWidth - objWrap.offsetWidth + 150;
+              firstGap = 60;
+            }
+
+            obj.get(0).addEventListener('touchmove', function() {
+              var transformMatrix = obj.css("-webkit-transform") ||
+                  obj.css("-moz-transform")    ||
+                  obj.css("-ms-transform")     ||
+                  obj.css("-o-transform")      ||
+                  obj.css("transform");
+              var matrix = transformMatrix.replace(/[^0-9\-.,]/g, '').split(',');
+              var x = matrix[12] || matrix[4];
+              var y = matrix[13] || matrix[5];
+              console.log(x, y);
+              if (x <= -sliderLength + listPadding) {
+                objWrap.classList.remove('gradient-right');
+              } else {
+                objWrap.classList.add('gradient-right');
+              }
+
+              if (x >= -firstGap) {
+                objWrap.classList.remove('gradient-left');
+              } else {
+                objWrap.classList.add('gradient-left');
+              }
+            });
+        }, 100);
+      });
+    }
+  };
+
 })(jQuery);
