204303bf7ce62a5fcf78746859d455054e4c8566
Matthew B Cannon <matthew.cannon@blinkreaction.com>
2017-06-06 13:22:26 -0400

    Got the intro tab populated by retention copy settings.

--- docroot/modules/custom/ymca_retention/src/Form/SettingsCopyForm.php
+++ docroot/modules/custom/ymca_retention/src/Form/SettingsCopyForm.php
@@ -4,6 +4,7 @@ namespace Drupal\ymca_retention\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
 
 /**
  * Provides form for managing module settings.
@@ -29,10 +30,9 @@ class SettingsCopyForm extends ConfigFormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('ymca_retention.copy_settings');
-ksm($config);
+
     $form['advanced'] = [
       '#type' => 'vertical_tabs',
-      // '#type' => 'fieldset',
       '#attributes' => ['class' => ['entity-meta']],
       '#weight' => 99,
     ];
@@ -48,7 +48,6 @@ ksm($config);
       ],
       '#weight' => 10,
       '#optional' => TRUE,
-      // '#tree' => TRUE,
     ];
 
     $form['intro_header'] = [
@@ -56,18 +55,18 @@ ksm($config);
       '#title' => $this->t('Header'),
       '#default_value' => $config->get('intro_header'),
       '#description' => $this->t('The heading of the introduction tab.'),
+      '#weight' => -99,
       '#group' => 'retention_intro',
     ];
 
-    $form['intro_body'] = [
-      // '#type' => 'text_format',
-      '#type' => 'textarea',
+    $form['retention_intro']['intro_body'] = [
+      '#type' => 'text_format',
       '#title' => $this->t('Body'),
-      // '#default_value' => 'Placeholder',
-      '#default_value' => $config->get('intro_body'),
+      '#default_value' => $config->get('intro_body')['value'],
+      '#format' => $config->get('intro_body')['format'],
       '#description' => $this->t('Copy displayed right after the header.'),
       '#format' => 'full_html',
-      '#group' => 'retention_intro',
+      '#weight' => -90,
     ];
 
     $form['intro_reg_btn'] = [
@@ -85,8 +84,6 @@ ksm($config);
       $form['retention_intro'][$name] = [
         '#type' => 'fieldset',
         '#title' => $this->t('Info Block @n', ['@n' => $i]),
-        // '#description' => $this->t('Info block # @n elements.', ['@n' => $i]),
-        // '#group' => 'retention_intro',
         '#attributes' => [
           'class' => ['entity-form-intro-block-' . $i],
         ],
@@ -104,15 +101,6 @@ ksm($config);
         '#group' => $name,
       ];
 
-      // $form["{$name}_copy"] = [
-      //   '#type' => 'textfield',
-      //   '#title' => $this->t('Copy'),
-      //   '#default_value' => $config->get("{$name}_copy"),
-      //   '#description' => $this->t('The copy of info block @n.', ['@n' => $i]),
-      //   '#weight' => -2,
-      //   '#group' => $name,
-      // ];
-ksm($config->get("{$name}_copy_2")['format']);
       $form['retention_intro'][$name]["{$name}_copy"] = [
         '#type' => 'text_format',
         '#title' => $this->t('Copy'),
@@ -123,15 +111,16 @@ ksm($config->get("{$name}_copy_2")['format']);
       ];
 
       $form['retention_intro'][$name]["{$name}_link"] = [
-        '#type' => 'url',
+        '#type' => 'textfield',
         '#title' => $this->t('Link'),
-        '#href' => $config->get("{$name}_link"),
+        '#default_value' => $config->get("{$name}_link"),
         '#description' => $this->t('The link of info block @n to apply to the image.', ['@n' => $i]),
         '#weight' => -1,
+        '#element_validate' => [[get_called_class(), 'validateUrl']],
       ];
 
       // Use the #managed_file FAPI element to upload an image file.
-      $form['retention_intro'][$name]["{$name}_img"] = array(
+      $form['retention_intro'][$name]["{$name}_img"] = [
         '#title' => t('Image'),
         '#field_name' => "{$name}_img",
         '#type' => 'managed_file',
@@ -144,99 +133,118 @@ ksm($config->get("{$name}_copy_2")['format']);
         ],
         '#upload_location' => 'public://ymca_retention/',
         '#weight' => 0,
-      );
+      ];
     }
 
+    // $form['date_registration_close'] = [
+    //   '#type' => 'textfield',
+    //   '#title' => $this->t('Registration close date and time'),
+    //   '#size' => 50,
+    //   '#maxlength' => 500,
+    //   '#default_value' => $config->get('date_registration_close'),
+    //   '#description' => $this->t('Date and time when registration will be closed.'),
+    // ];
 
-    $form['date_registration_close'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Registration close date and time'),
-      '#size' => 50,
-      '#maxlength' => 500,
-      '#default_value' => $config->get('date_registration_close'),
-      '#description' => $this->t('Date and time when registration will be closed.'),
-    ];
+    // $form['date_reporting_open'] = [
+    //   '#type' => 'textfield',
+    //   '#title' => $this->t('Reporting open date and time'),
+    //   '#size' => 50,
+    //   '#maxlength' => 500,
+    //   '#default_value' => $config->get('date_reporting_open'),
+    //   '#description' => $this->t('Date and time when reporting will be open.'),
+    // ];
 
-    $form['date_reporting_open'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Reporting open date and time'),
-      '#size' => 50,
-      '#maxlength' => 500,
-      '#default_value' => $config->get('date_reporting_open'),
-      '#description' => $this->t('Date and time when reporting will be open.'),
-    ];
+    // $form['date_reporting_close'] = [
+    //   '#type' => 'textfield',
+    //   '#title' => $this->t('Reporting close date and time'),
+    //   '#size' => 50,
+    //   '#maxlength' => 500,
+    //   '#default_value' => $config->get('date_reporting_close'),
+    //   '#description' => $this->t('Date and time when reporting will be closed.'),
+    // ];
 
-    $form['date_reporting_close'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Reporting close date and time'),
-      '#size' => 50,
-      '#maxlength' => 500,
-      '#default_value' => $config->get('date_reporting_close'),
-      '#description' => $this->t('Date and time when reporting will be closed.'),
-    ];
+    // $form['date_winners_announcement'] = [
+    //   '#type' => 'textfield',
+    //   '#title' => $this->t('Winners announcement date and time'),
+    //   '#size' => 50,
+    //   '#maxlength' => 500,
+    //   '#default_value' => $config->get('date_winners_announcement'),
+    //   '#description' => $this->t('Date and time when winners will be announced.'),
+    // ];
 
-    $form['date_winners_announcement'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Winners announcement date and time'),
-      '#size' => 50,
-      '#maxlength' => 500,
-      '#default_value' => $config->get('date_winners_announcement'),
-      '#description' => $this->t('Date and time when winners will be announced.'),
-    ];
+    // $form['calculate_visit_goal'] = [
+    //   '#type' => 'checkbox',
+    //   '#title' => $this->t('Calculate visit goal'),
+    //   '#default_value' => $config->get('calculate_visit_goal'),
+    // ];
 
-    $form['calculate_visit_goal'] = [
-      '#type' => 'checkbox',
-      '#title' => $this->t('Calculate visit goal'),
-      '#default_value' => $config->get('calculate_visit_goal'),
-    ];
+    // $form['new_member_goal_number'] = [
+    //   '#type' => 'textfield',
+    //   '#title' => $this->t('Goal of visits for new members'),
+    //   '#size' => 50,
+    //   '#maxlength' => 500,
+    //   '#default_value' => $config->get('new_member_goal_number'),
+    //   '#description' => $this->t('Goal of visits in this campaign for new members.'),
+    // ];
 
-    $form['new_member_goal_number'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Goal of visits for new members'),
-      '#size' => 50,
-      '#maxlength' => 500,
-      '#default_value' => $config->get('new_member_goal_number'),
-      '#description' => $this->t('Goal of visits in this campaign for new members.'),
-    ];
+    // $form['limit_goal_number'] = [
+    //   '#type' => 'textfield',
+    //   '#title' => $this->t('Limit goal of visits'),
+    //   '#size' => 50,
+    //   '#maxlength' => 500,
+    //   '#default_value' => $config->get('limit_goal_number'),
+    //   '#description' => $this->t('Limit goal of visits in this campaign.'),
+    // ];
 
-    $form['limit_goal_number'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Limit goal of visits'),
-      '#size' => 50,
-      '#maxlength' => 500,
-      '#default_value' => $config->get('limit_goal_number'),
-      '#description' => $this->t('Limit goal of visits in this campaign.'),
-    ];
+    // $form['date_checkins_start'] = [
+    //   '#type' => 'textfield',
+    //   '#title' => $this->t('Check-ins start date and time'),
+    //   '#size' => 50,
+    //   '#maxlength' => 500,
+    //   '#default_value' => $config->get('date_checkins_start'),
+    //   '#description' => $this->t('Start date and time, for getting data about check-ins in past months, before the campaign starts.'),
+    // ];
 
-    $form['date_checkins_start'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Check-ins start date and time'),
-      '#size' => 50,
-      '#maxlength' => 500,
-      '#default_value' => $config->get('date_checkins_start'),
-      '#description' => $this->t('Start date and time, for getting data about check-ins in past months, before the campaign starts.'),
-    ];
+    // $form['date_checkins_end'] = [
+    //   '#type' => 'textfield',
+    //   '#title' => $this->t('Check-ins end date and time'),
+    //   '#size' => 50,
+    //   '#maxlength' => 500,
+    //   '#default_value' => $config->get('date_checkins_end'),
+    //   '#description' => $this->t('End date and time, for getting data about check-ins in past months, before the campaign starts.'),
+    // ];
 
-    $form['date_checkins_end'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Check-ins end date and time'),
-      '#size' => 50,
-      '#maxlength' => 500,
-      '#default_value' => $config->get('date_checkins_end'),
-      '#description' => $this->t('End date and time, for getting data about check-ins in past months, before the campaign starts.'),
-    ];
-
-    $form['recent_winners_limit'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Recent winners limit'),
-      '#size' => 50,
-      '#maxlength' => 500,
-      '#default_value' => $config->get('recent_winners_limit'),
-      '#description' => $this->t('How many winners to show in the recent winners block.'),
-    ];
+    // $form['recent_winners_limit'] = [
+    //   '#type' => 'textfield',
+    //   '#title' => $this->t('Recent winners limit'),
+    //   '#size' => 50,
+    //   '#maxlength' => 500,
+    //   '#default_value' => $config->get('recent_winners_limit'),
+    //   '#description' => $this->t('How many winners to show in the recent winners block.'),
+    // ];
 
     return parent::buildForm($form, $form_state);
   }
+
+  /**
+   * Form element validation handler for #type 'url'.
+   *
+   * Note that #maxlength and #required is validated by _form_validate() already.
+   */
+  public static function validateUrl(&$element, FormStateInterface $form_state, &$complete_form) {
+    if ($element['#value'] !== '') {
+      if (strpos($element['#value'], '/') !== 0) {
+        $form_state->setError($element, t('The "Link" field needs to be an internal URL that begins with a leading "/".'));
+        return;
+      }
+      $value = 'internal:' . trim($element['#value']);
+      $url = Url::fromUri($value);
+      if (!$url->isRouted()) {
+        $form_state->setError($element, t('The URL %url is not a valid internal path.', array('%url' => $element['#value'])));
+      }
+    }
+  }
+
 
   /**
    * {@inheritdoc}
@@ -248,14 +256,10 @@ ksm($config->get("{$name}_copy_2")['format']);
       ->set('intro_body', $form_state->getValue('intro_body'))
       ->set('intro_reg_btn', $form_state->getValue('intro_reg_btn'));
 
-    ksm($form_state);
-    ksm($config);
-
     // Create 3 info blocks.
     for ($i = 1; $i < 4; $i++) {
       $name = "info_block_{$i}";
       $fid = $form_state->getValue("{$name}_img");
-      // $config =
       $config
         ->set("{$name}_header", $form_state->getValue("{$name}_header"))
         ->set("{$name}_copy", $form_state->getValue("{$name}_copy"))
@@ -272,23 +276,7 @@ ksm($config->get("{$name}_copy_2")['format']);
         $file_usage->add($file, 'ymca_retention', "{$name}_img", $uid);
       }
     }
-    // $config
-    //   ->set()
-    // $this->config('ymca_retention.copy_settings')
-    //   ->set('date_campaign_open', $form_state->getValue('date_campaign_open'))
-    //   ->set('date_campaign_close', $form_state->getValue('date_campaign_close'))
-    //   ->set('date_registration_open', $form_state->getValue('date_registration_open'))
-    //   ->set('date_registration_close', $form_state->getValue('date_registration_close'))
-    //   ->set('date_reporting_open', $form_state->getValue('date_reporting_open'))
-    //   ->set('date_reporting_close', $form_state->getValue('date_reporting_close'))
-    //   ->set('date_winners_announcement', $form_state->getValue('date_winners_announcement'))
-    //   ->set('calculate_visit_goal', $form_state->getValue('calculate_visit_goal'))
-    //   ->set('new_member_goal_number', $form_state->getValue('new_member_goal_number'))
-    //   ->set('limit_goal_number', $form_state->getValue('limit_goal_number'))
-    //   ->set('date_checkins_start', $form_state->getValue('date_checkins_start'))
-    //   ->set('date_checkins_end', $form_state->getValue('date_checkins_end'))
-    //   ->set('recent_winners_limit', $form_state->getValue('recent_winners_limit'))
-    //   ->save();
+
     $config->save();
 
     parent::submitForm($form, $form_state);
