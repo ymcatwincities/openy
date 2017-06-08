62432c76dd1810ec3d39781c38afe8bd7f0a698e
John Kerbaugh <john.kerbaugh@ffwagency.com>
2017-06-08 15:17:44 -0400

    [OYPD-451] Move programs search URLs into configuration with admin form.

--- modules/custom/openy_programs_search/src/Form/SettingsForm.php
+++ modules/custom/openy_programs_search/src/Form/SettingsForm.php
@@ -32,33 +32,61 @@ class SettingsForm extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('openy_programs_search.settings');
 
-    $form['client_id'] = array(
+    /* @see \Drupal\openy_programs_search\DataStorage::getUrlFromOpenyProgramsSearchSettings */
+    $form['client_id'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Daxko Client ID'),
       '#default_value' => $config->get('client_id'),
       '#description' => t('Add your Daxko account id here. It is most likely a short number, like 1234.'),
-    );
+    ];
 
-    $form['base_url'] = array(
+    /* @see \Drupal\openy_programs_search\DataStorage::getDaxkoPageSource */
+    $form['domain'] = [
       '#type' => 'textfield',
+      '#title' => $this->t('Daxko Domain'),
+      '#default_value' => $config->get('domain'),
+      '#description' => t('Add your Daxko base url here. It is most likely daxko.com.'),
+    ];
+
+    /* @see \Drupal\openy_programs_search\DataStorage::getChildCareRegistrationLink */
+    /* @see \Drupal\openy_programs_search\DataStorage::getMapRateOptions */
+    $form['base_url'] = [
+      '#type' => 'url',
       '#title' => $this->t('Daxko Base URL'),
       '#default_value' => $config->get('base_url'),
-      '#element_validate' => [['UrlHelper', 'isValid']],
       '#description' => t('Add your Daxko base url here. It is most likely https://operations.daxko.com.'),
-    );
+    ];
 
+    /* @see \Drupal\openy_programs_search\DataStorage::getRegistrationLink */
     $form['registration_path'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Daxko Registration Path'),
-      '#default_value' => $config->get('registration_link'),
+      '#default_value' => $config->get('registration_path'),
       '#description' => t('Add your Daxko registration path. Something like /Online/{{ client_id }}/Programs/Search.mvc/details. Where the path will be prefixed by your Daxko Base URL and the {{ client_id }} token will be replaced by your Daxko Client ID.'),
     ];
 
+    /* @see \Drupal\openy_programs_search\DataStorage::scrapeDaxkoSchoolsByProgram */
     $form['get_schools_by_program_path'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Daxko Get Schools By Program Path'),
       '#default_value' => $config->get('get_schools_by_program_path'),
       '#description' => t('Add your Daxko scrape schools by program path. Something like /Online/{{ client_id }}/Programs/ChildCareSearch.mvc/locations_by_program?program_id={{ program_id }}. Where the path will be prefixed by your Daxko Base URL, the {{ client_id }} token will be replaced by your Daxko Client ID, and {{ program_id }} will be replaced by program IDs.'),
+    ];
+
+    /* @see \Drupal\openy_programs_search\DataStorage::getCategories */
+    $form['get_categories_path'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Daxko Categories Path'),
+      '#default_value' => $config->get('get_categories_path'),
+      '#description' => t('Add your Daxko Categories path. Something like /Online/{{ client_id }}/Programs/search.mvc/categories. Where the path will be prefixed by your Daxko Base URL, the {{ client_id }} token will be replaced by your Daxko Client ID.'),
+    ];
+
+    /* @see \Drupal\openy_programs_search\DataStorage::getMapCategoriesByBranch */
+    $form['get_map_categories_by_branch_path'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Daxko Map Categories By Branch Path'),
+      '#default_value' => $config->get('get_map_categories_by_branch_path'),
+      '#description' => t('Add your Daxko Map Categories By Branch path. Something like /Online/{{ client_id }}/Programs/search.mvc/categories?branch_id={{ branch_id }}. Where the path will be prefixed by your Daxko Base URL, the {{ client_id }} token will be replaced by your Daxko Client ID, and {{ branch_id }} will be replaced by branch IDs.'),
     ];
 
     return parent::buildForm($form, $form_state);
@@ -72,6 +100,7 @@ class SettingsForm extends ConfigFormBase {
     $config = \Drupal::service('config.factory')->getEditable('openy_programs_search.settings');
 
     $config->set('client_id', $form_state->getValue('client_id'))->save();
+    $config->set('domain', $form_state->getValue('domain'))->save();
 
     if ($base_url = $form_state->getValue('base_url')) {
       if (preg_match("#https?://#", $base_url) === 0) {
@@ -80,11 +109,16 @@ class SettingsForm extends ConfigFormBase {
       $config->set('base_url', $base_url)->save();
     }
 
-    $config->set('registration_link', $form_state->getValue('registration_link'))
+    $config->set('registration_path', $form_state->getValue('registration_path'))
       ->save();
     $config->set('get_schools_by_program_path', $form_state->getValue('get_schools_by_program_path'))
       ->save();
-
+    $config->set('domain', $form_state->getValue('domain'))
+      ->save();
+    $config->set('get_categories_path', $form_state->getValue('get_categories_path'))
+      ->save();
+    $config->set('get_map_categories_by_branch_path', $form_state->getValue('get_map_categories_by_branch_path'))
+      ->save();
 
     parent::submitForm($form, $form_state);
   }
