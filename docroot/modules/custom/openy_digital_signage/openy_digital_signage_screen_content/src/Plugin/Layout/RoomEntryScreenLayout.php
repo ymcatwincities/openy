eda9778c203399d1601c723026f6bd1055ef4baa
Dmitry Drozdik <dmitriydrozdik@gmail.com>
2017-06-08 22:01:46 +0300

    [YGB-49] Add  Room Entry Screen Layout

---
+++ docroot/modules/custom/openy_digital_signage/openy_digital_signage_screen_content/src/Plugin/Layout/RoomEntryScreenLayout.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Drupal\openy_digital_signage_screen_content\Plugin\Layout;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Layout\LayoutDefault;
+use Drupal\Core\Plugin\PluginFormInterface;
+
+/**
+ * Room Entry Screen layout settings.
+ */
+class RoomEntryScreenLayout extends LayoutDefault implements PluginFormInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return parent::defaultConfiguration() + [
+        'color_scheme' => 'orange',
+      ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $configuration = $this->getConfiguration();
+    $form['color_scheme'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Color Scheme'),
+      '#default_value' => $configuration['color_scheme'],
+      '#options' => [
+        'orange' => $this->t('Orange'),
+        'blue' => $this->t('Blue'),
+        'purple' => $this->t('Purple'),
+      ],
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateConfigurationForm(array &$form, FormStateInterface $form_state) {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    $this->configuration['color_scheme'] = $form_state->getValue('color_scheme');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(array $regions) {
+    $build = parent::build($regions);
+
+    return $build;
+  }
+
+}
