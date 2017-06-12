c7e527f740217ff85c7f33b926efa8dbeaa6ada4
Goran Miric <goran.miric@develomon.com>
2017-06-12 09:38:58 +0200

    [YLI-237]: FAQ paragraph.

---
+++ js/paragraph.js
@@ -0,0 +1,28 @@
+(function ($) {
+
+  /**
+   * Faq paragraph behaviour.
+   */
+  Drupal.behaviors.ymacaliFaqParagraph = {
+    attach: function( context, settings ) {
+      $( context ).find( '.paragraph.paragraph--type--faq' ).once( 'paragraphFaq' ).each( function () {
+        // Q/A wrapper.
+        var wrapper = $( this );
+
+        // Question click event.
+        $( '.field-question', wrapper ).on( 'click', function() {
+          var answer = $( '.field-answer', wrapper );
+
+          if ( answer.is( ':hidden') ) {
+            answer.slideDown( 200 );
+          }
+          else {
+            answer.slideUp( 200 );
+          }
+        });
+
+      });
+    }
+  };
+
+})(jQuery);
