e59d63833c9683c81589d44ad3988ff438b57467
Goran Miric <goran.miric89@gmail.com>
2017-06-14 16:22:44 +0200

    [OYPD-444]: contentsCss path update.

--- modules/custom/glyphicons/src/Plugin/CKEditorPlugin/Glyphicons.php
+++ modules/custom/glyphicons/src/Plugin/CKEditorPlugin/Glyphicons.php
@@ -35,7 +35,7 @@ class Glyphicons extends CKEditorPluginBase {
   public function getConfig(Editor $editor) {
     return [
       'allowedContent' => TRUE,
-      'contentsCss' => '/libraries/glyphicons/css/style.css',
+      'contentsCss' => \Drupal::request()->getSchemeAndHttpHost() . '/libraries/glyphicons/css/style.css',
     ];
   }
 
