4675ed692a078dc79035917a80269eeb957685b1
Dmitry Drozdik <dmitriydrozdik@gmail.com>
2017-06-21 15:29:20 +0300

    Update Ctools module.

--- docroot/modules/contrib/ctools/src/Plugin/RelationshipManagerInterface.php
+++ docroot/modules/contrib/ctools/src/Plugin/RelationshipManagerInterface.php
@@ -1,12 +1,8 @@
 <?php
-/**
- * @file
- * Contains \Drupal\ctools\Plugin\RelationshipManagerInterface.
- */
+
 namespace Drupal\ctools\Plugin;
 use Drupal\Component\Plugin\Discovery\CachedDiscoveryInterface;
 use Drupal\Core\Plugin\Context\ContextAwarePluginManagerInterface;
-
 
 /**
  * Provides the Relationship plugin manager.
