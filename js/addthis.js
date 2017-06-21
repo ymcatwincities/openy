54ab43c0d7efd20b65101321c0aa83d8d72d5015
Goran Miric <goran.miric@develomon.com>
2017-06-21 11:56:36 +0200

    [YLI-215]: AddThis share.

---
+++ js/addthis.js
@@ -0,0 +1,4 @@
+<!-- Go to www.addthis.com/dashboard to customize your tools -->
+var imported = document.createElement( 'script' );
+imported.src = '//s7.addthis.com/js/300/addthis_widget.js#pubid=' + drupalSettings.addThisId;
+document.head.appendChild( imported );
--- src/Form/OpenyAddThisSettingsForm.php
+++ src/Form/OpenyAddThisSettingsForm.php
@@ -2,13 +2,47 @@
 
 namespace Drupal\openy_addthis\Form;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Configure AddThis settings for this site.
  */
-class OpenyAddThisSettingsForm extends ConfigFormBase {
+class OpenyAddThisSettingsForm extends ConfigFormBase implements ContainerInjectionInterface {
+
+  /**
+   * The entity manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The Entity manager.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The factory for configuration objects.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, ConfigFactoryInterface $config_factory) {
+    parent::__construct($config_factory);
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager'),
+      $container->get('config.factory')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -31,11 +65,28 @@ class OpenyAddThisSettingsForm extends ConfigFormBase {
     $config = $this->config('openy_addthis.settings');
 
     $form['public_id'] = [
-      '#type' => 'textarea',
+      '#type' => 'textfield',
       '#title' => $this->t('AddThis public id'),
       '#default_value' => $config->get('public_id'),
       '#required' => TRUE,
-      '#description' => $this->t('Your AddThis public id.'),
+      '#description' => $this->t('Your AddThis public id. Example: ra-xxxxxxxxxxxxxxx'),
+    ];
+
+    // Load note types.
+    $nodeTypes = $this->entityTypeManager->getStorage('node_type')->loadMultiple();
+
+    // Build options list.
+    $options = [];
+    foreach ($nodeTypes as $machineName => $nodeType) {
+      $options[$machineName] = $nodeType->label();
+    }
+
+    $form['bundles'] = [
+      '#type' => 'checkboxes',
+      '#title' => $this->t('Content types'),
+      '#description' => $this->t('Select list of content types where addThis should be enabled by default.'),
+      '#options' => $options,
+      '#default_value' => $config->get('bundles')?: [],
     ];
 
     return parent::buildForm($form, $form_state);
@@ -48,6 +99,7 @@ class OpenyAddThisSettingsForm extends ConfigFormBase {
     // Save the config.
     $this->config('openy_addthis.settings')
       ->set('public_id', $form_state->getValue('public_id'))
+      ->set('bundles', array_filter($form_state->getValue('bundles')))
       ->save();
 
     parent::submitForm($form, $form_state);
