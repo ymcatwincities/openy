7a6ab7b199f968d5bdc6da6e5dd7e81b0da9d0c2
Goran Miric <goran.miric@develomon.com>
2017-06-21 13:07:56 +0200

    [YLI-215]: Code style update.

--- js/addthis.js
+++ js/addthis.js
@@ -1,4 +1,7 @@
-<!-- Go to www.addthis.com/dashboard to customize your tools -->
+/**
+ * @file
+ * AddThis script.
+ */
 var imported = document.createElement( 'script' );
 imported.src = '//s7.addthis.com/js/300/addthis_widget.js#pubid=' + drupalSettings.addThisId;
 document.head.appendChild( imported );
