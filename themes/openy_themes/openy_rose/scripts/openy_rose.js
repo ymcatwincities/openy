ffa3ac84400aaccb8ccc6c38f98398b7ffc7b128
davidhernandez <david@narrabilis.com>
2017-06-22 11:37:42 -0400

    Adding a JS function to move the top nav when the skip link is in focus

--- themes/openy_themes/openy_rose/scripts/openy_rose.js
+++ themes/openy_themes/openy_rose/scripts/openy_rose.js
@@ -192,4 +192,21 @@
     }
   };
 
+  /**
+   * Adjust the top nav position when the skip link is in focus.
+   */
+  Drupal.behaviors.adjustSkipLink = {
+    attach: function (context, settings) {
+      // On focus, move the top nav down to show the skip link.
+      $('.skip-link').on('focus', function () {
+        var link_height = $(this).height();
+        $('.top-navs').css({'margin-top': link_height});
+      });
+      // When focus is lost, remove the unneeded height.
+      $('.skip-link').on('focusout', function () {
+        $('.top-navs').css({'margin-top': '0'});
+      });
+    }
+  };
+
 })(jQuery);
