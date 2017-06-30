b22f8de5863012a45d4f5d0b786fa44ba4feb3dd
Dmitry Drozdik <dmitriydrozdik@gmail.com>
2017-06-30 21:12:23 +0300

    [YGB-53] Add Classes Session Entity.

---
+++ docroot/modules/custom/openy_digital_signage/openy_digital_signage_classes_schedule/src/Entity/OpenYClassesSessionViewsData.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\openy_digital_signage_classes_schedule\Entity;
+
+use Drupal\views\EntityViewsData;
+use Drupal\views\EntityViewsDataInterface;
+
+/**
+ * Provides Views data for Digital Signage Classes Session entities.
+ *
+ *  * @ingroup openy_digital_signage_classes_schedule
+ */
+class OpenYClassesSessionViewsData extends EntityViewsData implements EntityViewsDataInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getViewsData() {
+    $data = parent::getViewsData();
+
+    $data['openy_ds_classes_session']['table']['base'] = array(
+      'field' => 'id',
+      'title' => $this->t('Digital Signage Classes Session'),
+      'help' => $this->t('Digital Signage Classes Session ID.'),
+    );
+
+    return $data;
+  }
+
+}
