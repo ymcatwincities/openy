11b4b6576d21c194602eb60ff50b71995925e2d9
Andrey Maximov <savagge@gmail.com>
2017-06-30 16:55:34 +0300

    [YGB-44] Manage screen schedule

--- docroot/modules/custom/openy_digital_signage/openy_digital_signage_schedule/src/Form/OpenYScheduleItemForm.php
+++ docroot/modules/custom/openy_digital_signage/openy_digital_signage_schedule/src/Form/OpenYScheduleItemForm.php
@@ -5,6 +5,12 @@ namespace Drupal\openy_digital_signage_schedule\Form;
 use Drupal\Core\Entity\ContentEntityForm;
 use Drupal\Core\Form\FormStateInterface;
 
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\AppendCommand;
+use Drupal\Core\Ajax\RemoveCommand;
+use Drupal\Core\Ajax\InvokeCommand;
+use Drupal\Core\Url;
+
 /**
  * Form controller for OpenY Digital Signage Schedule Item edit forms.
  *
@@ -36,7 +42,55 @@ class OpenYScheduleItemForm extends ContentEntityForm {
     unset($form['show_date']);
     unset($form['date']);
 
+    $route_name = \Drupal::service('current_route_match')->getRouteName();
+
+    if (in_array($route_name, ['screen_schedule.edit_schedule_item', 'screen_schedule.add_schedule_item'])) {
+      // Add new screen content link.
+      $form['content']['widget'][0]['new_content'] = [
+        '#type' => 'link',
+        '#title' => $this->t('New screen content'),
+        '#url' => Url::fromRoute('openy_digital_signage_schedule.new_screen_content_form_modal'),
+        '#attributes' => [
+          'class' => [
+            'use-ajax',
+            'new-screen-content-link',
+          ],
+        ],
+        '#attached' => [
+          'library' => ['core/drupal.dialog.ajax'],
+        ],
+      ];
+
+      $form['actions']['submit']['#ajax'] = [
+        'callback' => '::ajaxFormSubmitHandler',
+      ];
+    }
+
     return $form;
+  }
+
+  public function addNewScreenContentCallback(array &$form, FormStateInterface $form_state) {
+    $response = new AjaxResponse();
+    $response->addCommand(new InvokeCommand('.timeline-redraw-link', 'click'));
+    return $response;
+  }
+
+  public function ajaxFormSubmitHandler(array &$form, FormStateInterface $form_state) {
+    // TODO: pass Screen entity context here.
+    $schedule_item = $this->entity;
+    $screen_content = $schedule_item->content->entity;
+    // Build an edit Schedule item form.
+    $build = [
+      '#type' => 'markup',
+      '#markup' => '<h3>' . $schedule_item->label() . '</h3><div data-src="/node/' . $screen_content->id() .'" class="frame-container"></div>',
+    ];
+
+    // Return the rendered form as a proper Drupal AJAX response.
+    $response = new AjaxResponse();
+    $response->addCommand(new RemoveCommand('.screen-schedule-ui--right > *'));
+    $response->addCommand(new AppendCommand('.screen-schedule-ui--right', $build));
+    $response->addCommand(new InvokeCommand('.timeline-redraw-link', 'click'));
+    return $response;
   }
 
   /**
