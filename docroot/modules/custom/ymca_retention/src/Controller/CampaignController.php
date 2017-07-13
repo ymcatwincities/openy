2e35cea2e3dbf87d98cf5362af02be8d5f528cf2
Matthew B Cannon <matthew.cannon@ffwagency.com>
2017-07-13 11:30:40 -0400

    YMCA-1655: Updating tab two with the MyProgress bar.

--- docroot/modules/custom/ymca_retention/src/Controller/CampaignController.php
+++ docroot/modules/custom/ymca_retention/src/Controller/CampaignController.php
@@ -22,7 +22,7 @@ class CampaignController extends ControllerBase {
     $info = [
       'started' => $diff->invert,
       'days_left' => $diff->days + 1,
-//      'dates' => $this->getDates(),
+      // 'dates' => $this->getDates(),
     ];
 
     $response = new JsonResponse($info);
@@ -32,6 +32,7 @@ class CampaignController extends ControllerBase {
 
   /**
    * Return array with all dates of campaign.
+   *
    * @todo seems unused anymore.
    */
   private function getDates() {
