fd3038b1e123211bdb0b13a8cac334088cdfa93a
Alex <alexandar87@gmail.com>
2017-07-26 14:27:33 +0200

    [YLI-264]: Override ajax scrolltop.

--- openy_lily/scripts/ymca.js
+++ openy_lily/scripts/ymca.js
@@ -193,4 +193,30 @@
     }
   };
 
+  /**
+   * Views scroll to top ajax command override.
+   */
+  Drupal.behaviors.scrollOffset = {
+    attach: function (context, settings) {
+      Drupal.AjaxCommands.prototype.viewsScrollTop = function (ajax, response) {
+        // Scroll to the top of the view. This will allow users
+        // to browse newly loaded content after e.g. clicking a pager
+        // link.
+        var offset = $(response.selector).offset();
+        // We can't guarantee that the scrollable object should be
+        // the body, as the view could be embedded in something
+        // more complex such as a modal popup. Recurse up the DOM
+        // and scroll the first element that has a non-zero top.
+        var scrollTarget = response.selector;
+        while ($(scrollTarget).scrollTop() === 0 && $(scrollTarget).parent()) {
+          scrollTarget = $(scrollTarget).parent();
+        }
+        // Only scroll upward.
+        if (offset.top - 10 < $(scrollTarget).scrollTop()) {
+          $(scrollTarget).animate({scrollTop: (offset.top - 230)}, 500);
+        }
+      };
+    }
+  };
+
 })(jQuery, Drupal, drupalSettings);
