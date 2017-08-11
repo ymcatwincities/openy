786666e5a7e04f6bc61ce50f52e828c4e5758fb0
Vladimir Selivanov <vladimirs@imagexmedia.com>
2017-08-11 18:55:40 +0300

    Member entity type

---
+++ modules/custom/openy_campaign/src/Entity/Member.php
@@ -0,0 +1,526 @@
+<?php
+
+namespace Drupal\openy_campaign\Entity;
+
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\openy_campaign\MemberInterface;
+use Drupal\openy_campaign\PersonifyApi;
+
+/**
+ * Defines the Member entity.
+ *
+ * @ingroup openy_campaign
+ *
+ * @ContentEntityType(
+ *   id = "openy_campaign_member",
+ *   label = @Translation("Member entity"),
+ *   handlers = {
+ *     "view_builder" = "Drupal\Core\Entity\EntityViewBuilder",
+ *     "list_builder" = "Drupal\openy_campaign\Entity\Controller\MemberListBuilder",
+ *     "views_data" = "Drupal\views\EntityViewsData",
+ *     "form" = {
+ *       "default" = "Drupal\openy_campaign\Form\MemberForm",
+ *       "add" = "Drupal\openy_campaign\Form\MemberForm",
+ *       "edit" = "Drupal\openy_campaign\Form\MemberForm",
+ *       "delete" = "Drupal\openy_campaign\Form\MemberDeleteForm",
+ *     },
+ *     "access" = "Drupal\openy_campaign\EntityAccess\MemberAccessControlHandler",
+ *   },
+ *   base_table = "openy_campaign_member",
+ *   admin_permission = "administer content types",
+ *   fieldable = TRUE,
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "label" = "membership_id",
+ *   },
+ *   links = {
+ *     "canonical" = "/admin/config/openy-entities/openy-campaign-member/{openy_campaign_member}",
+ *     "edit-form" = "/admin/config/openy-entities/openy-campaign-member/{openy_campaign_member}/edit",
+ *     "delete-form" = "/admin/config/openy-entities/openy-campaign-member/{openy_campaign_member}/delete",
+ *     "collection" = "/admin/config/openy-entities/openy-campaign-member/list"
+ *   },
+ * )
+ */
+class Member extends ContentEntityBase implements MemberInterface {
+
+  /**
+   * {@inheritdoc}
+   *
+   * Define the field properties here.
+   *
+   * Field name, type and size determine the table structure.
+   *
+   * In addition, we can define how the field and its content can be manipulated
+   * in the GUI. The behaviour of the widgets used can be determined here.
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+
+    // Standard field, used as unique if primary index.
+    $fields['id'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('ID'))
+      ->setDescription(t('The ID of the Membership entity.'))
+      ->setReadOnly(TRUE);
+
+    $fields['mail'] = BaseFieldDefinition::create('email')
+      ->setLabel(t('Email'))
+      ->setDescription(t('The email of this user.'))
+      ->setDefaultValue('')
+      ->setDisplayOptions('view', [
+        'label' => 'above',
+        'type' => 'string',
+        'weight' => -6,
+      ])
+      ->setDisplayOptions('form', [
+        'type' => 'string',
+        'weight' => -6,
+      ])
+      ->setRequired(TRUE)
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayConfigurable('view', TRUE);
+
+    // Email address field to store email address from Personify.
+    $fields['personify_email'] = BaseFieldDefinition::create('email')
+      ->setLabel(t('Email from Personify'))
+      ->setDescription(t('The email of this user from Personify.'))
+      ->setDefaultValue('')
+      ->setDisplayOptions('view', [
+        'label' => 'above',
+        'type' => 'string',
+        'weight' => -6,
+      ])
+      ->setDisplayOptions('form', [
+        'type' => 'string',
+        'weight' => -6,
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayConfigurable('view', TRUE);
+
+    // Membership ID field for the member.
+    $fields['membership_id'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Membership ID'))
+      ->setDescription(t('The id on the membership card.'))
+      ->setSettings([
+        'default_value' => '',
+        'max_length' => 255,
+        'text_processing' => 0,
+      ])
+      ->setDisplayOptions('view', [
+        'label' => 'above',
+        'type' => 'string',
+        'weight' => -6,
+      ])
+      ->setDisplayOptions('form', [
+        'type' => 'string',
+        'weight' => -6,
+      ])
+      ->setRequired(TRUE)
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayConfigurable('view', TRUE);
+
+    // Personify ID field for the member.
+    $fields['personify_id'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Personify ID'))
+      ->setDescription(t('The personify id of the member.'))
+      ->setSettings([
+        'default_value' => '',
+        'max_length' => 255,
+        'text_processing' => 0,
+      ]);
+
+    $fields['created'] = BaseFieldDefinition::create('created')
+      ->setLabel(t('Created'))
+      ->setDescription(t('The time that the record was created.'));
+
+    $fields['first_name'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('First name'))
+      ->setDefaultValue('')
+      ->setDescription(t('Member first name.'))
+      ->setSettings([
+        'default_value' => '',
+        'max_length' => 255,
+        'text_processing' => 0,
+      ])
+      ->setDisplayOptions('view', [
+        'label' => 'above',
+        'type' => 'string',
+        'weight' => -3,
+      ])
+      ->setDisplayOptions('form', [
+        'type' => 'string',
+        'weight' => -3,
+      ])
+      ->setRequired(TRUE)
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['last_name'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Last name'))
+      ->setDescription(t('Member last name.'))
+      ->setSettings([
+        'default_value' => '',
+        'max_length' => 255,
+        'text_processing' => 0,
+      ])
+      ->setDisplayOptions('view', [
+        'label' => 'above',
+        'type' => 'string',
+        'weight' => -3,
+      ])
+      ->setDisplayOptions('form', [
+        'label' => 'above',
+        'type' => 'string',
+        'weight' => -3,
+      ])
+      ->setRequired(TRUE)
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['birth_date'] = BaseFieldDefinition::create('datetime')
+      ->setLabel(t('Birthday'))
+      ->setDescription(t('The date of birth.'))
+      ->setSetting('datetime_type', 'date');
+
+    $fields['is_employee'] = BaseFieldDefinition::create('boolean')
+      ->setLabel(t('User is an employee'))
+      ->setDefaultValue(FALSE)
+      ->setDisplayOptions('view', [
+        'label' => 'above',
+        'type' => 'boolean',
+        'weight' => -1,
+      ])
+      ->setDisplayOptions('form', [
+        'type' => 'boolean',
+        'weight' => -1,
+      ])
+      ->setSettings([
+        'on_label' => t('Member is an employee.'),
+        'off_label' => t('Member is not an employee.'),
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['branch'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Branch'))
+      ->setDescription(t('Member branch ID.'))
+      ->setSettings([
+        'target_type' => 'mapping',
+        'default_value' => 0,
+      ]);
+
+    $fields['visit_goal'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('Visit Goal'))
+      ->setDescription(t('Member visit goal.'))
+      ->setSettings([
+        'default_value' => 0,
+        'max_length' => 255,
+        'text_processing' => 0,
+      ])
+      ->setDisplayOptions('view', [
+        'label' => 'above',
+        'type' => 'string',
+        'weight' => -1,
+      ])
+      ->setDisplayOptions('form', [
+        'type' => 'string',
+        'weight' => -1,
+      ])
+      ->setDefaultValue(0)
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['total_visits'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('Visits'))
+      ->setDescription(t('Number of visits.'))
+      ->setDefaultValue(0);
+
+    $fields['total_bonuses'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('Bonuses'))
+      ->setDescription(t('Number of bonuses.'))
+      ->setDefaultValue(0);
+
+    $fields['created_by_staff'] = BaseFieldDefinition::create('boolean')
+      ->setLabel(t('Created by Staff'))
+      ->setDescription(t('Created by Staff'))
+      ->setDefaultValue(FALSE);
+
+    $fields['created_on_mobile'] = BaseFieldDefinition::create('boolean')
+      ->setLabel(t('Created using Mobile app'))
+      ->setDescription(t('Created using Mobile app'))
+      ->setDefaultValue(FALSE);
+
+    $fields['activity_track_swimming'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('Swimming'))
+      ->setDescription(t('Number of Swimming activities.'))
+      ->setDefaultValue(0);
+
+    $fields['activity_track_fitness'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('Fitness'))
+      ->setDescription(t('Number of Fitness activities.'))
+      ->setDefaultValue(0);
+
+    $fields['activity_track_groupx'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('Group X'))
+      ->setDescription(t('Number of Group X activities.'))
+      ->setDefaultValue(0);
+
+    $fields['activity_track_community'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('Community'))
+      ->setDescription(t('Number of Community activities.'))
+      ->setDefaultValue(0);
+
+    return $fields;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getId() {
+    return $this->get('id')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getEmail() {
+    return $this->get('mail')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setEmail($mail) {
+    $this->set('mail', $mail);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPersonifyEmail() {
+    return $this->get('personify_email')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setPersonifyEmail($email) {
+    $this->set('personify_email', $email);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getMemberId() {
+    return $this->get('membership_id')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPersonifyId() {
+    return $this->get('personify_id')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setMemberId($member_id) {
+    $this->set('membership_id', $member_id);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFirstName() {
+    return $this->get('first_name')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setFirstName($value) {
+    $this->set('first_name', $value);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getLastName() {
+    return $this->get('last_name')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setLastName($value) {
+    $this->set('last_name', $value);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFullName() {
+    $name = $this->getFirstName();
+    $name .= ' ';
+    $name .= $this->getLastName();
+
+    return $name;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getBranchId() {
+    return $this->get('branch')->target_id;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setBranchId($target_id) {
+    return $this->set('branch', $target_id);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getVisits() {
+    return $this->get('total_visits')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getBonuses() {
+    return $this->get('total_bonuses')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setVisits($value) {
+    $this->set('total_visits', $value);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setBonuses($value) {
+    $this->set('total_bonuses', $value);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isMemberEmployee() {
+    return $this->get('is_employee')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isCreatedByStaff() {
+    return $this->get('created_by_staff')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isCreatedOnMobile() {
+    return $this->get('created_on_mobile')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getVisitGoal() {
+    return $this->get('visit_goal')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setVisitGoal($value) {
+    $this->set('visit_goal', $value);
+    return $this;
+  }
+
+  /**
+   * Get member rank.
+   *
+   * @return int
+   *   Member rank.
+   */
+  public function getMemberRank() {
+    return 0;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function calculateVisitGoal($member_ids) {
+    $goals = [];
+    $settings = \Drupal::config('openy_campaign.general_settings');
+    // Get information about number of checkins before campaign.
+    $current_date = new \DateTime();
+    $from_date = new \DateTime($settings->get('date_checkins_start'));
+    $to_date = new \DateTime($settings->get('date_checkins_end'));
+
+    if ($to_date > $current_date) {
+      $to_date = $current_date;
+    }
+    $number_weeks = ceil($from_date->diff($to_date)->days / 7);
+
+    $results = PersonifyApi::getPersonifyVisitsBatch($member_ids, $from_date, $to_date);
+    if (!empty($results->ErrorMessage)) {
+      $logger = \Drupal::logger('openy_campaign_queue');
+      $logger->alert('Could not retrieve visits information for members for batch operation');
+      return [];
+    }
+
+    foreach ($results->FacilityVisitCustomerRecord as $past_result) {
+      // Get first visit date.
+      try {
+        $first_visit = new \DateTime($past_result->FirstVisitDate);
+      }
+      catch (\Exception $e) {
+        $first_visit = $from_date;
+      }
+
+      $member_weeks = $number_weeks;
+      // If user registered after From date, then recalculate number of weeks.
+      if ($first_visit > $from_date) {
+        $member_weeks = ceil($first_visit->diff($to_date)->days / 7);
+      }
+
+      // Calculate a goal for a member.
+      $goal = (int) $settings->get('new_member_goal_number');
+      if ($past_result->TotalVisits > 0) {
+        $limit_goal = $settings->get('limit_goal_number');
+        $calculated_goal = ceil((($past_result->TotalVisits / $member_weeks) * 2) + 1);
+        $goal = min(max($goal, $calculated_goal), $limit_goal);
+      }
+
+      // Visit goal for late members.
+      $close_date = new \DateTime($settings->get('date_campaign_close'));
+      $count_days = $current_date->diff($close_date)->days;
+      // Set visit goal not greater than number of days till the campaign end.
+      // TODO: is it correct? As we should still be able to get his visits for
+      // the period of campaign.
+      $count_days = max(1, $count_days);
+      $goal = min($goal, $count_days);
+      $goals[$past_result->MasterCustomerId] = $goal;
+    }
+
+    return $goals;
+  }
+
+}
---
+++ modules/custom/openy_campaign/src/Form/SettingsForm.php
@@ -0,0 +1,232 @@
+<?php
+
+namespace Drupal\openy_campaign\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides form for managing module settings.
+ */
+class SettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'openy_campaign_general_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['openy_campaign.general_settings'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('openy_campaign.general_settings');
+
+    $form['date_winners_announcement'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Winners announcement date and time'),
+      '#size' => 50,
+      '#maxlength' => 500,
+      '#default_value' => $config->get('date_winners_announcement'),
+      '#description' => $this->t('Date and time when winners will be announced.'),
+    ];
+
+    $form['calculate_visit_goal'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Calculate visit goal'),
+      '#default_value' => $config->get('calculate_visit_goal'),
+    ];
+
+    $form['new_member_goal_number'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Goal of visits for new members'),
+      '#size' => 50,
+      '#maxlength' => 500,
+      '#default_value' => $config->get('new_member_goal_number'),
+      '#description' => $this->t('Goal of visits in this campaign for new members.'),
+    ];
+
+    $form['limit_goal_number'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Limit goal of visits'),
+      '#size' => 50,
+      '#maxlength' => 500,
+      '#default_value' => $config->get('limit_goal_number'),
+      '#description' => $this->t('Limit goal of visits in this campaign.'),
+    ];
+
+    $form['date_checkins_start'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Check-ins start date and time'),
+      '#size' => 50,
+      '#maxlength' => 500,
+      '#default_value' => $config->get('date_checkins_start'),
+      '#description' => $this->t('Start date and time, for getting data about check-ins in past months, before the campaign starts.'),
+    ];
+
+    $form['date_checkins_end'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Check-ins end date and time'),
+      '#size' => 50,
+      '#maxlength' => 500,
+      '#default_value' => $config->get('date_checkins_end'),
+      '#description' => $this->t('End date and time, for getting data about check-ins in past months, before the campaign starts.'),
+    ];
+
+    $form['recent_winners_limit'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Recent winners limit'),
+      '#size' => 50,
+      '#maxlength' => 500,
+      '#default_value' => $config->get('recent_winners_limit'),
+      '#description' => $this->t('How many winners to show in the recent winners block.'),
+    ];
+
+    $excludes = $config->get('exclude_reg_product_codes');
+    if (!empty($excludes) && is_array($excludes)) {
+      $excludes = static::simplifyAllowedValues($config->get('exclude_reg_product_codes'));
+    }
+    $form['exclude_reg_product_codes'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Exclude Registration Product Codes'),
+      '#default_value' => $excludes,
+      '#description' => $this->t('Enter product codes, one per line. Example Code: <em>14_GETSUMMER</em>'),
+    ];
+
+    $form['error_msg_excluded_members'] = [
+      '#type' => 'text_format',
+      '#title' => $this->t('Excluded Members Error'),
+      '#default_value' => $config->get('error_msg_excluded_members')['value'],
+      '#format' => $config->get('error_msg_excluded_members')['format'],
+      '#description' => $this->t('Message to display if ProductCode is on the excluded list.'),
+    ];
+
+    $form['error_msg_incorrect_id'] = [
+      '#type' => 'text_format',
+      '#title' => $this->t('Error Message: Incorrect Member ID'),
+      '#default_value' => $config->get('error_msg_incorrect_id')['value'],
+      '#format' => $config->get('error_msg_incorrect_id')['format'],
+      '#description' => $this->t('Message to display if user is trying to register with an incorrect member ID.'),
+    ];
+
+    $form['error_msg_registered_before_start'] = [
+      '#type' => 'text_format',
+      '#title' => $this->t('Error Message: Already Registered Before Start'),
+      '#default_value' => $config->get('error_msg_registered_before_start')['value'],
+      '#format' => $config->get('error_msg_registered_before_start')['format'],
+      '#description' => $this->t('Message to display if user is trying to register again before the start of the campaign.'),
+    ];
+
+    $form['error_msg_registered_after_start'] = [
+      '#type' => 'text_format',
+      '#title' => $this->t('Error Message: Already Registered After Start'),
+      '#default_value' => $config->get('error_msg_registered_after_start')['value'],
+      '#format' => $config->get('error_msg_registered_after_start')['format'],
+      '#description' => $this->t('Message to display if user is trying to register again after the start of the campaign.'),
+    ];
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * Checks whether a candidate allowed value is valid.
+   *
+   * @param string $option
+   *   The option value entered by the user.
+   */
+  protected static function validateAllowedValue($option) {}
+
+  /**
+   * Simplifies allowed values to a key-value array from the structured array.
+   *
+   * @param array $structured_values
+   *   Array of items with a 'value' and 'label' key each for the allowed
+   *   values.
+   *
+   * @return string
+   *   Formatted back for the textarea.
+   */
+  protected static function simplifyAllowedValues(array $structured_values) {
+    return implode("\n", $structured_values);
+  }
+
+  /**
+   * Extracts the allowed values array from the allowed_values element.
+   *
+   * @param string $string
+   *   The raw string to extract values from.
+   *
+   * @return array|null
+   *   The array of extracted key/value pairs, or NULL if the string is invalid.
+   *
+   * @see \Drupal\options\Plugin\Field\FieldType\ListItemBase::allowedValuesString()
+   */
+  protected static function extractAllowedValues($string) {
+    $values = [];
+
+    $list = explode("\n", $string);
+    $list = array_map('trim', $list);
+    $list = array_filter($list, 'strlen');
+
+    $generated_keys = $explicit_keys = FALSE;
+    foreach ($list as $position => $text) {
+      // Check for an explicit key.
+      $matches = [];
+      if (preg_match('/(.*)\|(.*)/', $text, $matches)) {
+        // Trim key and value to avoid unwanted spaces issues.
+        $key = trim($matches[1]);
+        $value = trim($matches[2]);
+        $explicit_keys = TRUE;
+      }
+      // Otherwise see if we can use the value as the key.
+      elseif (!static::validateAllowedValue($text)) {
+        $key = $value = $text;
+        $explicit_keys = TRUE;
+      }
+      else {
+        return;
+      }
+
+      $values[$key] = $value;
+    }
+
+    // We generate keys only if the list contains no explicit key at all.
+    if ($explicit_keys && $generated_keys) {
+      return;
+    }
+
+    return $values;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $excluded_product_codes  = static::extractAllowedValues($form_state->getValue('exclude_reg_product_codes'));
+    $this->config('openy_campaign.general_settings')
+      ->set('date_winners_announcement', $form_state->getValue('date_winners_announcement'))
+      ->set('calculate_visit_goal', $form_state->getValue('calculate_visit_goal'))
+      ->set('new_member_goal_number', $form_state->getValue('new_member_goal_number'))
+      ->set('limit_goal_number', $form_state->getValue('limit_goal_number'))
+      ->set('date_checkins_start', $form_state->getValue('date_checkins_start'))
+      ->set('date_checkins_end', $form_state->getValue('date_checkins_end'))
+      ->set('recent_winners_limit', $form_state->getValue('recent_winners_limit'))
+      ->set('exclude_reg_product_codes', $excluded_product_codes)
+      ->set('error_msg_excluded_members', $form_state->getValue('error_msg_excluded_members'))
+      ->set('error_msg_incorrect_id', $form_state->getValue('error_msg_incorrect_id'))
+      ->set('error_msg_registered_before_start', $form_state->getValue('error_msg_registered_before_start'))
+      ->set('error_msg_registered_after_start', $form_state->getValue('error_msg_registered_after_start'))
+      ->save();
+
+    parent::submitForm($form, $form_state);
+  }
+
+}
