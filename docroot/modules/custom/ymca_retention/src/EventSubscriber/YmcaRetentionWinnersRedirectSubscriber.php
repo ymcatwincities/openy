edd59fd8ec8de949d159a032f413581726926e6e
Denis Golovnyak <denis.golovnyak@blinkreaction.com>
2017-08-15 11:31:23 +0300

    adds logic for upcoming and winners page redirects

--- docroot/modules/custom/ymca_retention/src/EventSubscriber/YmcaRetentionWinnersRedirectSubscriber.php
+++ docroot/modules/custom/ymca_retention/src/EventSubscriber/YmcaRetentionWinnersRedirectSubscriber.php
@@ -2,7 +2,9 @@
 
 namespace Drupal\ymca_retention\EventSubscriber;
 
+use Drupal\Core\Url;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
@@ -29,22 +31,37 @@ class YmcaRetentionWinnersRedirectSubscriber implements EventSubscriberInterface
    *   Event.
    */
   public function redirectToWinnersPage(GetResponseEvent $event) {
-    $route = \Drupal::service('current_route_match')->getRouteName();
+    $routes = array(
+      'challenge' => 'page_manager.page_view_ymca_retention_challenge_ymca_retention_challenge',
+      'upcoming' => 'page_manager.page_view_ymca_retention_challenge_pages_ymca_retention_challenge_upcoming',
+      'winners' => 'page_manager.page_view_ymca_retention_challenge_pages_ymca_retention_challenge_winners',
+    );
+    $current_route = \Drupal::service('current_route_match')->getRouteName();
 
-    if (!in_array($route, [
-      'page_manager.page_view_ymca_retention_challenge_ymca_retention_challenge',
-      'page_manager.page_view_ymca_retention_challenge_pages_ymca_retention_challenge_winners',
-    ])
-    ) {
+    if (!in_array($current_route, $routes)) {
       return;
     }
-    /*
-    $url = Url::fromRoute('page_manager.page_view_ymca_retention_challenge_ymca_retention_challenge');
-    $url = Url::fromRoute('page_manager.page_view_ymca_retention_challenge_pages_ymca_retention_challenge_winners');
-    $response = new RedirectResponse($url->toString() . '/upcoming', 302);
-    $response = new RedirectResponse($url->toString() . '/winners', 302);
-    $event->setResponse($response);
-     */
+
+    $route_id = array_search($current_route, $routes);
+    $settings = \Drupal::config('ymca_retention.general_settings');
+    $current_date = new \DateTime();
+    $date_winners_announcement = new \DateTime($settings->get('date_winners_announcement'));
+    $date_campaign_close = new \DateTime($settings->get('date_campaign_close'));
+    $url = Url::fromRoute($routes['challenge']);
+
+    if ($current_date > $date_campaign_close) {
+      if ($current_date > $date_winners_announcement && 'winners' == $route_id) {
+        return;
+      }
+
+      if ($current_date < $date_winners_announcement && 'upcoming' == $route_id) {
+        return;
+      }
+
+      $redirect_url = $current_date > $date_winners_announcement ? '/winners' : '/upcoming';
+      $response = new RedirectResponse($url->toString() . $redirect_url, 302);
+      $event->setResponse($response);
+    }
   }
 
 }
