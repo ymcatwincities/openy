26bb2b793c71d4f72a84d912ed4a0bff6e8bc4e5
AndreyMaximov <savagge@gmail.com>
2017-08-15 17:12:22 +0300

    [YGB-119] "GroupEx Pro import verify removed sessions"

--- docroot/modules/custom/openy_digital_signage/openy_digital_signage_classes_schedule/modules/openy_digital_signage_groupex_schedule/src/OpenYSessionsGroupExFetcherInterface.php
+++ docroot/modules/custom/openy_digital_signage/openy_digital_signage_classes_schedule/modules/openy_digital_signage_groupex_schedule/src/OpenYSessionsGroupExFetcherInterface.php
@@ -10,6 +10,21 @@ namespace Drupal\openy_digital_signage_groupex_schedule;
 interface OpenYSessionsGroupExFetcherInterface {
 
   /**
+   * Get default options for a request to GroupEx Pro.
+   *
+   * @return array
+   *   Default options for a request.
+   */
+  public function defaultOptions();
+
+  /**
+   * Retrieves list of locations ids to be imported.
+   *
+   * @return array
+   */
+  public function getLocations();
+
+  /**
    * Import sessions for specific location.
    *
    * @param int $location_id
@@ -21,5 +36,41 @@ interface OpenYSessionsGroupExFetcherInterface {
    * Fetch from GroupEx Pro.
    */
   public function fetchAll();
+
+  /**
+   * Check if there are entities to be deleted.
+   *
+   * @param array $feed
+   *   Pulled GroupEx pro feed.
+   * @param int $location_id
+   *   The location id.
+   *
+   * @return array
+   *   The array on entity ids to be deleted.
+   */
+  public function checkDeleted($feed, $location_id);
+
+  /**
+   * Removes entities by chunks.
+   *
+   * @param array $feed
+   *   Pulled GroupEx pro feed.
+   * @param int $location_id
+   *   The location id.
+   *
+   * @return array
+   *   The array on entity ids to be deleted.
+   */
+  public function removeDeleted($ids);
+
+  /**
+   * Create or update sessions.
+   *
+   * @param array $data
+   *   Data received from GroupEx Pro.
+   * @param int $location_id
+   *   Location node id.
+   */
+  public function processData(array $data, $location_id);
 
 }
