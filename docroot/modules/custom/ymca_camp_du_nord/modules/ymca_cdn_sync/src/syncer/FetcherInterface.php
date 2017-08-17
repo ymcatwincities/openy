41d56aa68cf3ee86cd994d461fbf8941992af7ac
Dmytro Danylevskyi <dmytro@danylevskyi.com>
2017-08-17 10:27:17 +0300

    [YTCDN-34] Add initial file structure and entity

--- docroot/modules/custom/ymca_camp_du_nord/modules/ymca_cdn_sync/src/syncer/FetcherInterface.php
+++ docroot/modules/custom/ymca_camp_du_nord/modules/ymca_cdn_sync/src/syncer/FetcherInterface.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace Drupal\ymca_camp_du_nord_sync\syncer;
+namespace Drupal\ymca_cdn_sync\syncer;
 
 /**
  * Interface FetcherInterface.
  *
- * @package Drupal\ymca_camp_du_nord\syncer
+ * @package Drupal\ymca_cdn_sync\syncer
  */
 interface FetcherInterface {
 
