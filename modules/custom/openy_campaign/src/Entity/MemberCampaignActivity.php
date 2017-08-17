c2b1f93d6529f1a8e9681485d0ed67e4c63eb966
Yuriy Gerasimov <yuri.gerasimov@gmail.com>
2017-08-17 14:28:33 -0700

    Block, paragraph, demo content. Work in progress.

---
+++ modules/custom/openy_campaign/src/Entity/MemberCampaignActivity.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ *
+ */
+
+namespace Drupal\openy_campaign\Entity;
+
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\EntityTypeInterface;
+
+use Drupal\openy_campaign\MemberCampaignActivityInterface;
+
+/**
+ * Track participant's check-ins and activities.
+ *
+ * @ingroup openy_campaign
+ *
+ * @ContentEntityType(
+ *   id = "openy_member_campaign_activity",
+ *   label = @Translation("Member Activity entity"),
+ *   handlers = {
+ *     "views_data" = "Drupal\views\EntityViewsData",
+ *   },
+ *   base_table = "openy_member_campaign_activity",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "label" = "id",
+ *   },
+ * )
+ */
+class MemberCampaignActivity  extends ContentEntityBase implements MemberCampaignActivityInterface {
+
+  /**
+   * Types of entity.
+   */
+  const TYPE_CHECKIN  = 0;
+  const TYPE_ACTIVITY = 1;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+
+    // Standard field, used as unique if primary index.
+    $fields['id'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('ID'))
+      ->setDescription(t('The ID of the Activity entity.'))
+      ->setReadOnly(TRUE);
+
+    $fields['created'] = BaseFieldDefinition::create('created')
+      ->setLabel(t('Created'))
+      ->setDescription(t('The time the record was created.'));
+
+    $fields['date'] = BaseFieldDefinition::create('timestamp')
+      ->setLabel(t('Date'))
+      ->setDescription(t('The timestamp for the day when activity was logged.'));
+
+    $fields['member'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Campaign Member ID'))
+      ->setDescription(t('The campaign member ID.'))
+      ->setSettings([
+        'target_type' => 'openy_campaign_member',
+        'default_value' => 0,
+      ]);
+
+    $fields['type'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('Either facility check in or activity.'))
+      ->setDefaultValue(FALSE)
+      ->setDisplayOptions('view', [
+        'label' => 'above',
+        'type' => 'integer',
+        'weight' => -1,
+      ]);
+
+    $fields['activity'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Activity term'))
+      ->setDescription(t('What activity this record is for.'))
+      ->setSettings([
+        'target_type' => 'taxonomy_term',
+        'default_value' => 0,
+      ]);
+
+    return $fields;
+  }
+}
---
+++ modules/custom/openy_campaign/src/Plugin/Block/CampaignActivityBlock.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Drupal\openy_campaign\Plugin\Block;
+
+use Drupal\Core\Form\FormBuilderInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Block\BlockBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a 'Activity Tracking' block.
+ *
+ * @Block(
+ *   id = "campaign_activity_block",
+ *   admin_label = @Translation("Campaign Activity Tracking"),
+ *   category = @Translation("Paragraph Blocks")
+ * )
+ */
+class CampaignActivityBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Form builder.
+   *
+   * @var \Drupal\Core\Form\FormBuilderInterface
+   */
+  protected $formBuilder;
+
+  /**
+   * Constructs a new Block instance.
+   *
+   * @param array $configuration
+   *   The plugin configuration.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Form\FormBuilderInterface $formBuilder
+   *   Form builder.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, FormBuilderInterface $formBuilder) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->formBuilder = $formBuilder;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('form_builder')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+    // @TODO: Get the ID of campaign.
+    $campaignId = 68; // Node id of Campaign.
+
+    $form = $this->formBuilder->getForm('Drupal\openy_campaign\Form\ActivityBlockForm', $campaignId);
+    return [
+      'form' => $form,
+    ];
+  }
+
+}
