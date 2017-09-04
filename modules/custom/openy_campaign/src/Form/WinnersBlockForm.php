86656b12ce7e935dfb71f08dd6b3b653a755f869
Vladimir Selivanov <vladimirs@imagexmedia.com>
2017-09-04 12:48:03 +0300

    Winners block init

---
+++ modules/custom/openy_campaign/src/Form/WinnersBlockForm.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Drupal\openy_campaign\Form;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Render\RendererInterface;
+
+/**
+ * Provides a "openy_campaign_winners_block_form" form.
+ */
+class WinnersBlockForm extends FormBase {
+
+  /**
+   * Renderer.
+   *
+   * @var \Drupal\Core\Render\RendererInterface
+   */
+  protected $renderer;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+
+  /**
+   * CalcBlockForm constructor.
+   *
+   * @param \Drupal\Core\Render\RendererInterface $renderer
+   *   Renderer.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct(RendererInterface $renderer, EntityTypeManagerInterface $entity_type_manager) {
+    $this->renderer = $renderer;
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('renderer'),
+      $container->get('entity_type.manager')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'openy_campaign_winners_block_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, $campaignId = NULL) {
+    // Get all regions - branches
+    $branches = [];
+
+    // Ajax reload blocks with winners
+    $winners = [];
+    $winners = $this->getCampaignWinners($campaignId);
+    $prizes = [];
+    $prizes = $this->getCampaignPrizes($campaignId);
+
+
+
+    // Attach the library for pop-up dialogs/modals.
+    $form['#attached']['library'][] = 'core/drupal.dialog.ajax';
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+  }
+
+  protected function getCampaignWinners($campaignId) {
+    $winners = [];
+
+    return $winners;
+  }
+
+  protected function getCampaignPrizes($campaignId) {
+    $prizes = [];
+
+    return $prizes;
+  }
+
+}
