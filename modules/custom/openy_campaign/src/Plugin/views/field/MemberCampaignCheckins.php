1117f9aaed1b3dd11cdcd3a84b52882ad1493a2e
Alexander Kolesnikov <kolesnikoff@gmail.com>
2017-09-05 11:07:00 +0300

    Generate members list by campaign.

---
+++ modules/custom/openy_campaign/src/Plugin/views/field/MemberCampaignCheckins.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\d8views\Plugin\views\field\MemberCampaignCheckins
+ */
+
+namespace Drupal\openy_campaign\Plugin\views\field;
+
+use Drupal\openy_campaign\Entity\MemberCheckin;
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\ResultRow;
+
+/**
+ * Checkins handler for the member entity.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("member_campaign_checkins")
+ */
+class MemberCampaignCheckins extends FieldPluginBase {
+
+  /**
+   * @{inheritdoc}
+   */
+  public function query() {
+    // Leave empty to avoid a query on this field.
+  }
+
+  /**
+   * @{inheritdoc}
+   */
+  public function render(ResultRow $values) {
+    /** @var \Drupal\openy_campaign\Entity\MemberCampaign $entity */
+    $entity = $values->_entity;
+    
+    /** @var \Drupal\openy_campaign\Entity\Member $member */
+    $member = $entity->getMember();
+    
+    /** @var \Drupal\node\Entity\Node $campaign */
+    $campaign = $entity->getCampaign();
+
+    /** @var \DateTime $start */
+    $start = $campaign->field_campaign_start_date->date;
+    // Reset time to include the current day to the list.
+    $start->setTime(0, 0, 0);
+
+    /** @var \DateTime $end */
+    $end = $campaign->field_campaign_end_date->date;
+
+    $facilityCheckInIds = MemberCheckin::getFacilityCheckIns($member->id(), $start, $end);
+
+    return count($facilityCheckInIds);
+
+  }
+}
