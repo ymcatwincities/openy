c9daeb4fa9d5601d55836d49ef0cf4280140b726
Dmytro Danylevskyi <dmytro@danylevskyi.com>
2017-09-13 16:35:51 +0300

    Add templates

--- modules/openy_features/openy_block/modules/openy_block_expander/src/Controller/DemoController.php
+++ modules/openy_features/openy_block/modules/openy_block_expander/src/Controller/DemoController.php
@@ -13,6 +13,12 @@ class DemoController extends ControllerBase {
    * {@inheritdoc}
    */
   public function content() {
+//    $render = [
+//      '#theme' => 'page_expander',
+//      'block' => [
+//      ]
+//    ];
+
     $block = \Drupal\block_content\Entity\BlockContent::load(3);
     $render = \Drupal::entityTypeManager()->getViewBuilder('block_content')->view($block);
 
