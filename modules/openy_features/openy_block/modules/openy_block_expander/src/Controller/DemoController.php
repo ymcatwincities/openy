bac90ef3557d10f90f3659d004990f1c0fdb146e
Dmytro Danylevskyi <dmytro@danylevskyi.com>
2017-09-18 15:58:51 +0300

    Add theme nogotiator

--- modules/openy_features/openy_block/modules/openy_block_expander/src/Controller/DemoController.php
+++ modules/openy_features/openy_block/modules/openy_block_expander/src/Controller/DemoController.php
@@ -3,26 +3,38 @@
 namespace Drupal\openy_block_expander\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Theme\ThemeNegotiatorInterface;
 
 /**
  * An example controller.
  */
-class DemoController extends ControllerBase {
+class DemoController extends ControllerBase implements ThemeNegotiatorInterface {
 
   /**
    * {@inheritdoc}
    */
   public function content() {
-//    $render = [
-//      '#theme' => 'page_expander',
-//      'block' => [
-//      ]
-//    ];
+    // -> form
+    // @todo Create class with the form in Form namespace.
+    // @todo implement logic for setting different theme depending on selection.
 
+    // -> here
+    // @todo use form_builder service to render the form here.
     $block = \Drupal\block_content\Entity\BlockContent::load(3);
     $render = \Drupal::entityTypeManager()->getViewBuilder('block_content')->view($block);
 
     return $render;
+  }
+
+  public function applies(RouteMatchInterface $route_match) {
+    // config.
+    return TRUE;
+  }
+
+  public function determineActiveTheme(RouteMatchInterface $route_match) {
+    // select from config
+    return 'stark';
   }
 
 }
