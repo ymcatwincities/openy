7531209c4aface2e329b9d9cfd13b9f83d3c53de
dimb <dimb.main@gmail.com>
2017-09-26 01:39:57 +0300

    Main page basics wrappers, coutndown

---
+++ modules/custom/openy_campaign/js/campaignCountdown.js
@@ -0,0 +1,35 @@
+Drupal.behaviors.campaignCountdown = {
+    attach: function (context, settings) {
+        if (Drupal.behaviors.campaignCountdown.length){
+            return;
+        }
+        simplyCountdown('.countdown', {
+            year: 2020, // required
+            month: 6, // required
+            day: 28, // required
+            hours: 0, // Default is 0 [0-23] integer
+            minutes: 0, // Default is 0 [0-59] integer
+            seconds: 0, // Default is 0 [0-59] integer
+            words: { //words displayed into the countdown
+                days: 'day',
+                hours: 'hour',
+                minutes: 'minute',
+                seconds: 'second',
+                pluralLetter: 's'
+            },
+            plural: true, //use plurals
+            inline: false, //set to true to get an inline basic countdown like : 24 days, 4 hours, 2 minutes, 5 seconds
+            inlineClass: 'simply-countdown-inline', //inline css span class in case of inline = true
+            // in case of inline set to false
+            enableUtc: true,
+            onEnd: function () {
+                // your code
+                return;
+            },
+            refresh: 1000, //default refresh every 1s
+            sectionClass: 'simply-section', //section css class
+            amountClass: 'simply-amount', // amount css class
+            wordClass: 'simply-word' // word css class
+        });
+    }
+};
