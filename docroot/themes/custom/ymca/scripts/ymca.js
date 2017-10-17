eafd4b5f4964ab117a410a9f0db51baea716f127
Andrii TIupa <andrii.tiupa@ffwagency.com>
2017-10-17 13:31:55 +0300

    [YTCDN-46] fixes for Emergency form

--- docroot/themes/custom/ymca/scripts/ymca.js
+++ docroot/themes/custom/ymca/scripts/ymca.js
@@ -420,11 +420,16 @@
    */
   Drupal.behaviors.cdn_contact_form = {
     attach: function (context, settings) {
-      $('.cdn-contact-form .form-radio').on('change', function() {
-        if ($(this).prop('checked') === true) {
-          $(this).parents('.js-webform-radios').find('.checked').removeClass('checked');
-          $(this).parent().addClass('checked');
+      $('.cdn-contact-form .form-radio').each(function() {
+        if ($(this).val() == 0) {
+          $(this).next().addClass('no');
         }
+        $(this).on('change', function() {
+          if ($(this).prop('checked') === true) {
+            $(this).parents('.js-webform-radios').find('.checked').removeClass('checked');
+            $(this).parent().addClass('checked');
+          }
+        });
       });
     }
   };
