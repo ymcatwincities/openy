75af65e66a24577990655d26155a0a981ea5e53c
Dmitry Drozdik <dmitriydrozdik@gmail.com>
2017-10-18 15:23:54 +0300

    [YGB-128] Add Personify Fetcher module.

---
+++ docroot/modules/custom/openy_digital_signage/openy_digital_signage_classes_schedule/modules/openy_digital_signage_personify_schedule/src/OpenYSessionsPersonifyFetcherInterface.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drupal\openy_digital_signage_personify_schedule;
+
+/**
+ * Provides an interface for Personify data fetcher service.
+ *
+ * @ingroup openy_digital_signage_personify_schedule
+ */
+interface OpenYSessionsPersonifyFetcherInterface {
+
+  /**
+   * Retrieves list of locations ids to be imported.
+   *
+   * @return array
+   *   IDs of location to be imported.
+   */
+  public function getLocations();
+
+  /**
+   * Import sessions for specific location.
+   *
+   * @param int $location_id
+   *   Mapping location id.
+   */
+  public function fetchLocation($location_id);
+
+  /**
+   * Fetch from Personify.
+   */
+  public function fetchAll();
+
+  /**
+   * Check if there are entities to be deleted.
+   *
+   * @param array $feed
+   *   Pulled Personify pro feed.
+   * @param int $location_id
+   *   The location id.
+   *
+   * @return array
+   *   The array on entity ids to be deleted.
+   */
+  public function checkDeleted($feed, $location_id);
+
+  /**
+   * Removes entities by chunks.
+   *
+   * @param array $ids
+   *   Pulled Personify feed.
+   */
+  public function removeDeleted($ids);
+
+  /**
+   * Create or update sessions.
+   *
+   * @param array $data
+   *   Data received from Personify.
+   * @param int $location_id
+   *   Location node id.
+   */
+  public function processData(array $data, $location_id);
+
+}
