561e3cc934106698991fa63b26dddbc69a755981
Andrii Podanenko <podarokua@gmail.com>
2017-11-03 18:56:46 +0200

    Update openyonclickinstall.sh

--- scripts/openyonclickinstall.sh
+++ scripts/openyonclickinstall.sh
@@ -2,27 +2,27 @@
 # To get OpenY on DigitalOcean one-app LAMP 16.04 droplet run the command:
 # bash < <(curl -s https://raw.githubusercontent.com/ymcatwincities/openy-project/8.1.x/scripts/openyonclickinstall.sh)
 # as root user
-echo "Hello, OpenY evaluator.\n"
+printf "Hello, OpenY evaluator.\n"
 
-echo "Installing OpenY into /var/www/html\n"
+printf "Installing OpenY into /var/www/html\n"
 
-echo "\nMaking backup of existing /var/www/html folder to /var/www/html.bak\n"
+printf "\nMaking backup of existing /var/www/html folder to /var/www/html.bak\n"
 sudo rm -rf /var/www/html.bak/html || true
 sudo mv /var/www/html /var/www/html.bak || true
 
-echo "\nInstalling composer\n"
+printf "\nInstalling composer\n"
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 sudo php composer-setup.php --install-dir=/usr/bin --filename=composer
 
-echo "\nInstalling drush 8.1.15. In case if you need newer version - install it manually, please.\n"
+printf "\nInstalling drush 8.1.15. In case if you need newer version - install it manually, please.\n"
 sleep 5
 
 wget https://github.com/drush-ops/drush/releases/download/8.1.15/drush.phar
 chmod +x drush.phar
 sudo mv drush.phar /usr/local/bin/drush
 
-echo "\nInstalling needed php extensions\n"
+printf "\nInstalling needed php extensions\n"
 sudo apt-get -y update || true
 sudo apt-get -y install php-mbstring php-curl php-zip unzip php-dom php-xml php-simplexml|| true
 
@@ -37,7 +37,7 @@ drush dl -y drupal --destination=/tmp --default-major=8 --drupal-project-rename=
 cd /tmp/drupal
 drush si -y minimal --db-url=mysql://root:$root_pass@localhost/drupal && drush sql-drop -y
 
-echo "\nPreparing OpenY code tree \n"
+printf "\nPreparing OpenY code tree \n"
 composer create-project ymcatwincities/openy-project:8.1.x-dev /var/www/html --no-interaction
 
 cp /tmp/drupal/sites/default/settings.php /var/www/html/docroot/sites/default/settings.php
