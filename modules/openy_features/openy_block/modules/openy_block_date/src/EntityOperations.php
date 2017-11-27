52c47a7ade698918fb24ed82a4542de78041f2bd
Dmytro Danylevskyi <dd@root.ua>
2017-11-27 07:25:47 +0200

    Implement proper date logic

--- modules/openy_features/openy_block/modules/openy_block_date/src/EntityOperations.php
+++ modules/openy_features/openy_block/modules/openy_block_date/src/EntityOperations.php
@@ -5,6 +5,7 @@ namespace Drupal\openy_block_date;
 use Drupal\Component\Datetime\TimeInterface;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Entity\Display\EntityViewDisplayInterface;
 use Drupal\Core\Entity\EntityInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Datetime\DrupalDateTime;
@@ -57,42 +58,56 @@ class EntityOperations implements ContainerInjectionInterface {
   }
 
   /**
-   * Acts on hook_ENTITY_TYPE_build_defaults_alter().
+   * Acts on hook_ENTITY_TYPE_view_alter().
    *
    * @param array $build
    *   Build.
    * @param \Drupal\Core\Entity\EntityInterface $entity
    *   Entity.
-   * @param $view_mode
-   *   View mode.
+   * @param \Drupal\Core\Entity\Display\EntityViewDisplayInterface $display
+   *   Display.
    *
    * @return array
    *   Build.
    */
-  public function buildDefaultsAlter(array $build, EntityInterface $entity, $view_mode) {
-    if ($view_mode != 'default') {
-      return $build;
-    }
-
-    // Remove default rendered data.
-    unset($build['#block_content']);
-
+  public function viewAlter(array $build, EntityInterface $entity, EntityViewDisplayInterface $display) {
     $requestTime = $this->time->getRequestTime();
-    $timezone = $this->configFactory->get('timezone.default');
 
     $dates = [];
     $fields_date = ['field_start_date', 'field_end_date'];
     foreach ($fields_date as $field) {
-      // Set tags with future block changes timestamps.
-      $dateTime = DrupalDateTime::createFromFormat(DATETIME_DATETIME_STORAGE_FORMAT, $entity->$field->value, $timezone);
+      // Set tags with timestamps of future block changes.
+      $dateTime = DrupalDateTime::createFromFormat(DATETIME_DATETIME_STORAGE_FORMAT, $entity->$field->value, 'UTC');
       $timestamp = $dateTime->getTimestamp();
       if ($timestamp > $requestTime) {
         $build['#cache']['tags'][] = self::TAG . ":$timestamp";
       }
+
+      $dates[$field]['timestamp'] = $timestamp;
     }
 
-    // @todo Show contents depending on the date.
-    $build['#markup'] = 'Add markup here';
+    // Show content depending on time.
+    $elements = [
+      'field_start_date',
+      'field_end_date',
+      'field_content_date_before',
+      'field_content_date_between',
+      'field_content_date_end',
+    ];
+
+    foreach ($elements as $element) {
+      hide($build[$element]);
+    }
+
+    if ($requestTime < $dates['field_start_date']['timestamp']) {
+      show($build['field_content_date_before']);
+    }
+    elseif ($requestTime >= $dates['field_start_date']['timestamp'] && $requestTime < $dates['field_end_date']['timestamp']) {
+      show($build['field_content_date_between']);
+    }
+    elseif ($requestTime >= $dates['field_end_date']['timestamp']) {
+      show($build['field_content_date_end']);
+    }
 
     return $build;
   }
