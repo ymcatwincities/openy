24248e502ac6e107d9bea1bc80d4cdc66e25cf5e
Dmytro Danylevskyi <dd@root.ua>
2018-04-25 15:59:07 +0300

    Fix selecting specific cabin from the bottom list

--- docroot/modules/custom/ymca_camp_du_nord/src/Form/CdnFormFull.php
+++ docroot/modules/custom/ymca_camp_du_nord/src/Form/CdnFormFull.php
@@ -134,6 +134,22 @@ class CdnFormFull extends FormBase {
       $state['cid'] = $state['cabin'];
     }
 
+    if (isset($query['cabin']) && !empty($query['cabin'])) {
+      $state['cabin'] = $query['cabin'];
+    }
+
+    if (isset($query['cid']) && !empty($query['cid'])) {
+      $state['cid'] = $query['cid'];
+    }
+
+    if (isset($query['cid']) && empty($query['cid'])) {
+      $state['cid'] = $state['cabin'];
+    }
+
+    if (!isset($query['cid'])) {
+      $state['cid'] = $state['cabin'];
+    }
+
     $this->state = $state;
   }
 
