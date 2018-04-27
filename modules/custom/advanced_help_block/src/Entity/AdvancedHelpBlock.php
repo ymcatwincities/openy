465a92828dfc0067bb670cc412aa886bd79a33dd
Oleksandr Medvediev <oleksandrm@imagexmedia.com>
2018-04-27 16:28:52 +0300

    OS-18: add advanced_help_block module.

---
+++ modules/custom/advanced_help_block/src/Entity/AdvancedHelpBlock.php
@@ -0,0 +1,278 @@
+<?php
+
+namespace Drupal\advanced_help_block\Entity;
+
+use Drupal\advanced_help_block\AdvancedHelpBlockInterface;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Entity\EntityChangedTrait;
+use Drupal\advanced_help_block\AdvancedHelpBlocksInterface;
+use Drupal\link\LinkItemInterface;
+use Drupal\user\UserInterface;
+
+/**
+ * Defines the Contact entity.
+ *
+ * @ingroup advanced_help_block
+ *
+ * This is the main definition of the entity type. From it, an entityType is
+ * derived. The most important properties in this example are listed below.
+ *
+ * id: The unique identifier of this entityType. It follows the pattern
+ * 'moduleName_xyz' to avoid naming conflicts.
+ *
+ * label: Human readable name of the entity type.
+ *
+ * handlers: Handler classes are used for different tasks. You can use
+ * standard handlers provided by D8 or build your own, most probably derived
+ * from the standard class. In detail:
+ *
+ * - view_builder: we use the standard controller to view an instance. It is
+ *   called when a route lists an '_entity_view' default for the entityType
+ *   (see routing.yml for details. The view can be manipulated by using the
+ *   standard drupal tools in the settings.
+ *
+ * - list_builder: We derive our own list builder class from the
+ *   entityListBuilder to control the presentation.
+ *   If there is a view available for this entity from the views module, it
+ *   overrides the list builder. @todo: any view? naming convention?
+ *
+ * - form: We derive our own forms to add functionality like additional fields,
+ *   redirects etc. These forms are called when the routing list an
+ *   '_entity_form' default for the entityType. Depending on the suffix
+ *   (.add/.edit/.delete) in the route, the correct form is called.
+ *
+ * - access: Our own accessController where we determine access rights based on
+ *   permissions.
+ *
+ * More properties:
+ *
+ *  - base_table: Define the name of the table used to store the data. Make sure
+ *    it is unique. The schema is automatically determined from the
+ *    BaseFieldDefinitions below. The table is automatically created during
+ *    installation.
+ *
+ *  - fieldable: Can additional fields be added to the entity via the GUI?
+ *    Analog to content types.
+ *
+ *  - entity_keys: How to access the fields. Analog to 'nid' or 'uid'.
+ *
+ *  - links: Provide links to do standard tasks. The 'edit-form' and
+ *    'delete-form' links are added to the list built by the
+ *    entityListController. They will show up as action buttons in an additional
+ *    column.
+ *
+ * There are many more properties to be used in an entity type definition. For
+ * a complete overview, please refer to the '\Drupal\Core\Entity\EntityType'
+ * class definition.
+ *
+ * The following construct is the actual definition of the entity type which
+ * is read and cached. Don't forget to clear cache after changes.
+ *
+ * @ContentEntityType(
+ *   id = "advanced_help_block",
+ *   label = @Translation("Advanced Help Block"),
+ *   handlers = {
+ *     "view_builder" = "Drupal\Core\Entity\EntityViewBuilder",
+ *     "list_builder" = "Drupal\advanced_help_block\Entity\Controller\AdvancedHelpBlockBuilder",
+ *     "views_data" = "Drupal\views\EntityViewsData",
+ *     "form" = {
+ *       "add" = "Drupal\advanced_help_block\Form\AdvancedHelpBlockForm",
+ *       "edit" = "Drupal\advanced_help_block\Form\AdvancedHelpBlockForm",
+ *       "delete" = "Drupal\advanced_help_block\Form\AdvancedHelpBlockDeleteForm",
+ *     },
+ *     "access" = "Drupal\advanced_help_block\AdvancedHelpBlockAccessControlHandler",
+ *   },
+ *   base_table = "advanced_help_block",
+ *   admin_permission = "administer advanced_help_block entity",
+ *   fieldable = TRUE,
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "label" = "name",
+ *     "uuid" = "uuid"
+ *   },
+ *   links = {
+ *     "canonical" = "/advanced_help_block/{advanced_help_block}",
+ *     "edit-form" = "/advanced_help_block/{advanced_help_block}/edit",
+ *     "delete-form" = "/advanced_help_block/{advanced_help_block}/delete",
+ *     "collection" = "/advanced_help_block/list"
+ *   },
+ *   field_ui_base_route = "advanced_help_block.advanced_help_block_settings",
+ * )
+ *
+ * The 'links' above are defined by their path. For core to find the corresponding
+ * route, the route name must follow the correct pattern:
+ *
+ * entity.<entity-name>.<link-name> (replace dashes with underscores)
+ * Example: 'entity.advanced_help_block.canonical'
+ *
+ * See routing file above for the corresponding implementation
+ *
+ * The 'Contact' class defines methods and fields for the contact entity.
+ *
+ * Being derived from the ContentEntityBase class, we can override the methods
+ * we want. In our case we want to provide access to the standard fields about
+ * creation and changed time stamps.
+ *
+ * Our interface (see ContactInterface) also exposes the EntityOwnerInterface.
+ * This allows us to provide methods for setting and providing ownership
+ * information.
+ *
+ * The most important part is the definitions of the field properties for this
+ * entity type. These are of the same type as fields added through the GUI, but
+ * they can by changed in code. In the definition we can define if the user with
+ * the rights privileges can influence the presentation (view, edit) of each
+ * field.
+ */
+class AdvancedHelpBlock extends ContentEntityBase implements AdvancedHelpBlockInterface {
+
+  use EntityChangedTrait;
+
+  /**
+   * {@inheritdoc}
+   *
+   * When a new entity instance is added, set the user_id entity reference to
+   * the current user as the creator of the instance.
+   */
+  public static function preCreate(EntityStorageInterface $storage_controller, array &$values) {
+    parent::preCreate($storage_controller, $values);
+    $values += array(
+      'user_id' => \Drupal::currentUser()->id(),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCreatedTime() {
+    return $this->get('created')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getOwner() {
+    return $this->get('user_id')->entity;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getOwnerId() {
+    return $this->get('user_id')->target_id;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setOwnerId($uid) {
+    $this->set('user_id', $uid);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setOwner(UserInterface $account) {
+    $this->set('user_id', $account->id());
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * Define the field properties here.
+   *
+   * Field name, type and size determine the table structure.
+   *
+   * In addition, we can define how the field and its content can be manipulated
+   * in the GUI. The behaviour of the widgets used can be determined here.
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+
+    // Standard field, used as unique if primary index.
+    $fields['id'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('ID'))
+      ->setDescription(t('The ID of the Contact entity.'))
+      ->setReadOnly(TRUE);
+
+    // Standard field, unique outside of the scope of the current project.
+    $fields['uuid'] = BaseFieldDefinition::create('uuid')
+      ->setLabel(t('UUID'))
+      ->setDescription(t('The UUID of the Contact entity.'))
+      ->setReadOnly(TRUE);
+
+    // Name field for the contact.
+    // We set display options for the view as well as the form.
+    // Users with correct privileges can change the view and edit configuration.
+
+    $fields['title'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Title'))
+      ->setDescription(t('The name of the Advanced Help Block entity.'))
+      ->setSettings(array(
+        'default_value' => '',
+        'max_length' => 255,
+        'text_processing' => 0,
+      ))
+      ->setDisplayOptions('view', array(
+        'label' => 'above',
+        'type' => 'string',
+        'weight' => -6,
+      ))
+      ->setDisplayOptions('form', array(
+        'type' => 'string_textfield',
+        'weight' => -6,
+      ))
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['description'] = BaseFieldDefinition::create('text_long')
+      ->setLabel(t('Description'))
+      ->setDescription(t('The Description of the block'))
+      ->setDisplayOptions('form', array(
+        'type'   => 'text_textarea',
+        'weight' => -6
+      ))
+      ->setRequired(TRUE)
+      ->setDisplayConfigurable('form', TRUE);
+
+    $fields['video'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Video'))
+      ->setDescription(t('The link to youtube video.'))
+      ->setSettings(array(
+        'default_value' => '',
+        'max_length' => 255,
+        'text_processing' => 0,
+      ))
+      ->setDisplayOptions('view', array(
+        'label' => 'above',
+        'type' => 'string',
+        'weight' => -6,
+      ))
+      ->setDisplayOptions('form', array(
+        'type' => 'string_textfield',
+        'weight' => -6,
+      ))
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['url'] = BaseFieldDefinition::create('link')
+      ->setLabel(t('Page Url'))
+      ->setDescription(t('The link of page where this Help block will be shown'))
+      ->setSettings([
+        'link_type' => LinkItemInterface::LINK_GENERIC,
+        'title' => DRUPAL_DISABLED,
+      ])
+      ->setDisplayOptions('form', [
+        'type' => 'link_default',
+        'weight' => -2,
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayConfigurable('view', TRUE);
+
+
+    return $fields;
+  }
+}
---
+++ modules/custom/advanced_help_block/src/Form/AdvancedHelpBlockDeleteForm.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\advanced_help_block\Form;
+
+use Drupal\Core\Entity\ContentEntityConfirmFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+
+/**
+ * Provides a form for deleting a advanced_help_block entity.
+ *
+ * @ingroup advanced_help_block
+ */
+class AdvancedHelpBlockDeleteForm extends ContentEntityConfirmFormBase {
+
+  /**
+   * Returns the question to ask the user.
+   *
+   * @return string
+   *   The form question. The page title will be set to this value.
+   */
+  public function getQuestion()
+  {
+    // TODO: Implement getQuestion() method.
+  }
+
+  /**
+   * Returns the route to go to if the user cancels the action.
+   *
+   * @return \Drupal\Core\Url
+   *   A URL object.
+   */
+  public function getCancelUrl()
+  {
+    // TODO: Implement getCancelUrl() method.
+  }
+}
