39b98a5298477864a7fcb31b5ffdd2d9c139c39b
Vladimir Selivanov <vladimirs@imagexmedia.com>
2018-05-09 21:37:37 +0300

    OS-26 JS library cache issue

--- modules/custom/openy_map/src/Element/OpenYMap.php
+++ modules/custom/openy_map/src/Element/OpenYMap.php
@@ -40,6 +40,7 @@ class OpenYMap extends RenderElement {
     $element['#attached']['drupalSettings']['openyMap'] = $element['#element_variables'];
     $tags = \Drupal::configFactory()->get('openy_map.settings')->get('default_tags');
     $element['#attached']['drupalSettings']['openyMapSettings']['default_tags'] = array_values(array_filter($tags));
+    $element['#cache']['tags'][] = 'config:openy_map.settings';
 
     return $element;
   }
