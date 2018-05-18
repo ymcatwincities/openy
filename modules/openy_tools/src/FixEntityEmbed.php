9eedde84cc52336251eb29d1a483623dd3127c77
podarok <podarok@ua.fm>
2018-05-18 20:08:42 +0300

    Not finished

--- modules/openy_tools/src/FixEntityEmbed.php
+++ modules/openy_tools/src/FixEntityEmbed.php
@@ -34,6 +34,13 @@ class FixEntityEmbed {
     $db = \Drupal::database();
 
     $tables = [
+      'node__field_lead_description' => ['field_lead_description_value'],
+      'node__field_secondary_sidebar' => ['field_secondary_sidebar_value'],
+      'node__field_sidebar' => ['field_sidebar_value'],
+      'node__field_summary' => ['field_summary_value'],
+      'node__field_ygtc_content' => ['field_ygtc_content_value'],
+      'paragraph_revision__field_prgf_description' => ['field_prgf_description_value'],
+      'paragraph__field_prgf_description' => ['field_prgf_description_value'],
       'block_content_revision__body' => ['body_value'],
       'block_content_revision__field_block_content' => ['field_block_content_value'],
       'block_content_r__6de56f762b' => ['field_ygtc_content_date_between_value'],
@@ -49,82 +56,67 @@ class FixEntityEmbed {
       'node_revision__field_sidebar' => ['field_sidebar_value'],
       'node_revision__field_summary' => ['field_summary_value'],
       'node_revision__field_ygtc_content' => ['field_ygtc_content_value'],
-      'node__field_lead_description' => ['field_lead_description_value'],
-      'node__field_secondary_sidebar' => ['field_secondary_sidebar_value'],
-      'node__field_sidebar' => ['field_sidebar_value'],
-      'node__field_summary' => ['field_summary_value'],
-      'node__field_ygtc_content' => ['field_ygtc_content_value'],
-      'paragraph_revision__field_prgf_description' => ['field_prgf_description_value'],
-      'paragraph__field_prgf_description' => ['field_prgf_description_value'],
     ];
 
     foreach ($tables as $table => $columns) {
       foreach ($columns as $field) {
         $result = $db->select($table, 't')
           ->fields('t')
-          ->condition('t.' . $field, '%' . $db->escapeLike('drupal-entity-inline') . '%', 'LIKE')
+          ->condition(
+            't.' . $field,
+            '%' . $db->escapeLike('drupal-entity-inline') . '%',
+            'LIKE'
+          )
           ->execute();
 
         while ($data = $result->fetchObject()) {
           $replacement = [];
-          $dom = Html::load($data->$field);
-          $xpath = new \DOMXPath($dom);
-
-          /** @var \DOMElement $node */
-          foreach ($xpath->query('//*[@data-entity-type and (@data-entity-uuid or @data-entity-id) and (@data-entity-embed-display or @data-view-mode)]') as $node) {
-            // Skip if it's not menu_link.
-            if ($node->getAttribute('data-embed-button') != 'menu_link') {
-              continue;
+          preg_match_all(
+            "/<drupal-entity-inline.*<\/drupal-entity-inline>/miU",
+            $data->$field,
+            $test
+          );
+          $i = 0;
+          if (count($test[0])) {
+            foreach ($test[0] as $drupalEntityInline) {
+              preg_match_all("/\"menu_link\"/miU", $drupalEntityInline, $fail);
+              if (count($fail[0]) >= 2) {
+                throw new \Exception('Regex is wrong');
+              }
+              else {
+                if (count($fail[0]) == 0) {
+                  continue;
+                }
+              }
+              $i = 0;
+              $dom = Html::load($data->$field);
+              $xpath = new \DOMXPath($dom);
+              foreach ($xpath->query(
+                '//*[@data-entity-type and (@data-entity-uuid or @data-entity-id) and (@data-entity-embed-display or @data-view-mode)]'
+              ) as $node) {
+                $uuid = $node->getAttribute('data-entity-uuid');
+                $label = $node->getAttribute('data-entity-label');
+                $replacement = '<drupal-entity
+                              data-button="0"
+                              data-embed-button="menu_link"
+                              data-entity-embed-display="entity_reference:entity_reference_label_url"
+                              data-entity-embed-display-settings="{&quot;route_link&quot;:1,&quot;route_title&quot;:&quot;' . $label . '&quot;}"
+                              data-entity-type="menu_link_content"
+                              data-entity-uuid="' . $uuid . '"></drupal-entity>';
+                $i = 0;
+                $data->$field = str_replace($drupalEntityInline, $replacement, $data->$field);
+                $db->update($table)
+                  ->fields([
+                    $field => $data->$field,
+                  ])
+                  ->execute();
+              }
             }
-
-            $uuid = $node->getAttribute('data-entity-uuid');
-            $label = $node->getAttribute('data-entity-label');
-            $replacement[] = '<drupal-entity
-              data-button="0"
-              data-embed-button="menu_link"
-              data-entity-embed-display="entity_reference:entity_reference_label_url"
-              data-entity-embed-display-settings="{&quot;route_link&quot;:1,&quot;route_title&quot;:&quot;' . $label . '&quot;}"
-              data-entity-type="menu_link_content"
-              data-entity-uuid="' . $uuid . '"></drupal-entity>';
           }
-
-          // Find all embeds that need to be replaced.
-          preg_match_all("/<drupal-entity-inline.*<\/drupal-entity-inline>/miU", $data->$field, $test);
-          if (empty($test[0])) {
-            continue;
-          }
-
-          // Skip if it's not menu_link.
-          foreach ($test[0] as $i => $item) {
-            if (strpos($item, 'menu_link') === FALSE) {
-              unset($test[0][$i]);
-            }
-          }
-
-          if (count($replacement) != count($test[0])) {
-            throw new \Exception('Possibly invalid regex.');
-          }
-
-          // Create map for str_replace.
-          foreach ($test[0] as $i => $embed) {
-            $replace['from'][] = $embed;
-            $replace['to'][] = $replacement[$i];
-          }
-
-          if ($replace) {
-            $data->$field = str_replace($replace['from'], $replace['to'], $data->$field);
-          }
-
-          $db->update($table)
-            ->fields([
-              $field => $data->$field,
-            ])
-            ->execute();
-
-          $this->loggerChannel->info(sprintf('Fixed entity embed in field %s in table %s', $field, $table));
         }
 
       }
+      $this->loggerChannel->info(sprintf('Fixed entity embed in field %s in table %s', $field, $table));
     }
 
   }
