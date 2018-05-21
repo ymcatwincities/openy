389c8bf557d33a78f485af7988e09493042ff043
Vladimir Selivanov <vladimirs@imagexmedia.com>
2018-05-21 16:45:33 +0300

    OS-32 Multiple Branch hours

--- modules/custom/openy_hours_formatter/src/Plugin/Field/FieldFormatter/CustomHoursToday.php
+++ modules/custom/openy_hours_formatter/src/Plugin/Field/FieldFormatter/CustomHoursToday.php
@@ -22,77 +22,50 @@ class CustomHoursToday extends CustomHoursFormatterDefault {
    * {@inheritdoc}
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
-    $elements = [];
-    $lazy_hours = [];
-    foreach ($items as $delta => $item) {
-      $groups = [];
-      $rows = [];
-      $label = '';
+    $elementsParent = parent::viewElements($items, $langcode);
 
+    $lazy_hours = $lazy_hours_placeholder = [];
+    foreach ($items as $delta => $item) {
       // Group days by their values.
       foreach ($item as $i_item) {
-        // Do not process label. Store it name for later usage.
         $name = $i_item->getName();
-        if ($name == 'hours_label') {
-          $label = $i_item->getValue();
-          continue;
-        }
-
         $day = str_replace('hours_', '', $name);
         $value = $i_item->getValue() ? $i_item->getValue() : 'closed';
-        $lazy_hours[$day] = $value;
-        if ($groups && end($groups)['value'] == $value) {
-          $array_keys = array_keys($groups);
-          $group = &$groups[end($array_keys)];
-          $group['days'][] = $day;
-        }
-        else {
-          $groups[] = [
-            'value' => $value,
-            'days' => [$day],
-          ];
+        // Do not process label.
+        if ($day != 'label') {
+          $lazy_hours[$day] = $value;
         }
       }
 
-      foreach ($groups as $group_item) {
-        $title = sprintf('%s - %s', ucfirst(reset($group_item['days'])), ucfirst(end($group_item['days'])));
-        if (count($group_item['days']) == 1) {
-          $title = ucfirst(reset($group_item['days']));
-        }
-        $hours = $group_item['value'];
-        $rows[] = [$title . ':', $hours];
+      if ($delta == 0) {
+        $lazy_hours_placeholder = [
+          '#lazy_builder' => [
+            'openy_hours_formatter.hours_today:generateHoursToday',
+            $lazy_hours,
+          ],
+          '#create_placeholder' => TRUE,
+        ];
       }
-
-      $lazy_hours_placeholder = [
-        '#lazy_builder' => [
-          'openy_hours_formatter.hours_today:generateHoursToday',
-          $lazy_hours,
-        ],
-        '#create_placeholder' => TRUE,
-      ];
-
-      $elements[$delta] = [
-        '#theme' => 'openy_hours_formatter',
-        '#hours' => $lazy_hours_placeholder,
-        '#week' => [
-          'title' => [
-            '#type' => 'html_tag',
-            '#tag' => 'h5',
-            '#value' => $label,
-          ],
-          'table' => [
-            '#theme' => 'table',
-            '#header' => [],
-            '#rows' => $rows,
-          ],
-        ],
-        '#attached' => [
-          'library' => [
-            'openy_hours_formatter/openy_hours_formatter',
-          ],
-        ],
-      ];
     }
+
+    $elements[0] = [
+      '#theme' => 'openy_hours_formatter',
+      '#hours' => $lazy_hours_placeholder,
+      '#week' => [
+        '#theme' => 'item_list',
+        '#attributes' => [
+          'class' => [
+            'branch-hours',
+          ],
+        ],
+        '#items' => $elementsParent,
+      ],
+      '#attached' => [
+        'library' => [
+          'openy_hours_formatter/openy_hours_formatter',
+        ],
+      ],
+    ];
 
     return $elements;
   }
