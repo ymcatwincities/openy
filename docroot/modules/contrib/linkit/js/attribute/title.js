144b9f068871bb6bccb9213347985859b2c0f899
podarok <podarokua@gmail.com>
2018-05-24 14:18:13 +0300

    Update full_html filter with Linkit for YGTC

--- docroot/modules/contrib/linkit/js/attribute/title.js
+++ docroot/modules/contrib/linkit/js/attribute/title.js
@@ -5,26 +5,25 @@
 
 (function ($, Drupal, document) {
 
-  'use strict';
+    'use strict';
 
-  var fieldName = '[name="attributes[title]"]';
-    // var fieldName = '[name="attributes[uuid]"]';
-    // var fieldName = '[name="attributes[entity_type_id]"]';
+    var fieldName = '[name="attributes[title]"]';
+    var fieldNameUuid = '[name="attributes[data-drupal-entity-uuid]"]';
+    var fieldNameEntityTypeId = '[name="attributes[data-drupal-entity-type-id]"]';
 
-  /**
-   * Automatically populate the title attribute.
-   */
-  $(document).bind('linkit.autocomplete.select', function (triggerEvent, event, ui) {
-    if (ui.item.hasOwnProperty('title')) {
-      $('form.linkit-editor-dialog-form').find(fieldName).val(ui.item.title);
-    }
-      if (ui.item.hasOwnProperty('uuid')) {
-      // $('form.linkit-editor-dialog-form').find(fieldName).val(ui.item.title);
-          console.log('hacked', ui.item.uuid);
-      }
-      if (ui.item.hasOwnProperty('entity_type_id')) {
-          console.log('hacked', ui.item.entity_type_id);
-      }
-  });
+    /**
+     * Automatically populate the title attribute.
+     */
+    $(document).bind('linkit.autocomplete.select', function (triggerEvent, event, ui) {
+        if (ui.item.hasOwnProperty('title')) {
+            $('form.linkit-editor-dialog-form').find(fieldName).val(ui.item.title);
+        }
+        if (ui.item.hasOwnProperty('linkit_entity_uuid')) {
+            $('form.linkit-editor-dialog-form').find(fieldNameUuid).val(ui.item.linkit_entity_uuid);
+        }
+        if (ui.item.hasOwnProperty('entity_type_id')) {
+            $('form.linkit-editor-dialog-form').find(fieldNameEntityTypeId).val(ui.item.entity_type_id);
+        }
+    });
 
 })(jQuery, Drupal, document);
