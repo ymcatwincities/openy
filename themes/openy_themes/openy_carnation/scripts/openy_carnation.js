0c3efbf81be70c082a3d30cedfe0d6ee760f9e2d
Aaron Christian <aaronc@imagexmedia.com>
2018-05-24 17:13:23 -0700

    First kick at new theme.

---
+++ themes/openy_themes/openy_carnation/scripts/openy_carnation.js
@@ -0,0 +1,54 @@
+/**
+ * @file
+ * OpenY Carnation JS.
+ */
+
+(function ($) {
+  "use strict";
+  Drupal.openy_carnation = Drupal.openy_carnation || {};
+
+  // Sidebar collapsible.
+  Drupal.behaviors.openy_carnation_init = {
+    attach: function (context, settings) {
+      $('.layout-container').prepend('<div class="banner-zone-node"></div>');
+      $('.wrapper-field-header-content .paragraph--type--banner').appendTo('.banner-zone-node');
+      $('.menu-icon-1').addClass('home-link');
+      $('.webform-submission-form').addClass('container');
+
+      if ($('.banner-zone-node').text().length > 0 ) {
+        $('body').addClass('with-banner');
+      }
+
+      if ($('.banner-zone-node').text().length == 0 ) {
+        $('body').addClass('without-banner');
+      }
+
+      if($(".field-link-attribute:contains('New Window')").length) {
+        $('.field-prgf-clm-link a').attr('target', '_blank');
+      }
+
+      // menus
+      $(".row-level-3").hide();
+
+      $( ".row-level-2 li" ).mouseover(function() {
+        var show_div = this.className;
+        if(show_div.match(/menu-item(.*)/g)){
+          var parent_class = show_div.match(/menu-item(.*)/g);
+          var level3_class =  parent_class[0].split(" ")[0];
+          $("."+level3_class+" .row-level-3").show();
+        }
+      });
+
+      $( ".row-level-2 li" ).mouseout(function() {
+        var show_div = this.className;
+        if(show_div.match(/menu-item(.*)/g)){
+          var parent_class = show_div.match(/menu-item(.*)/g);
+          var level3_class =  parent_class[0].split(" ")[0];
+          $("."+level3_class+" .row-level-3").hide();
+        }
+      });
+
+    }
+  };
+
+})(jQuery);
