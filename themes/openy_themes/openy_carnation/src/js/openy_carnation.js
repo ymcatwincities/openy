0c54ca672e5f2354ce7f7198aabdc451c9726d58
Aaron Christian <aaronc@imagexmedia.com>
2018-05-30 12:04:41 -0700

    More work on the header and porting over JS from dayton. Converting to BS4.

--- themes/openy_themes/openy_carnation/src/js/openy_carnation.js
+++ themes/openy_themes/openy_carnation/src/js/openy_carnation.js
@@ -7,50 +7,88 @@
   "use strict";
   Drupal.openy_carnation = Drupal.openy_carnation || {};
 
+  //  Move Header Banner paragraph to header.
+  Drupal.behaviors.openy_carnation_banner_node = {
+    attach: function (context, settings) {
+      var banner_header = $('.paragraph--type--banner');
+      if (banner_header.length > 0) {
+        $('.banner-zone-node').once('move').append(banner_header.eq(0));
+        $('body').addClass('with-banner');
+      } else {
+        $('body').addClass('without-banner');
+      }
+    }
+  };
+
+  // Show/hide desktop search block.
+  Drupal.behaviors.openy_carnation_search_md = {
+    attach: function (context, settings) {
+      var search_md = $('.site-search button');
+      var main_menu_links_md = $('.page-head__main-menu .nav-level-1 li:not(:eq(0))').find('a, button');
+      var search_close_md = $('.page-head__search-close');
+
+      search_md.once('search-toggle-hide').on('click', function () {
+        main_menu_links_md.removeClass('show').addClass('fade');
+      });
+
+      search_close_md.once('search-toggle-show').on('click', function () {
+        main_menu_links_md.addClass('show');
+      });
+    }
+  };
+
+  // Add class to header when mobile menu is opened.
+  Drupal.behaviors.openy_carnation_mob_menu = {
+    attach: function (context, settings) {
+      var sidebar = $('#sidebar');
+      var $target = $('.top-navs');
+
+      sidebar.on('show.bs.collapse', function () {
+        $target.addClass('menu-in');
+      });
+
+      sidebar.on('hide.bs.collapse', function () {
+        $target.removeClass('menu-in');
+      });
+    }
+  };
+
+  // Dropdown menu height.
+  Drupal.behaviors.openyDropdownMenu = {
+    attach: function (context, settings) {
+      $('.page-head__main-menu .nav-level-2').each(function (index, item) {
+        var item = $(item);
+        var offset = item.offset();
+        var bottom = offset.top + item.height() - $(window).scrollTop();
+        var maxHeight = $(window).height() - bottom - 15;
+        item.find('.dropdown-menu').eq(0).css('max-height', maxHeight);
+      });
+    }
+  };
+
+  // BS4 data-spy: affix replacement
+  Drupal.behaviors.openy_carnation_header_affix = {
+    attach: function (context, settings) {
+      $(window).on('scroll', function(event) {
+        var scrollValue = $(window).scrollTop();
+        if (scrollValue === settings.scrollTopPx || scrollValue > 1) {
+          $('.top-navs').addClass('affix');
+        } else if (scrollValue === settings.scrollTopPx || scrollValue < 1) {
+          $('.top-navs').removeClass('affix');
+        }
+      });
+    }
+  };
+
   // Sidebar collapsible.
   Drupal.behaviors.openy_carnation_init = {
     attach: function (context, settings) {
-
-      // homepage banner
-      $('.layout-container').once('banner-zone').each(function() {
-        $('.layout-container').prepend('<div class="banner-zone-node"></div>');
-        $('.wrapper-field-header-content .paragraph--type--banner').appendTo('.banner-zone-node');
-
-        if ($('.banner-zone-node').text().length > 0 ) {
-          $('body').addClass('with-banner');
-        }
-
-        if ($('.banner-zone-node').text().length <= 0 ) {
-          $('body').addClass('without-banner');
-        }
-      });
 
       $('.webform-submission-form').addClass('container');
 
       if($(".field-link-attribute:contains('New Window')").length) {
         $('.field-prgf-clm-link a').attr('target', '_blank');
       }
-
-      // menus
-      // $(".row-level-3").hide();
-
-      $( ".row-level-2 li" ).mouseover(function() {
-        var show_div = this.className;
-        if(show_div.match(/menu-item(.*)/g)){
-          var parent_class = show_div.match(/menu-item(.*)/g);
-          var level3_class =  parent_class[0].split(" ")[0];
-          $("."+level3_class+" .row-level-3").show();
-        }
-      });
-
-      $( ".row-level-2 li" ).mouseout(function() {
-        var show_div = this.className;
-        if(show_div.match(/menu-item(.*)/g)){
-          var parent_class = show_div.match(/menu-item(.*)/g);
-          var level3_class =  parent_class[0].split(" ")[0];
-          $("."+level3_class+" .row-level-3").hide();
-        }
-      });
 
     }
   };
