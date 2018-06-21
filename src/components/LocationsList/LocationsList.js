e86117d2efc5f8f112b6128a26f5cd4e1e946025
Shuklina <ls@root.ua>
2018-06-21 17:18:47 +0300

    [#7] Make proper containers & components structure

--- src/components/LocationsList/LocationsList.js
+++ src/components/LocationsList/LocationsList.js
@@ -1,15 +1,19 @@
 import React, {Component} from 'react';
 import {Container, Row, Col, Button, FormGroup, Label, Input} from 'reactstrap';
 import {FaCaretDown} from 'react-icons/lib/fa';
-import RightSidebar from "./RightSidebar";
+import Sidebar from "../Sidebar/Sidebar";
+import './LocationList.css';
 
+/**
+ * Renders list locations with checkboxes and "Next" button.
+ */
 class LocationsList extends Component {
   render() {
 
     return (
       <div>
         <Container>
-          <Row className={'categories'}>
+          <Row className={'locations'}>
             <Col md={8}>
               <FormGroup tag="fieldset">
                 <h2>Location(s)</h2>
@@ -223,7 +227,7 @@ class LocationsList extends Component {
               <Button className={"d-flex m-auto justify-content-center pt-2 pb-2 pl-5 pr-5 btn-next text-uppercase"}>Next</Button>
             </Col>
             <Col md={4} className={'order-first order-md-last'}>
-              <RightSidebar />
+              <Sidebar />
             </Col>
           </Row>
         </Container>
