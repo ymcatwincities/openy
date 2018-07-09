0f2caa7d65abc611594597eedf015f7bd4c24329
mattshoaf <matt.shoaf@clickfarminteractive.com>
2018-07-09 10:39:17 -0500

    fix for index of alerts

--- modules/openy_features/openy_node/modules/openy_node_alert/js/openy_node_alert.js
+++ modules/openy_features/openy_node/modules/openy_node_alert/js/openy_node_alert.js
@@ -32,8 +32,13 @@
         var slick = self.parents('.slick__slider').first();
         // Remove dismissed alerts.
         if ($.inArray(nid, dismissed) != -1) {
-          if (slick.length > 0) {
-            slick.slick('slickRemove', self.parents('.slick__slide').eq(0).index()-1);
+          var index = self.parents('.slick__slide').eq(0).index();
+          if (slick.length > 0 && index > 0) {
+            var slickCheck = slick.slick('slickRemove', index -1);
+            if(!slickCheck) {
+              self.remove();
+              slick.parents('.slick-track').prevObject.remove();
+            }
           }
           else {
             self.remove();
