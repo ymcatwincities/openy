80020f7556181cdd5310bac583107e0283c2241a
Shuklina <ls@root.ua>
2018-07-09 17:46:13 +0300

    [#3059] Rebuild Results Screen

--- docroot/reactapps/prgreg/src/components/ResultsList/ResultsList.js
+++ docroot/reactapps/prgreg/src/components/ResultsList/ResultsList.js
@@ -7,13 +7,13 @@ import {
   CardBody,
   CardTitle,
   Button,
+  CardText,
   FormGroup,
   Label,
-  Input,
-  CardText
+  Input
 } from 'reactstrap';
-import { FaQuestionCircle } from 'react-icons/lib/fa';
-import Sidebar from '../Sidebar/Sidebar';
+
+import { FaExclamationCircle } from 'react-icons/lib/fa';
 import './ResultsList.css';
 
 /**
@@ -24,10 +24,25 @@ class ResultsList extends Component {
     return (
       <div>
         <Container>
-          <Row className={'results mb-2'}>
-            <Col md={8}>
-              <h2>Results</h2>
-              <p>The following activities are available.</p>
+          <Row className={' categories results mb-2'}>
+            <Col md={12}>
+              <Row>
+                <Col md={8}>
+              <h2>Registration</h2>
+              <p>Please provide the details marked with a  <FaExclamationCircle size={20} /> to proceed.</p>
+                </Col>
+              <Col md={4}>
+                <div className={'kids-data'}>
+                  <Button
+                    className={
+                      'd-flex ml-auto justify-content-center pt-2 pb-2 pl-5 pr-5 btn-next text-uppercase'
+                    }
+                  >
+                   Back to results
+                  </Button>
+                </div>
+              </Col>
+          </Row>
               <Card className={'data-time my-3'}>
                 <CardBody>
                   <CardTitle className={'text-uppercase mb-0'}>
@@ -41,18 +56,13 @@ class ResultsList extends Component {
                     Blaisdell (M-Th)
                   </h5>
                 </div>
-                <div className="p-2 align-self-center">
-                  <p className={'mb-0'}>
-                    <small>Members: $94.00; Non-Members: $202.00</small>
-                  </p>
-                </div>
               </div>
               <Card className={'package-results'}>
                 <CardBody>
                   <Row>
-                    <Col md={8}>
+                    <Col md={12}>
                       <Row>
-                        <Col md={7}>
+                        <Col md={4}>
                           <CardText className="mb-0 font-weight-bold">
                             Swim Lessons
                           </CardText>
@@ -63,270 +73,86 @@ class ResultsList extends Component {
                             Swim Strokes
                           </CardText>
                         </Col>
-                        <Col md={5}>
+                        <Col md={4}>
                           <CardText className={'mb-0 font-weight-bold orange'}>
                             1 spot remaining
                           </CardText>
                           <CardText>8:00 - 9:00am</CardText>
                         </Col>
+                        <Col md={4}>
+                          <FormGroup>
+                            <div className={'d-flex'}>
+                              <div className={"d-flex align-items-center pr-2"}>
+                            <FaExclamationCircle size={20} />
+                              </div>
+
+                            <Input type="select" name="select" placeholder="fdfs" id="exampleSelect">
+                              <option value="" disabled selected>Select Participant</option>
+                              <option>Terri</option>
+                              <option>Craig</option>
+                            </Input>
+                            </div>
+                            <Label for="exampleSelect">or: <small><a href={'#'}> Add New Participant</a></small></Label>
+                          </FormGroup>
+                        </Col>
                       </Row>
 
                       <hr />
 
                       <Row>
-                        <Col md={7}>
+                        <Col md={4}>
                           <CardText className="mb-0 font-weight-bold">
                             Swim Lessons
                           </CardText>
                           <CardText className={'mb-0'}>Parent & Child</CardText>
                         </Col>
-                        <Col md={5}>
+                        <Col md={4}>
                           <CardText className={'mb-0 orange font-weight-bold'}>
                             3 spots remaining
                           </CardText>
-                          <CardText>8:00 - 9:00am</CardText>
+                          <CardText>8:15 - 9:15am</CardText>
+                        </Col>
+                        <Col md={4}>
+                          <FormGroup>
+                            <div className={'d-flex'}>
+                              <div className={"d-flex align-items-center pr-2"}>
+                                <FaExclamationCircle size={20} />
+                              </div>
+
+                              <Input type="select" name="select" placeholder="fdfs" id="exampleSelect">
+                                <option value="" disabled selected>Select Participant</option>
+                                <option>Terri</option>
+                                <option>Craig</option>
+                              </Input>
+                            </div>
+                            <Label for="exampleSelect">or: <small><a href={'#'}> Add New Participant</a></small></Label>
+                          </FormGroup>
                         </Col>
                       </Row>
-                    </Col>
-                    <Col md={4} className={'align-self-center'}>
-                      <Button className={'p-2 btn-result text-uppercase'}>
-                        Select Package
-                      </Button>
-                    </Col>
-                  </Row>
-                </CardBody>
-              </Card>
-
-              <div className="d-flex">
-                <div className="mr-auto p-2">
-                  <h5 className={'my-3 location-results font-weight-bold'}>
-                    Southdale (M-Th)
-                  </h5>
-                </div>
-                <div className="p-2 align-self-center">
-                  <p className={'mb-0'}>
-                    <small>Members: $94.00; Non-Members: $202.00</small>
-                  </p>
-                </div>
-              </div>
-              <Card className={'package-results'}>
-                <CardBody>
-                  <Row>
-                    <Col md={8}>
-                      <FormGroup check>
-                        <Row>
-                          <Col md={7}>
-                            <Label check>
-                              <Input type="radio" name="radio1" />
-                              <CardText className="mb-0 font-weight-bold">
-                                Swim Lessons
-                              </CardText>
-                              <CardText className={'mb-0'}>
-                                Preschool Child
-                              </CardText>
-                              <CardText className={'text-muted small'}>
-                                Swim Strokes
-                              </CardText>
-                            </Label>
-                          </Col>
-                          <Col md={5}>
-                            <CardText className={'mb-0 font-weight-bold green'}>
-                              6 spots remaining
-                            </CardText>
-                            <CardText>8:30 - 9:30am</CardText>
-                          </Col>
-                        </Row>
-
-                        <Row>
-                          <Col md={7}>
-                            <Label check>
-                              <Input type="radio" name="radio1" />
-                              <CardText className="mb-0 font-weight-bold">
-                                Swim Lessons
-                              </CardText>
-                              <CardText className={'mb-0'}>
-                                Parent & Child
-                              </CardText>
-                            </Label>
-                          </Col>
-                          <Col md={5}>
-                            <CardText
-                              className={'mb-0 font-weight-bold orange'}
-                            >
-                              2 spots remaining
-                            </CardText>
-                            <CardText>8:45 - 9:45am</CardText>
-                          </Col>
-                        </Row>
-
-                        <hr />
-                        <Row>
-                          <Col md={7}>
-                            <CardText className="mb-0 font-weight-bold">
-                              Swim Lessons
-                            </CardText>
-                            <CardText className={'mb-0'}>
-                              Parent & Child
-                            </CardText>
-                          </Col>
-                          <Col md={5}>
-                            <CardText
-                              className={'mb-0 font-weight-bold orange'}
-                            >
-                              2 spot remaining
-                            </CardText>
-                            <CardText>8:00 - 9:00am</CardText>
-                          </Col>
-                        </Row>
-                      </FormGroup>
-                    </Col>
-                    <Col md={4} className={'align-self-center'}>
-                      <Button className={'p-2 btn-result text-uppercase'}>
-                        Select Package
-                      </Button>
-                    </Col>
-                  </Row>
-                </CardBody>
-              </Card>
-
-              <Card className={'data-time my-3'}>
-                <CardBody>
-                  <CardTitle className={'text-uppercase mb-0'}>
-                    June 18, 2018 - June 28, 2018
-                  </CardTitle>
-                </CardBody>
-              </Card>
-              <div className="d-flex">
-                <div className="mr-auto p-2">
-                  <h5 className={'my-3 location-results font-weight-bold'}>
-                    Blaisdell (M-Th)
-                  </h5>
-                </div>
-                <div className="p-2 align-self-center">
-                  <p className={'mb-0'}>
-                    <small>Members: $94.00; Non-Members: $202.00</small>
-                  </p>
-                </div>
-              </div>
-              <Card className={'package-results'}>
-                <CardBody>
-                  <Row>
-                    <Col md={8}>
-                      <Row>
-                        <Col md={7}>
-                          <CardText className="mb-0 font-weight-bold">
-                            Swim Lessons
-                          </CardText>
-                          <CardText className={'mb-0'}>
-                            Preschool Child
-                          </CardText>
-                          <CardText className={'text-muted small'}>
-                            Swim Strokes
-                          </CardText>
-                        </Col>
-                        <Col md={5}>
-                          <div className={'d-flex'}>
-                            <CardText
-                              className={
-                                'mb-0 font-weight-bold red text-uppercase mr-3'
-                              }
-                            >
-                              Wait list
-                            </CardText>
-                            <FaQuestionCircle />
-                          </div>
-                          <CardText>8:00 - 9:00am</CardText>
-                        </Col>
-                      </Row>
-                      <hr />
-                      <Row>
-                        <Col md={7}>
-                          <CardText className="mb-0 font-weight-bold">
-                            Swim Lessons
-                          </CardText>
-                          <CardText className={'mb-0'}>Parent & Child</CardText>
-                        </Col>
-                        <Col md={5}>
-                          <CardText className={'mb-0 font-weight-bold orange'}>
-                            5 spots remaining
-                          </CardText>
-                          <CardText>8:00 - 9:00am</CardText>
-                        </Col>
-                      </Row>
-                    </Col>
-                    <Col md={4} className={'align-self-center'}>
-                      <Button className={'py-2 btn-result text-uppercase'}>
-                        Select Package
-                      </Button>
-                    </Col>
-                  </Row>
-                </CardBody>
-              </Card>
-
-              <div className="d-flex">
-                <div className="mr-auto p-2">
-                  <h5 className={'my-3 location-results font-weight-bold'}>
-                    Southdale (M-Th)
-                  </h5>
-                </div>
-                <div className="p-2 align-self-center">
-                  <p className={'mb-0'}>
-                    <small>Members: $94.00; Non-Members: $202.00</small>
-                  </p>
-                </div>
-              </div>
-              <Card className={'package-results'}>
-                <CardBody>
-                  <Row>
-                    <Col md={8}>
-                      <Row>
-                        <Col md={7}>
-                          <CardText className="mb-0 font-weight-bold">
-                            Swim Lessons
-                          </CardText>
-                          <CardText className={'mb-0'}>
-                            Preschool Child
-                          </CardText>
-                          <CardText className={'text-muted small'}>
-                            Swim Strokes
-                          </CardText>
-                        </Col>
-                        <Col md={5}>
-                          <CardText className={'mb-0 font-weight-bold green'}>
-                            9 spots remaining
-                          </CardText>
-                          <CardText>8:30 - 9:30am</CardText>
-                        </Col>
-                      </Row>
-
-                      <hr />
-                      <Row>
-                        <Col md={7}>
-                          <CardText className="mb-0 font-weight-bold">
-                            Swim Lessons
-                          </CardText>
-                          <CardText className={'mb-0'}>Parent & Child</CardText>
-                        </Col>
-                        <Col md={5}>
-                          <CardText className={'mb-0 font-weight-bold orange'}>
-                            2 spots remaining
-                          </CardText>
-                          <CardText>8:00 - 9:00am</CardText>
-                        </Col>
-                      </Row>
-                    </Col>
-                    <Col md={4} className={'m-auto align-self-center'}>
-                      <Button className={'p-2 btn-result text-uppercase'}>
-                        Select Package
-                      </Button>
                     </Col>
                   </Row>
                 </CardBody>
               </Card>
             </Col>
-            <Col md={4} className={'order-first order-md-last'}>
-              <Sidebar />
+            <Col md={12}>
+              <div className={'d-flex ml-auto py-4 justify-content-end'}>
+                <div className="p-2">
+                  <p className={'mb-0 font-weight-bold'}>
+                    Members: $94.00
+                  </p>
+                </div>
+                <div>
+                  <Button
+                    className={
+                      'pt-2 pb-2 pl-5 pr-5 btn-next text-uppercase'
+                    }
+                  >
+                    Add to cart
+                  </Button>
+                </div>
+              </div>
             </Col>
+
           </Row>
         </Container>
       </div>
