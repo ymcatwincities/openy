2919d86b3a49c65136f83244efb483a14834b6b4
Andrey T <andrey@imagexmedia.com>
2018-07-12 17:41:18 +0300

    Extend sessions demo content

--- modules/custom/openy_repeat/src/RepeatManager.php
+++ modules/custom/openy_repeat/src/RepeatManager.php
@@ -196,13 +196,13 @@ class RepeatManager implements SessionInstanceManagerInterface {
     $session_instances = [];
 
     $weekday_mapping = [
-      'sunday' => '0',
-      'monday' => '1',
-      'tuesday' => '2',
-      'wednesday' => '3',
-      'thursday' => '4',
-      'friday' => '5',
-      'saturday' => '6',
+      'sunday' => '1',
+      'monday' => '2',
+      'tuesday' => '3',
+      'wednesday' => '4',
+      'thursday' => '5',
+      'friday' => '6',
+      'saturday' => '7',
     ];
 
     foreach ($session_schedule['dates'] as $schedule_item) {
