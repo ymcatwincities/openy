40c21b4b6bc2a45a73e8c617b47f2284fb48704c
Andrii Podanenko <podarokua@gmail.com>
2018-08-07 15:36:48 +0300

    Sure changes

--- docroot/profiles/contrib/openy/src/Form/ContentSelectForm.php
+++ docroot/profiles/contrib/openy/src/Form/ContentSelectForm.php
@@ -29,6 +29,7 @@ class ContentSelectForm extends FormBase {
       'camps' => $this->t('Demo Camps'),
       'blog' => $this->t('Demo Blog Posts'),
       'news' => $this->t('Demo News Posts'),
+      'event' => $this->t('Demo Event Posts'),
       'programs' => $this->t('Demo Programs & Categories'),
       'landing' => $this->t('Demo Landing Pages'),
       'menus' => $this->t('Demo Menus'),
