50ceb87030d873d737ce25be58986cf838f0c0e2
Andrii Podanenko <podarokua@gmail.com>
2018-08-29 18:10:05 +0300

    Working on local dev

---
+++ docroot/profiles/contrib/openy/modules/openy_features/openy_node/modules/openy_node_alert/js/openy-alerts/src/api.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+const apiURL = process.env.REACT_APP_API_URL || '';
+
+export default axios.create({
+    baseURL: apiURL,
+    auth: {
+        username: 'ygtc',
+        password: 'openy'
+    }
+});
