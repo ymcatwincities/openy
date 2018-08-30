008fc7c36818c9aa099047f937c9829375d20a3a
Andrii Podanenko <podarokua@gmail.com>
2018-08-30 17:36:52 +0300

    Working Proof of Concept with Slick. TODO: theming, cookie, uri context

--- docroot/profiles/contrib/openy/modules/openy_features/openy_node/modules/openy_node_alert/js/openy-alerts/src/index.js
+++ docroot/profiles/contrib/openy/modules/openy_features/openy_node/modules/openy_node_alert/js/openy-alerts/src/index.js
@@ -1,7 +1,9 @@
+/*global Drupal b:true*/
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import AppHeader from './AppHeader';
+import AppFooter from './AppFooter';
 import registerServiceWorker from './registerServiceWorker';
 
 import { Provider } from 'react-redux';
@@ -10,15 +12,38 @@ import store from './stores/store';
 
 
 if (typeof Drupal === 'undefined') {
-    ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('openy_alerts_app'));
-    registerServiceWorker();
+    ReactDOM.render(<Provider store={store}><AppHeader /></Provider>, document.getElementById('openy_alerts_app_header'));
+    // registerServiceWorker();
 }
-// else {
-//     Drupal.behaviors.drupal_block_reactive = {
-//         attach: (context) => {
-//             // Render our component.
-//             ReactDOM.render(<App />, document.getElementById('openy_alerts_app'));
-//             registerServiceWorker();
-//         }
-//     };
-// }
+
+if (typeof Drupal === 'undefined') {
+    ReactDOM.render(<Provider store={store}><AppFooter /></Provider>, document.getElementById('openy_alerts_app_footer'));
+    // registerServiceWorker();
+}
+else {
+    Drupal.behaviors.openy_alerts_app = {
+        attach: (context) => {
+            // Insert header region for alerts after <header> element.
+            if (document.getElementById('openy_alerts_app_header') !== null) {
+                return;
+            }
+            var e = document.createElement('div');
+            e.innerHTML = '<div id="openy_alerts_app_header"></div>';
+            while(e.firstChild) {
+                document.getElementsByTagName('header')[0].parentNode.insertBefore(e.firstChild, document.getElementsByTagName('header')[0].nextSibling);
+            }
+            // Render our component.
+            ReactDOM.render(<Provider store={store}><AppHeader /></Provider>, document.getElementById('openy_alerts_app_header'));
+            // Insert footer region for alerts inside "pre-footer" class element.
+            if (document.getElementById('openy_alerts_app_footer') !== null) {
+                return;
+            }
+            var f = document.createElement('div');
+            f.innerHTML = '<div id="openy_alerts_app_footer"></div>';
+            while(f.firstChild) {
+                document.getElementsByClassName("pre-footer")[0].appendChild(f.firstChild);
+            }
+            ReactDOM.render(<Provider store={store}><AppFooter /></Provider>, document.getElementById('openy_alerts_app_footer'));
+        }
+    };
+}
