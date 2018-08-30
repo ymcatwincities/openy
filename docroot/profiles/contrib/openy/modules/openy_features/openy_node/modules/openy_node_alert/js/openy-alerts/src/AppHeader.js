d34b8bcb8ec62c927d927004e2b1d48aafe75a9b
Andrii Podanenko <podarokua@gmail.com>
2018-08-30 18:35:53 +0300

    Theming 99%

--- docroot/profiles/contrib/openy/modules/openy_features/openy_node/modules/openy_node_alert/js/openy-alerts/src/AppHeader.js
+++ docroot/profiles/contrib/openy/modules/openy_features/openy_node/modules/openy_node_alert/js/openy-alerts/src/AppHeader.js
@@ -10,21 +10,42 @@ import Slider from "react-slick";
 import {fetchAlerts} from "./actions/backend";
 
 class App extends Component {
-
+    constructor(props) {
+        super(props);
+        this.next = this.next.bind(this);
+        this.previous = this.previous.bind(this);
+    }
+    next() {
+        this.slider.slickNext();
+    }
+    previous() {
+        this.slider.slickPrev();
+    }
 
     render() {
+        const NextArrow = () => {
+            return <a href="#" data-role="none" className="slick-next slick-arrow" role="button"
+                      aria-disabled="false">Next</a>
+        };
+        const PrevArrow = () => {
+            return <a href="#" data-role="none" className="slick-prev slick-arrow slick-disabled" role="button"
+                      aria-disabled="true">Previous</a>
+        };
         var sliderSettings = {
-            dots: true,
-            infinite: true,
+            dots: false,
+            infinite: false,
             speed: 500,
             slidesToShow: 1,
-            slidesToScroll: 1
+            slidesToScroll: 1,
+            arrows: false
+            // className: 'slick__slider',
         };
         const HeaderAlertItemList = () => {
             if (this.props.header) {
                 return Object.keys(this.props.header).map(i => {
                     return this.props.header[i].map(a => {
-                        return <HeaderAlertItem key={a.title} label={a.title} iconColor={true} linkTitle={false} description={a.description}/>
+                        return <HeaderAlertItem key={a.title} label={a.title} iconColor={true} linkTitle={false}
+                                                description={a.description}/>
                     });
                 })
             }
@@ -35,10 +56,21 @@ class App extends Component {
         };
         return (
             <div className="App">
-                <div className={'header-alerts-list alerts'}>
-                    <Slider {...sliderSettings}>
+                <div className={'header-alerts-list alerts header-alerts-list-processed'}>
+                    <Slider ref={c => (this.slider = c)} {...sliderSettings}>
                         {HeaderAlertItemList()}
                     </Slider>
+                    <div className="container">
+                        <div className="slick__counter"><span className="current"></span> of <span
+                            className="total"></span></div>
+                        <div className="slick__arrow">
+                            <a href="#" data-role="none" className="slick-prev slick-arrow" role="button"
+                               aria-disabled="true" onClick={this.previous}>Previous</a><a href="#" data-role="none"
+                                                                            className="slick-next slick-arrow"
+                                                                            role="button" aria-disabled="false"
+                                                                                           onClick={this.next}>Next</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
