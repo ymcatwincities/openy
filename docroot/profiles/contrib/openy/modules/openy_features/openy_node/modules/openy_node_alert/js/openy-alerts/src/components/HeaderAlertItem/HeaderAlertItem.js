dc4521d176b6a6dbda8a064954d21a5a1b340f6c
Andrii Podanenko <podarokua@gmail.com>
2018-08-30 18:35:53 +0300

    Theming 99%

--- docroot/profiles/contrib/openy/modules/openy_features/openy_node/modules/openy_node_alert/js/openy-alerts/src/components/HeaderAlertItem/HeaderAlertItem.js
+++ docroot/profiles/contrib/openy/modules/openy_features/openy_node/modules/openy_node_alert/js/openy-alerts/src/components/HeaderAlertItem/HeaderAlertItem.js
@@ -14,30 +14,32 @@ let linkStyle = {
 /**
  * Renders alert item.
  */
-const HeaderAlertItem = ({ iconColor, label, linkTitle, linkUrl, description }) => (
-    <div className="container header-alert">
-        <div className="row site-alert__wrapper">
-            {iconColor &&
+const HeaderAlertItem = ({iconColor, label, linkTitle, linkUrl, description}) => (
+    <div className="site-alert site-alert--header">
+        <div className="container header-alert">
+            <div className="row site-alert__wrapper">
+                {iconColor &&
                 <div className="site-alert__icon" style={iconStyle}>
-                  <i className="fa fa-exclamation-circle" aria-hidden="true"></i>
+                    <i className="fa fa-exclamation-circle" aria-hidden="true"></i>
                 </div>
-            }
-            <div className="col-xs-12 col-sm-6 col-md-7 col-lg-8">
-                <div className="site-alert__title">{renderHTML(label)}</div>
-                <div className="site-alert__content header-alert__content">
-                    {renderHTML(description)}
+                }
+                <div className="col-xs-12 col-sm-6 col-md-7 col-lg-8">
+                    <div className="site-alert__title">{renderHTML(label)}</div>
+                    <div className="site-alert__content header-alert__content">
+                        {renderHTML(description)}
+                    </div>
                 </div>
-            </div>
-            {linkTitle &&
-            <div className="col-xs-12 col-sm-5 col-md-4 col-lg-3 site-alert__cta">
-                <div className="field-alert-link">
-                    <a href={linkUrl} style={linkStyle}>{linkTitle}</a>
+                {linkTitle &&
+                <div className="col-xs-12 col-sm-5 col-md-4 col-lg-3 site-alert__cta">
+                    <div className="field-alert-link">
+                        <a href={linkUrl} style={linkStyle}>{linkTitle}</a>
+                    </div>
                 </div>
-            </div>      
-            }
+                }
 
-            <a href="#close" className="site-alert__dismiss"><i className="fa fa-times" aria-hidden="true"><span
-                className="visually-hidden">Close alert {renderHTML(label)}</span></i></a>
+                <a href="#close" className="site-alert__dismiss"><i className="fa fa-times" aria-hidden="true"><span
+                    className="visually-hidden">Close alert {renderHTML(label)}</span></i></a>
+            </div>
         </div>
     </div>
 );
