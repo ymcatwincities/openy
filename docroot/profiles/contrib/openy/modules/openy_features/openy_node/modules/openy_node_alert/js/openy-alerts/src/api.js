db343836a99b7ec97d7a042f65be4b2fa7bf3335
Andrii Podanenko <podarokua@gmail.com>
2018-09-03 18:49:30 +0300

    Fix subfolder install

--- docroot/profiles/contrib/openy/modules/openy_features/openy_node/modules/openy_node_alert/js/openy-alerts/src/api.js
+++ docroot/profiles/contrib/openy/modules/openy_features/openy_node/modules/openy_node_alert/js/openy-alerts/src/api.js
@@ -1,8 +1,10 @@
+/*global drupalSettings b:true*/
+/*eslint no-restricted-globals: ["warn", "drupalSettings"]*/
 import axios from 'axios';
 const apiURL = process.env.REACT_APP_API_URL || '';
 
 export default axios.create({
-  baseURL: apiURL,
+  baseURL: apiURL + drupalSettings.path.baseUrl,
   auth: {
     username: 'ygtc',
     password: 'openy'
