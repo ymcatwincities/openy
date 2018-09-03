ab23bfaef41f603a8d53433a24feba7824f5778f
Andrii Podanenko <podarokua@gmail.com>
2018-09-03 18:26:40 +0300

    Make alerts working. Cookie - done. Closing - done

--- docroot/profiles/contrib/openy/modules/openy_features/openy_node/modules/openy_node_alert/js/openy-alerts/src/AppFooter.js
+++ docroot/profiles/contrib/openy/modules/openy_features/openy_node/modules/openy_node_alert/js/openy-alerts/src/AppFooter.js
@@ -1,97 +1,124 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import HeaderAlertItem from './components/HeaderAlertItem/HeaderAlertItem';
-import Slider from "react-slick";
+import Slider from 'react-slick';
 
-import {fetchAlerts} from "./actions/backend";
+import { fetchAlerts } from './actions/backend';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.next = this.next.bind(this);
-        this.previous = this.previous.bind(this);
-    }
-    next() {
-        this.slider.slickNext();
-    }
-    previous() {
-        this.slider.slickPrev();
-    }
+  constructor(props) {
+    super(props);
+    this.next = this.next.bind(this);
+    this.previous = this.previous.bind(this);
+  }
+  next() {
+    this.slider.slickNext();
+  }
+  previous() {
+    this.slider.slickPrev();
+  }
 
-    render() {
-        var sliderSettings = {
-            dots: false,
-            infinite: false,
-            speed: 500,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows: false,
-            variableWidth: false,
-            centerMode: true,
-            centerPadding: '0px',
-        };
-        const HeaderAlertItemList = () => {
-            if (this.props.footer) {
-                return Object.keys(this.props.footer).map(i => {
-                    return this.props.footer[i].map(a => {
-                        return <HeaderAlertItem key={a.title} label={a.title} iconColor={a.iconColor} linkTitle={a.linkText} linkUrl={a.linkUrl}
-                                                description={a.description} txtColor={a.textColor} bgColor={a.bgColor}/>
-                    });
-                })
-            }
-            else {
-                return null;
-            }
-
-        };
-        return (
-            <div className="App">
-                <div className={'footer-alerts-list alerts footer-alerts-list-processed'}>
-                    <Slider ref={c => (this.slider = c)} {...sliderSettings}>
-                        {HeaderAlertItemList()}
-                    </Slider>
-                    <div className="container">
-                        <div className="slick__counter"><span className="current"></span> of <span
-                            className="total"></span></div>
-                        <div className="slick__arrow">
-                            <a href="#" data-role="none" className="slick-prev slick-arrow" role="button"
-                               aria-disabled="true" onClick={this.previous}>Previous</a><a href="#" data-role="none"
-                                                                                           className="slick-next slick-arrow"
-                                                                                           role="button" aria-disabled="false"
-                                                                                           onClick={this.next}>Next</a>
-                        </div>
-                    </div>
-                </div>
+  render() {
+    var sliderSettings = {
+      dots: false,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      variableWidth: false,
+      centerMode: true,
+      centerPadding: '0px'
+    };
+    const HeaderAlertItemList = () => {
+      if (this.props.footer) {
+        return Object.keys(this.props.footer).map(i => {
+          return this.props.footer[i].map(a => {
+            return (
+              <HeaderAlertItem
+                key={a.id}
+                alertId={a.id}
+                label={a.title}
+                iconColor={a.iconColor}
+                linkTitle={a.linkText}
+                linkUrl={a.linkUrl}
+                description={a.description}
+                txtColor={a.textColor}
+                bgColor={a.bgColor}
+              />
+            );
+          });
+        });
+      } else {
+        return null;
+      }
+    };
+    return (
+      <div className="App">
+        <div
+          className={'footer-alerts-list alerts footer-alerts-list-processed'}
+        >
+          <Slider ref={c => (this.slider = c)} {...sliderSettings}>
+            {HeaderAlertItemList()}
+          </Slider>
+          <div className="container">
+            <div className="slick__counter">
+              <span className="current" /> of <span className="total" />
             </div>
-        );
-    }
+            <div className="slick__arrow">
+              <a
+                href="#"
+                data-role="none"
+                className="slick-prev slick-arrow"
+                role="button"
+                aria-disabled="true"
+                onClick={this.previous}
+              >
+                Previous
+              </a>
+              <a
+                href="#"
+                data-role="none"
+                className="slick-next slick-arrow"
+                role="button"
+                aria-disabled="false"
+                onClick={this.next}
+              >
+                Next
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
-    componentDidMount() {
-        // this.props.fetchAlerts('/locations/ridgedale_ymca');
-    }
+  componentDidMount() {
+    // this.props.fetchAlerts('/locations/ridgedale_ymca');
+  }
 }
 
 const mapDispatchToProps = dispatch => {
-    return {
-        fetchAlerts: uri => {
-            dispatch(fetchAlerts(uri))
-        }
+  return {
+    fetchAlerts: uri => {
+      dispatch(fetchAlerts(uri));
     }
-}
+  };
+};
 
 const mapStateToProps = state => {
-    return {
-        alerts: state.init.alerts,
-        header: state.init.alerts.header,
-        footer: state.init.alerts.footer,
-    }
-}
+  return {
+    alerts: state.init.alerts,
+    header: state.init.alerts.header,
+    footer: state.init.alerts.footer
+  };
+};
 
 const AppFooter = connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(App)
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
 
 export default AppFooter;
--- docroot/profiles/contrib/openy/modules/openy_features/openy_node/modules/openy_node_alert/js/openy-alerts/src/stores/store.js
+++ docroot/profiles/contrib/openy/modules/openy_features/openy_node/modules/openy_node_alert/js/openy-alerts/src/stores/store.js
@@ -1,3 +1,3 @@
 const store =
-    process.env.NODE_ENV === 'production' ? 'store.prod' : 'store.dev';
+  process.env.NODE_ENV === 'production' ? 'store.prod' : 'store.dev';
 module.exports = require(`./${store}`);
