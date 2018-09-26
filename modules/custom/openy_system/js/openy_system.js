a099916d7f3f2eb533fe16afd8123833f11ff978
Andriy Prokopenko <andprk@gmail.com>
2018-09-26 16:55:18 +0300

    [OPENY-158] Update popup window text

--- modules/custom/openy_system/js/openy_system.js
+++ modules/custom/openy_system/js/openy_system.js
@@ -7,7 +7,7 @@
   Drupal.behaviors.openy_confirm_form = {
     attach: function (context, settings) {
       $("#packages-uninstall-confirm").on('click', function ()  {
-        return confirm(Drupal.t("Are you sure you want to uninstall packages?"))
+        return confirm(Drupal.t("Are you sure you want to uninstall packages? Data and content from within these packages will be lost."))
       });
     }
   };
