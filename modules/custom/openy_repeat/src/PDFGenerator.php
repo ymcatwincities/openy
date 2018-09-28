ffef9372161a8f4425f3669dbee8bc6c878e5482
Andrey T <andrey@imagexmedia.com>
2018-09-28 16:46:22 +0300

    fixed major issues with pdf generation for schedules

--- modules/custom/openy_repeat/src/PDFGenerator.php
+++ modules/custom/openy_repeat/src/PDFGenerator.php
@@ -19,11 +19,11 @@ class PDFGenerator {
     if (empty($settings)) {
       return;
     }
-    if ($temporary_directory = file_directory_temp()) {
-      define("_MPDF_TEMP_PATH", $temporary_directory);
-    }
+    $temporary_directory = file_directory_temp();
+    define("_MPDF_TEMP_PATH", $temporary_directory);
+    ini_set("pcre.backtrack_limit", "5000000");
     $html = mb_convert_encoding(render($settings['body']), 'UTF-8', 'UTF-8');
-    $mpdf = new \Mpdf\Mpdf(['format' => 'A4-L']);
+    $mpdf = new \Mpdf\Mpdf(['format' => 'A4-L', 'tempDir' => $temporary_directory]);
     $stylesheet = file_get_contents(drupal_get_path('module', 'openy_repeat') . '/css/print_pdf.css');
     $mpdf->SetHTMLFooter(render($settings['footer']));
     $mpdf->WriteHTML($stylesheet, 1);
