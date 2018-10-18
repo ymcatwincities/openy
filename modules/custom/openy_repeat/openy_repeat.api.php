33ffbec5adb9221661253dfc54280168a90d605b
Yuriy Gerasimov <yuri.gerasimov@gmail.com>
2018-10-18 09:47:18 -0700

    Issue #1342: Add Sign Up button for GroupExPro integration.

---
+++ modules/custom/openy_repeat/openy_repeat.api.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Documents hooks provided by this module.
+ */
+
+/**
+ * Modify the schedules search results prior output.
+ * 
+ * Example: GroupExPro modifies links to register for activity.
+ */
+function hook_openy_repeat_results_alter(&$result, $request) {
+
+}
+
