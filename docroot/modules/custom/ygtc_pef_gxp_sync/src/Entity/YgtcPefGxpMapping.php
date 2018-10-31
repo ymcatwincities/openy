21d933e4329a7da0e2b71e0ad49b1bb53d79fc96
Dmytro Danylevskyi <dd@root.ua>
2018-10-31 16:29:09 +0200

    Add YgtcPefGxpMapping

---
+++ docroot/modules/custom/ygtc_pef_gxp_sync/src/Entity/YgtcPefGxpMapping.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Drupal\ygtc_pef_gxp_sync\Entity;
+
+use Drupal\Core\Annotation\Translation;
+use Drupal\Core\Entity\Annotation\ContentEntityType;
+use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
+
+/**
+ * Defines the repeat entity class.
+ *
+ * @ContentEntityType(
+ *   id = "ygtc_pef_gxp_mapping",
+ *   label = @Translation("Ygtc PEF GXP Mapping"),
+ *   base_table = "ygtc_pef_gxp_mapping",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "uuid" = "uuid"
+ *   },
+ *   translatable = FALSE,
+ *   fieldable = FALSE,
+ *   admin_permission = "administer site configuration",
+ * )
+ */
+class YgtcPefGxpMapping extends ContentEntityBase implements YgtcPefGxpMappingInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+    $fields['id'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('Repeat ID'))
+      ->setDescription(t('The repeat ID.'))
+      ->setReadOnly(TRUE);
+
+    $fields['uuid'] = BaseFieldDefinition::create('uuid')
+      ->setLabel(t('UUID'))
+      ->setDescription(t('The UUID of the entity.'))
+      ->setReadOnly(TRUE);
+
+    $fields['session'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Session'))
+      ->setDescription(t('Reference to the Session.'))
+      ->setSetting('target_type', 'node')
+      ->setDisplayOptions('view', [
+        'label' => 'above',
+        'type' => 'entity_reference_label',
+      ])
+      ->setDisplayOptions('form', [
+        'type' => 'entity_reference_autocomplete'
+      ]);
+
+    $fields['md5'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('MD5 hash of source object'))
+      ->setDescription(t('Used to compare with source data object.'))
+      ->setRequired(TRUE)
+      ->setSettings(array(
+        'default_value' => '',
+        'max_length' => 32,
+      ))
+      ->setDisplayOptions('view', array(
+        'label' => 'above',
+        'type' => 'string',
+        'weight' => 0,
+      ))
+      ->setDisplayOptions('form', array(
+        'type' => 'string_textfield',
+        'weight' => 0,
+      ))
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayConfigurable('view', TRUE);
+
+    return $fields;
+  }
+
+}
---
+++ docroot/modules/custom/ygtc_pef_gxp_sync/src/Entity/YgtcPefGxpMappingInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\ygtc_pef_gxp_sync\Entity;
+
+use Drupal\Core\Entity\ContentEntityInterface;
+
+/**
+ * Interface YgtcPefGxpMappingInterface.
+ *
+ * @package Drupal\ygtc_pef_gxp_sync\Entity
+ */
+interface YgtcPefGxpMappingInterface extends ContentEntityInterface {
+
+}
