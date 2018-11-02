38e9d230a058d5eba53b5558eb8a380284562342
Dmytro Danylevskyi <dd@root.ua>
2018-11-02 14:04:58 +0200

    Recreate session entity if groupex sours has been changed

---
+++ docroot/modules/custom/ygtc_pef_gxp_sync/src/YgtcPefGxpMappingRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\ygtc_pef_gxp_sync;
+
+use Drupal\Core\Entity\EntityTypeManager;
+
+class YgtcPefGxpMappingRepository {
+
+  /**
+   * Entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManager
+   */
+  protected $entityTypeManager;
+
+  /**
+   * YgtcPefGxpMappingRepository constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManager $entityTypeManager
+   *   Entity type manager.
+   */
+  public function __construct(EntityTypeManager $entityTypeManager) {
+    $this->entityTypeManager = $entityTypeManager;
+  }
+
+  /**
+   * Get mapping items by product id.
+   *
+   * @param $productId
+   *
+   * @return \Drupal\Core\Entity\EntityInterface[]
+   *   The list of founded items.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   */
+  public function getMappingByProductId($productId) {
+    return $this->entityTypeManager
+      ->getStorage('ygtc_pef_gxp_mapping')
+      ->loadByProperties(['product_id' => $productId]);
+  }
+
+}
