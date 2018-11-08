4ca5f2a6f64262074053490f5ada7d6f43e88787
Andriy Prokopenko <andprk@gmail.com>
2018-11-08 13:23:33 +0200

    [OPENY-171] Fix routes and descriptions

--- modules/custom/openy_group_schedules/modules/groupex_form_cache/src/GroupexFormCacheHtmlRouteProvider.php
+++ modules/custom/openy_group_schedules/modules/groupex_form_cache/src/GroupexFormCacheHtmlRouteProvider.php
@@ -110,7 +110,7 @@ class GroupexFormCacheHtmlRouteProvider extends AdminHtmlRouteProvider {
    */
   protected function getSettingsFormRoute(EntityTypeInterface $entity_type) {
     if (!$entity_type->getBundleEntityType()) {
-      $route = new Route("/admin/structure/{$entity_type->id()}/settings");
+      $route = new Route("/admin/openy/integrations/groupex-pro/{$entity_type->id()}/settings");
       $route
         ->setDefaults([
           '_form' => 'Drupal\groupex_form_cache\Form\GroupexFormCacheSettingsForm',
