2deace900db3f4070bc57723023458a1eab7c464
Andrii Podanenko <podarokua@gmail.com>
2018-11-30 15:45:59 +0200

    Add new command cypress_run.sh to run just cypress tests

--- docroot/cypress_run.sh
+++ docroot/cypress_run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-if [ "$1" = "--windows" ]; then
-    time ansible-playbook -vvvv reinstall.yml -i 'localhost,' --connection=local --extra-vars "is_windows=true cypress_run=true" --tags="cypress-commands"
+if [ ! -z $1 ]; then
+    time ansible-playbook -vvvv reinstall.yml -t cypress-commands -e all_cypress_tests=false -e cypress_test_to_run="$1"  -e cypress_run=true -i 'localhost,' --connection=local
+    cat build_reports/cypress.html
 else
     time ansible-playbook -vvvv reinstall.yml -t cypress-commands -e cypress_run=true -i 'localhost,' --connection=local
 fi
