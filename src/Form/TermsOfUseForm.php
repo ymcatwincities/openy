f83472ddbf6e0a3d10bb5397f2835669f6ae90f9
Miroslav Lee <miroslav.lee@fivejars.com>
2019-01-08 20:24:22 +0300

    [OPENY-238] Rename TermsOfUseForm class and added functions for translation

--- src/Form/TermsOfUseForm.php
+++ src/Form/TermsOfUseForm.php
@@ -8,7 +8,7 @@ use Drupal\Core\Form\FormStateInterface;
 /**
  * Provides a form to configure and rewrite settings.php.
  */
-class TermOfUseForm extends FormBase {
+class TermsOfUseForm extends FormBase {
 
   /**
    * {@inheritdoc}
@@ -21,41 +21,41 @@ class TermOfUseForm extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $form['#title'] = t('Terms of Use');
+    $form['#title'] = $this->t('Terms of Use');
 
     $form['participant'] = [
       '#type' => 'checkbox',
-      '#title' => 'We agree to the <a target="_blank" href="https://github.com/ymcatwincities/openy/wiki/Open-Y-Participant-Agreement">Open Y Participant Agreement</a> and <a target="_blank" href="https://github.com/ymcatwincities/openy/wiki/Open-Y-Terms-of-Use">Terms of Use</a>',
+      '#title' => $this->t('We agree to the <a target="_blank" href="https://github.com/ymcatwincities/openy/wiki/Open-Y-Participant-Agreement">Open Y Participant Agreement</a> and <a target="_blank" href="https://github.com/ymcatwincities/openy/wiki/Open-Y-Terms-of-Use">Terms of Use</a>'),
       '#weight' => 1,
     ];
 
     $form['llc'] = [
       '#type' => 'checkbox',
-      '#title' => 'Open Y, LLC is a separate entity established by YMCA of the Greater Twin Cities to support and amplify digital collaboration among YMCA associations. YUSA supports the Open Y platform with respect to use by its Member Associations but is not responsible for and does not control the services provided by Open Y, LLC.',
+      '#title' => $this->t('Open Y, LLC is a separate entity established by YMCA of the Greater Twin Cities to support and amplify digital collaboration among YMCA associations. YUSA supports the Open Y platform with respect to use by its Member Associations but is not responsible for and does not control the services provided by Open Y, LLC.'),
       '#weight' => 2,
     ];
 
     $form['privacy'] = [
       '#type' => 'checkbox',
-      '#title' => 'Open Y recommends that each participating YMCA association develop and implement its own cybersecurity policies and obtain cyber liability and data privacy insurance.',
+      '#title' => $this->t('Open Y recommends that each participating YMCA association develop and implement its own cybersecurity policies and obtain cyber liability and data privacy insurance.'),
       '#weight' => 3,
     ];
 
     $form['acknowledge'] = [
       '#type' => 'checkbox',
-      '#title' => 'I acknowledge that Open Y is open source content and that all content is provided “as is” without any warranty of any kind. Open Y makes no warranty that its services will meet your requirements, be safe, secure, uninterrupted, timely, accurate, or error-free, or that your information will be secure. Open Y will not maintain and support Open Y templates indefinitely. The entire risk as to the quality and performance of the content is with you.',
+      '#title' => $this->t('I acknowledge that Open Y is open source content and that all content is provided “as is” without any warranty of any kind. Open Y makes no warranty that its services will meet your requirements, be safe, secure, uninterrupted, timely, accurate, or error-free, or that your information will be secure. Open Y will not maintain and support Open Y templates indefinitely. The entire risk as to the quality and performance of the content is with you.'),
       '#weight' => 4,
     ];
 
     $form['obtaining'] = [
       '#type' => 'checkbox',
-      '#title' => 'Open Y recommends obtaining a reputable agency to assist with the implementation of the Open Y platform and further development for your specific needs.',
+      '#title' => $this->t('Open Y recommends obtaining a reputable agency to assist with the implementation of the Open Y platform and further development for your specific needs.'),
       '#weight' => 5,
     ];
 
     $form['submit'] = [
       '#type' => 'submit',
-      '#value' => 'Continue Installation',
+      '#value' => $this->t('Continue Installation'),
       '#weight' => 15,
       '#states' => [
         'disabled' => [
