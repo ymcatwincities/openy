4298214651672c72714f86f45a930bc8050c0ae5
Aaron Christian <aaronc@imagexmedia.com>
2019-01-10 11:35:52 -0800

    OS-258: Makes the toggle card selected state available as a vue method.

--- modules/custom/openy_repeat/js/locations.js
+++ modules/custom/openy_repeat/js/locations.js
@@ -5,6 +5,26 @@
   Drupal.behaviors.repeat_locations = {
     attach: function (context, settings) {
 
+      $(window).once('openy-load-selected-locations').on('load', function() {
+        $('.openy-card__item input', context).each(function () {
+          if ($(this).is(':checked')) {
+            $(this).parents('.openy-card__item').addClass('selected');
+          }
+        });
+        toggleSubmit(context);
+      });
+
+      // Toggle active class on location item.
+      $('.openy-card__item input', context).once('openy-selected-locations').on('change', function() {
+        if(!$(this).parents('.openy-card__item').hasClass('selected')) {
+          $(this).parents('.openy-card__item').addClass('selected');
+        }
+        else {
+          $(this).parents('.openy-card__item').removeClass('selected');
+        }
+        toggleSubmit(context);
+      });
+
       // Attach location arguments to url on submit.
       $('.js-submit-locations', context).once('openy-submit-locations').click(function() {
         if ($(this).hasClass('disabled')) {
@@ -24,6 +44,19 @@
         location.href = url + '/?locations=' + chkArray.join(',');
       });
 
+    }
+  };
+
+  // Toggle disable the submit button.
+  var toggleSubmit = function(context) {
+    if($('.openy-card__item.selected label').length > 0) {
+      $('.js-submit-locations', context)
+        .removeClass('disabled')
+        .parent()
+        .find('.error')
+        .remove();
+    } else {
+      $('.js-submit-locations', context).addClass('disabled');
     }
   };
 
