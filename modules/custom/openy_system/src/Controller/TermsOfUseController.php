11ea4c66f231d27883a51ad253c60d92c629a125
Miroslav Lee <miroslav.lee@fivejars.com>
2019-01-29 12:57:32 +0300

    [OPENY-264] Code refactoring

--- modules/custom/openy_system/src/Controller/TermsOfUseController.php
+++ modules/custom/openy_system/src/Controller/TermsOfUseController.php
@@ -3,6 +3,8 @@
 namespace Drupal\openy_system\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Datetime\DateFormatterInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 
@@ -12,21 +14,44 @@ use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 class TermsOfUseController extends ControllerBase implements ContainerInjectionInterface {
 
   /**
+   * TermsOfUseController constructor.
+   *
+   * @param \Drupal\Core\Datetime\DateFormatterInterface $dateFormatter
+   */
+  public function __construct(DateFormatterInterface $dateFormatter) {
+    $this->dateFormatter = $dateFormatter;
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+   *   The Drupal service container.
+   *
+   * @return static
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('date.formatter')
+    );
+  }
+
+  /**
    * Provides the terms.txt file.
    *
    * @return \Symfony\Component\HttpFoundation\Response
    *   The terms.txt file as a response object with 'text/plain' content type.
    */
   public function content() {
-    $config = \Drupal::config('openy.terms_and_conditions.schema');
+    $config = $this->config('openy.terms_and_conditions.schema');
     $timestamp = $config->get('accepted_version');
     $content = $this->t('Terms and Conditions were not accepted');
 
     // Is accepted.
     if ($timestamp) {
-      $date = \Drupal::service('date.formatter')
+      $date = $this->dateFormatter
         ->format($timestamp, 'custom', 'F d, Y', 'UTC');
-      $time = \Drupal::service('date.formatter')
+      $time = $this->dateFormatter
         ->format($timestamp, 'custom', 'H:i:s', 'UTC');
       $content = $this->t(
         'Terms and Conditions were accepted on @date at @time time UTC',
