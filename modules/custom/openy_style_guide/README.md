c656ade81a1ab716d61bff07e9257dfea23ab3d9
Shuklina <ls@root.ua>
2019-02-13 13:58:52 +0200

    Create Style Guide module

---
+++ modules/custom/openy_style_guide/README.md
@@ -0,0 +1,6 @@
+# Open Y Style Guide. 
+Provides a Style Guide page.
+## How to use:
+- [ ] Create `Style Guide` menu;
+- [ ] Create Landing page with provide a menu link to `Style Guide` menu;
+- [ ] Go to path `/styleguide`.
---
+++ modules/custom/openy_style_guide/src/Controller/OpenyStyleGuideController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Drupal\openy_style_guide\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Url;
+
+/**
+ * Provides route responses for the Open Y Style Guide module.
+ */
+class OpenyStyleGuideController extends ControllerBase {
+
+  /**
+   * Renders content.
+   */
+  public function content() {
+    $query = \Drupal::service('entity.query')
+      ->get('menu_link_content')
+      ->condition('menu_name', 'style-guide');
+    $entity_ids = $query->execute();
+
+    /** @var \Drupal\Core\Entity\Sql\SqlContentEntityStorage $storageManager */
+    $storageManager = \Drupal::entityTypeManager()->getStorage('menu_link_content');
+    $activeTheme = \Drupal::service('theme.manager')->getActiveTheme();
+
+    $items = [];
+    foreach ($entity_ids as $id) {
+      $entity = $storageManager->load($id);
+      $value = $entity->get('link')->getValue();
+      $items[] = [
+        'title' => $entity->get('title')->value,
+        'link' => Url::fromUri($value[0]['uri']),
+      ];
+    }
+    if (empty($items)) {
+      return [
+        '#markup' => '<p>No Style Guide items found.</p>',
+      ];
+    }
+
+    return [
+      '#theme' => 'style_guide',
+      '#items' =>  $items,
+      '#active_theme' => $activeTheme->getName(),
+    ];
+  }
+}
