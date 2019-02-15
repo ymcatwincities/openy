21d114abdd98b66e08a8bda5abb872ae95ae7125
Brandon Stone <b@bunnylaundering.com>
2019-02-14 17:21:39 -0800

    Implements a JS refresh functionality for today hours.

--- modules/custom/openy_hours_formatter/js/openy_hours_formatter.js
+++ modules/custom/openy_hours_formatter/js/openy_hours_formatter.js
@@ -1,4 +1,5 @@
 (function ($) {
+
   "use strict";
   Drupal.behaviors.openy_hours_formatter = {
     attach: function (context, settings) {
@@ -21,5 +22,83 @@
           .find('.branch-hours').addClass('hidden');
       });
     }
+  };
+
+  Drupal.behaviors.today_hours = {
+
+    /**
+     * Used to track the setInveral.
+     */
+    refreshTimer: false,
+
+    /**
+     * See https://moment.github.io/luxon/docs/manual/formatting.
+     * @returns {string}
+     */
+    getDayOfWeek() {
+      return luxon.DateTime.local().setZone('America/Los_Angeles').toFormat('c');
+    },
+
+    /**
+     * See https://moment.github.io/luxon/docs/manual/formatting.
+     * @returns {string}
+     */
+    getDate() {
+      return luxon.DateTime.local().setZone('America/Los_Angeles').toFormat('yyyy-LL-dd');
+    },
+
+    /**
+     * Primary method for updating the today hours.
+     */
+    updateTodayHours() {
+      var $todayHours = $('.today-hours > .today');
+      var hoursData = [];
+      var branchHours = drupalSettings.openy_hours_formatter.branch_hours || [];
+
+      // Prioritize these arbitrary hours names first.
+      ['branch_hours', 'center_hours', 'open_hours', 'before_school_enrichment'].reverse().forEach(function(name) {
+        if (typeof branchHours[name] != 'undefined') {
+          hoursData = branchHours[name];
+        }
+      });
+      // Fallback to the first set of hours then.
+      if (!hoursData.length && branchHours.length) {
+        hoursData = branchHours.slice(0, 1);
+      }
+
+      if (hoursData.length) {
+        var todayString = this.getDate();
+        var dayOfWeek = this.getDayOfWeek();
+        dayOfWeek--;
+        var hours = hoursData;
+        var exceptions = []; // Holidays and other day exceptions will come later.
+
+        if (typeof exceptions[todayString] != 'undefined') {
+          $todayHours.text(exceptions[todayString]);
+        }
+        else {
+          $todayHours.text(hours[dayOfWeek]);
+        }
+      }
+    },
+
+    attach: function (context, settings) {
+
+      var $todayHours = $('.today-hours > .today');
+      var onceClass = 'refresh-interval-set';
+
+      // Bail out if there's already refresh action set.
+      if (!$todayHours.hasClass(onceClass)) {
+
+        // This will ensure that if people leave the tab open or the page comes back
+        // into memory on a phone the hour will always be correct.
+        this.refreshTimer = setInterval(this.updateTodayHours, 60 * 1000);
+
+        // RUn for the first time
+        this.updateTodayHours();
+        $todayHours.addClass(onceClass);
+      }
+    }
+
   };
 })(jQuery);
