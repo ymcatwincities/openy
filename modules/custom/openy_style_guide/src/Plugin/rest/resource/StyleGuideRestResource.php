e9bcb4bd72a6ebfa80f75424220986cbef280649
Shuklina <ls@root.ua>
2019-02-25 14:47:21 +0200

    Add DI for Rest Plugin

--- modules/custom/openy_style_guide/src/Plugin/rest/resource/StyleGuideRestResource.php
+++ modules/custom/openy_style_guide/src/Plugin/rest/resource/StyleGuideRestResource.php
@@ -2,9 +2,14 @@
 
 namespace Drupal\openy_style_guide\Plugin\rest\resource;
 
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Entity\Query\Sql\Query;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Url;
 use Drupal\rest\Plugin\ResourceBase;
 use Drupal\rest\ModifiedResourceResponse;
-use Drupal\Core\Url;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Psr\Log\LoggerInterface;
 
 /**
  * Provides a resource to get view modes by entity and bundle.
@@ -17,23 +22,84 @@ use Drupal\Core\Url;
  *   }
  * )
  */
-class StyleGuideRestResource extends ResourceBase {
+class StyleGuideRestResource extends ResourceBase implements ContainerFactoryPluginInterface {
 
   /**
-   * Responds to entity GET requests.
-   * @return \Drupal\rest\ResourceResponse
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * @var \Drupal\Core\Entity\Query\Sql\Query
+   */
+  protected $queryFactory;
+
+  /**
+   * Constructs a new StyleGuideRestResource object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param array $serializer_formats
+   *   The available serialization formats.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   A logger instance.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager service.
+   * @param \Drupal\Core\Entity\Query\Sql\Query $queryFactory
+   *   The query factory.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    array $serializer_formats,
+    LoggerInterface $logger,
+    EntityTypeManagerInterface $entity_type_manager,
+    Query $queryFactory) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
+    $this->entityTypeManager = $entity_type_manager;
+    $this->queryFactory = $queryFactory;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->getParameter('serializer.formats'),
+      $container->get('logger.factory')->get('openy_style_guide'),
+      $container->get('entity_type.manager'),
+      $container->get('entity.query')->get('menu_link_content')
+    );
+  }
+
+  /**
+   * Responds to GET requests.
+   *
+   * @return ModifiedResourceResponse
+   *   The HTTP response object.
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
    */
   public function get() {
-    $query = \Drupal::service('entity.query')
-      ->get('menu_link_content')
+    $query = $this->queryFactory
       ->condition('menu_name', 'style-guide');
     $entity_ids = $query->execute();
 
     /** @var \Drupal\Core\Entity\Sql\SqlContentEntityStorage $storageManager */
-    $storageManager = \Drupal::entityTypeManager()->getStorage('menu_link_content');
-    $items = [];
-    $storage = \Drupal::entityTypeManager()->getStorage('domain');
+    $storageManager = $this->entityTypeManager->getStorage('menu_link_content');
+    $storage = $this->entityTypeManager->getStorage('domain');
     $domains = $storage->loadMultiple();
+    $items = [];
 
     /** @var  $domain */
     foreach ($domains as $domain) {
