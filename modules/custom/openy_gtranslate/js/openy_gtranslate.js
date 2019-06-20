bbd4c0d62d423efb821a5fb031d2bafa8d2c6983
Anatoliy Polyakov <anpolimus@gmail.com>
2019-06-20 16:40:03 +0200

    Rebuilt google translate backend logic

--- modules/custom/openy_gtranslate/js/openy_gtranslate.js
+++ modules/custom/openy_gtranslate/js/openy_gtranslate.js
@@ -1,25 +1,42 @@
-(function ($) {
+/**
+ * @file openy_gtranslate.js
+ */
+(function ($, Drupal, drupalSettings) {
 
-  Drupal.behaviors.openy_gtranslate = {
-    attach: function (context, settings) {
-      $('button.navbar-toggle, button.navbar-toggler').click(function () {
-        var langSelect = $('.goog-te-menu-frame').first();
+    "use strict";
 
-        $('nav .navbar-nav li.language > a').on('click', function (e, context) {
-          e.preventDefault();
-          langSelect.show();
-          langSelect.addClass('open');
-          return false;
-        });
+    Drupal.behaviors.openy_gtranslate = {
+        attach: function (context, settings) {
+            setTimeout(function () {
+                var langSelect = $('.goog-te-menu-frame').first();
 
-        $('body').on('click', function (e, context) {
-          if (langSelect.hasClass('open')) {
-            langSelect.hide();
-            langSelect.removeClass('open');
-          }
-        });
-      });
-    }
-  };
+                $('nav .navbar-nav li.language > a').on('click', function (e, context) {
+                    e.preventDefault();
+                    langSelect.show();
+                    langSelect.addClass('open');
+                    return false;
+                });
 
-})(jQuery);
+                $('body').on('click', function (e, context) {
+                    if (langSelect.hasClass('open')) {
+                        langSelect.hide();
+                        langSelect.removeClass('open');
+                    }
+                });
+            }, 100);
+        }
+    };
+
+    Drupal.behaviors.openy_gtranslate_mobile = {
+        attach: function (context, settings) {
+            var el = $('#google_translate_element');
+            setTimeout(function () {
+                $(".navbar-toggler, .navbar-toggle").on('click', function () {
+                    $('.navbar-nav > li.language a').hide();
+                    el.removeClass('hidden-xs').removeClass('hidden-sm').appendTo('.navbar-nav > li.language');
+                });
+            }, 100);
+        }
+    };
+
+}(jQuery, Drupal, drupalSettings));
