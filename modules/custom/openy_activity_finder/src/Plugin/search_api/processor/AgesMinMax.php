ba20d07b461e72ab536c83deb71aa0e0b7e385ec
Andrey T <andrey@imagexmedia.com>
2019-06-26 14:38:06 -0700

    AF: improve ages search processor, etc

--- modules/custom/openy_activity_finder/src/Plugin/search_api/processor/AgesMinMax.php
+++ modules/custom/openy_activity_finder/src/Plugin/search_api/processor/AgesMinMax.php
@@ -35,6 +35,7 @@ class AgesMinMax extends ProcessorPluginBase {
         'description' => $this->t('Creates range of integer values from 2 provided min max values.'),
         'type' => 'string',
         'processor_id' => $this->getPluginId(),
+        'is_list' => TRUE,
       ];
       $properties['search_api_af_ages_min_max'] = new ProcessorProperty($definition);
     }
@@ -53,17 +54,18 @@ class AgesMinMax extends ProcessorPluginBase {
       $max_age = $entity->field_session_max_age->value;
       if (empty($min_age)) {
         // Set min age as 0 years if min age was not set in the session.
-        $max_age = 0;
+        $min_age = 0;
       }
       if (empty($max_age)) {
         // Set max age as 100 years if max age was not set the a session.
         $max_age = 100 * 12;
       }
-      if ($min_age && $max_age) {
-        $fields = $this->getFieldsHelper()
-          ->filterForPropertyPath($item->getFields(), NULL, 'search_api_af_ages_min_max');
-        foreach ($fields as $field) {
-          $field->addValue(range($min_age, $max_age));
+      $range = range($min_age, $max_age, 6);
+      $fields = $this->getFieldsHelper()
+        ->filterForPropertyPath($item->getFields(), NULL, 'search_api_af_ages_min_max');
+      foreach ($fields as $field) {
+        foreach ($range as $value) {
+          $field->addValue($value);
         }
       }
     }
