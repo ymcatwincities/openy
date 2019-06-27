f5e796f1bca715f9c8b5f29f78fe0a3fa1371fa0
hamrant <karpylenko.ihor@gmail.com>
2019-06-27 23:05:09 +0300

    [YGBWHB-29] Implement plugins on backend

---
+++ src/HomeBranchLibraryBase.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Drupal\openy_home_branch;
+
+use Drupal\Component\Plugin\PluginBase;
+
+/**
+ * Defines the base plugin for HomeBranchLibrary classes.
+ *
+ * @see \Drupal\openy_home_branch\HomeBranchLibraryManager
+ * @see \Drupal\openy_home_branch\HomeBranchLibraryInterface
+ * @see \Drupal\openy_home_branch\Annotation\HomeBranchLibrary
+ * @see plugin_api
+ */
+class HomeBranchLibraryBase extends PluginBase implements HomeBranchLibraryInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getId() {
+    return $this->pluginDefinition['id'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getTitle() {
+    return $this->pluginDefinition['title'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getEntityName() {
+    return $this->pluginDefinition['entity'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isAllowedForAttaching($variables) {
+    return TRUE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getLibrary() {
+    return '';
+  }
+
+}
