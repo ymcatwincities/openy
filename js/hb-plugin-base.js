15efc09dc73d5754b6971c9c74fa308e3ca57429
Andrey Maximov <savagge@gmail.com>
2019-07-30 22:37:50 +0300

    [YGBWHB-15] Refactor plugin system and the plugins accordingly

--- js/hb-plugin-base.js
+++ js/hb-plugin-base.js
@@ -12,10 +12,10 @@
     this.settings = $.extend({
 
       // Selector that related to event (on-change, on-click).
-      selector: '.some-selector',
+      selector: null,
 
       // Event that trigger onChange function.
-      event: 'change',
+      event: null,
 
       // Storage for created element.
       element: null,
@@ -31,48 +31,26 @@
 
     }, options);
 
-
     let self = this;
 
     // Re-init plugin instance on storage update.
     $(document).on('hb-after-storage-update', function (event, data) {
-      self.settings.init();
+      self.settings.init(self);
     });
 
-    // Run settings.onChange function on settings.event.
-    // Note: unbind() used for prevent events firing multiple times.
-    $(this.settings.selector).unbind().on(this.settings.event, function (event) {
-      // TODO: looks like on-click event for hb-menu-selector.js not work here.
-      self.settings.onChange(event, this);
-    });
-
-    // Add Markup provided by plugin instance.
-    self.settings.addMarkup();
-    self.settings.init();
-
-    return this;
-  };
-
-  /**
-   * Init Home Branch plugins on the page.
-   *
-   * @type {Drupal~behavior}
-   */
-  Drupal.behaviors.openyHomeBranch = {
-    attach(context, settings) {
-      $('body', context).once('hb-plugin').each(function () {
-        if (typeof Drupal.homeBranch.plugins !== 'undefined' && Drupal.homeBranch.plugins.length > 0) {
-          Drupal.homeBranch.plugins.forEach(function (plugin, key, arr, context) {
-            plugin.attach(context);
-          });
-        }
-      });
-
-      // TODO: temp solution.
-      $('a.hb-menu-selector').unbind().on('click', function (event) {
-        $(document).trigger('hb-modal-show');
+    // Run settings.onChange function when settings.event fires.
+    if (this.settings.event && this.settings.selector) {
+      $(document).on(this.settings.event, this.settings.selector, function (event, el) {
+        self.settings.onChange(event, this);
+        return false;
       });
     }
+
+    // Add Markup provided by plugin instance.
+    self.settings.addMarkup(self);
+    self.settings.init(self);
+
+    return this;
   };
 
 })(jQuery, Drupal, drupalSettings);
--- js/hb-plugins/hb-location-finder.js
+++ js/hb-plugins/hb-location-finder.js
@@ -34,57 +34,67 @@
     attach: (context) => {
       // TODO: get selector from settings.
       let locationsList = $('.field-prgf-location-finder .locations-list .views-row__wrapper', context);
-      locationsList.find('.views-row .node--type-branch').each(function (index) {
-        let id = $(this).attr('data-hb-id');
-        let el = $(this);
-        // Add sort attribute with default index for correct order on change.
-        el.closest('.views-row').attr('data-hb-sort', index);
-        el.closest('.views-row').attr('data-hb-sort-origin', index);
-        // Attach plugin instance to each location teaser.
-        // @see openy_home_branch/js/hb-plugin-base.js
-        $(this).hbPlugin({
-          selector: '.hb-location-checkbox',
-          event: 'change',
-          element: null,
-          init: function () {
-            if (!this.element) {
-              return;
-            }
-            let selected = Drupal.homeBranch.getValue('id');
-            let branchEl = this.element.closest('.node--type-branch.node--view-mode-teaser');
-            if ($(this.element).val() === selected) {
-              this.element.prop('checked', true);
-              branchEl.addClass('hb-selected');
-              branchEl.find('.hb-location-checkbox-wrapper label').text('My Home Branch');
-              branchEl.closest('.views-row').attr('data-hb-sort', -1);
-            }
-            else {
-              this.element.prop('checked', false);
-              branchEl.removeClass('hb-selected');
-              branchEl.find('.hb-location-checkbox-wrapper label').text('Set as my Home Branch');
-              branchEl.closest('.views-row').attr('data-hb-sort', branchEl.closest('.views-row').attr('data-hb-sort-origin'));
-            }
-          },
-          onChange: function (event, el) {
-            // Save selected value in сookies storage.
-            let id = ($(el).is(':checked')) ? $(el).val() : null;
-            Drupal.homeBranch.setId(id);
-            // Run sort after plugin instances re-init.
-            $(document).delay(300).trigger('hb-location-finder-sort', locationsList);
-          },
-          addMarkup: function (context) {
-            el.append(`
-            <div class="hb-location-checkbox-wrapper">
-              <input type="checkbox" value="` + id + `" id="hb-location-checkbox-` + id + `" class="hb-location-checkbox">
-              <label>Set as my Home Branch</label>
-            </div>
-            `);
-            // Save created element in plugin.
-            this.element = $('#hb-location-checkbox-' + id);
-          },
-        });
+      locationsList.find('.views-row .node--type-branch')
+        .each(function (index) {
+          let el = $(this);
+          // Add sort attribute with default index for correct order on change.
+          el.closest('.views-row').attr('data-hb-sort', index);
+          el.closest('.views-row').attr('data-hb-sort-origin', index);
+          //el.closest('.views-row').find('.location-item--title').prepend(index);
+        })
+        .each(function() {
+          // Attach plugin instance to each location teaser.
+          // @see openy_home_branch/js/hb-plugin-base.js
+          $(this).hbPlugin({
+            selector: '.hb-location-checkbox',
+            event: 'change',
+            element: null,
+            branchTeaserSelector: '.node--type-branch.node--view-mode-teaser',
+            selectedText: 'My Home Branch',
+            notSelectedText: 'Set as my Home Branch',
+            init: function () {
+              if (!this.element) {
+                return;
+              }
+              let selected = Drupal.homeBranch.getValue('id');
+              let branchEl = this.element.closest(this.branchTeaserSelector);
+              if ($(this.element).val() === selected) {
+                this.element.prop('checked', true);
+                branchEl.addClass('hb-selected');
+                branchEl.find('.hb-location-checkbox-wrapper label').text(this.selectedText);
+                branchEl.closest('.views-row').attr('data-hb-sort', -1);
+              }
+              else {
+                this.element.prop('checked', false);
+                branchEl.removeClass('hb-selected');
+                branchEl.find('.hb-location-checkbox-wrapper label').text(this.notSelectedText);
+                branchEl.closest('.views-row').attr('data-hb-sort', branchEl.closest('.views-row').attr('data-hb-sort-origin'));
+              }
+            },
+            onChange: function (event, el) {
+              if ($(el).attr('id') == this.element.attr('id')) {
+                // Save selected value in сookies storage.
+                let id = ($(el).is(':checked')) ? $(el).val() : null;
+                Drupal.homeBranch.setId(id);
+                // Run sort after plugin instances re-init.
+                // $(document).delay(300).trigger('hb-location-finder-sort', locationsList);
+              }
+            },
+            addMarkup: function (context) {
+              let id = context.attr('data-hb-id');
+              let $markup = $(`
+              <div class="hb-location-checkbox-wrapper">
+                <input type="checkbox" value="` + id + `" id="hb-location-checkbox-` + id + `" class="hb-location-checkbox">
+                <label for="hb-location-checkbox-` + id + `">Set as my Home Branch</label>
+              </div>
+              `);
+              $markup.appendTo(context);
+              // Save created element in plugin.
+              this.element = $markup.find('input');
+            },
+          });
 
-      });
+        });
 
       // Trigger sort after plugin attach.
       $(document).trigger('hb-location-finder-sort', locationsList);
