1686c12436a3d5227d41bd2af0dfd6cab13bcb51
andrey <savagge@gmail.com>
2019-08-09 13:48:20 +0300

    [YGBWHB-17] Handle ampersands in Branch names

--- modules/custom/openy_home_branch/js/hb-plugins/hb-pef-location.js
+++ modules/custom/openy_home_branch/js/hb-plugins/hb-pef-location.js
@@ -10,7 +10,7 @@
    */
   Drupal.homeBranch.plugins.push({
     name: 'hb-pef-location',
-    attach: (settings) => {
+    attach: function (settings) {
       // Attach plugin instance to header menu item.
       // @see openy_home_branch/js/hb-plugin-base.js
       $('.hb-menu-selector').hbPlugin(settings);
@@ -22,19 +22,19 @@
       event: null,
       element: null,
       init: function () {
-        let selected = Drupal.homeBranch.getValue('id');
+        var selected = Drupal.homeBranch.getValue('id');
         if (!selected) {
           return;
         }
-        let locations = Drupal.homeBranch.getLocations();
+        var locations = Drupal.homeBranch.getLocations();
         // Get url from paragraph's field.
-        let url = $(this.linkSelector).attr('href');
-        location.href = url + '/?locations=' + encodeURIComponent(locations[selected]);
+        var url = $(this.linkSelector).attr('href');
+        location.href = url + '/?locations=' + encodeURIComponent(locations[selected].replace('&', '%26'));
 
         // That's just for the visual effect.
         $(this.inputSelector).find('input[value="' + locations[selected] + '"]').click();
       }
-    },
+    }
   });
 
 })(jQuery, Drupal, drupalSettings);
