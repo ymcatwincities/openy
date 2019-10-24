2034da329ddb6755c5b9e8168d3c17b7cc851492
Dmitry <dmitry.chubar@fivejars.com>
2019-10-24 14:39:13 +0300

    AF: Add search index config for SOLR backend.

--- modules/custom/openy_activity_finder/src/Form/SettingsForm.php
+++ modules/custom/openy_activity_finder/src/Form/SettingsForm.php
@@ -2,6 +2,9 @@
 
 namespace Drupal\openy_activity_finder\Form;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Cache\CacheBackendInterface;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
@@ -14,6 +17,20 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * Settings Form for daxko.
  */
 class SettingsForm extends ConfigFormBase {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The module handler.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
 
   /**
    * Guzzle Http Client.
@@ -30,12 +47,12 @@ class SettingsForm extends ConfigFormBase {
   protected $cache;
 
   /**
-   * Constructs a new Class.
-   *
-   * @param \GuzzleHttp\Client $http_client
-   *   The http_client.
+   * {@inheritdoc}
    */
-  public function __construct(Client $http_client, CacheBackendInterface $cache) {
+  public function __construct(ConfigFactoryInterface $config_factory, ModuleHandlerInterface $module_handler, EntityTypeManagerInterface $entity_type_manager, Client $http_client, CacheBackendInterface $cache) {
+    parent::__construct($config_factory);
+    $this->moduleHandler = $module_handler;
+    $this->entityTypeManager = $entity_type_manager;
     $this->httpClient = $http_client;
     $this->cache = $cache;
   }
@@ -45,6 +62,9 @@ class SettingsForm extends ConfigFormBase {
    */
   public static function create(ContainerInterface $container) {
     return new static(
+      $container->get('config.factory'),
+      $container->get('module_handler'),
+      $container->get('entity_type.manager'),
       $container->get('http_client'),
       $container->get('cache.render')
     );
@@ -78,9 +98,8 @@ class SettingsForm extends ConfigFormBase {
       'openy_activity_finder.solr_backend' => 'Solr Backend (local db)',
     ];
 
-    $moduleHandler = \Drupal::service('module_handler');
-    if ($moduleHandler->moduleExists('openy_daxko2')) {
-      $backend_options['openy_daxko2.openy_activity_finder_backend'] = 'Daxko 2 (live API calls)';
+    if ($this->moduleHandler->moduleExists('openy_daxko2')){
+      $backend_options['openy_daxko2.openy_activity_finder_backend'] = $this->t('Daxko 2 (live API calls)');
     }
 
     $form['backend'] = [
@@ -89,14 +108,40 @@ class SettingsForm extends ConfigFormBase {
       '#required' => TRUE,
       '#title' => $this->t('Backend for Activity Finder'),
       '#default_value' => $config->get('backend'),
-      '#description' => t(''),
+      '#description' => $this->t('Search API backend for Activity Finder'),
+    ];
+
+    $search_api_indexes = $this->entityTypeManager
+      ->getStorage('search_api_index')->loadByProperties();
+
+    $indexes = [];
+    if ($search_api_indexes) {
+      foreach ($search_api_indexes as $index) {
+        $indexes[$index->id()] = $index->get('name');
+      }
+    }
+
+    $form['index'] = [
+      '#type' => 'select',
+      '#options' => $indexes,
+      '#title' => $this->t('Search API index'),
+      '#default_value' => $config->get('index'),
+      '#description' => $this->t('Search API Index to use for SOLR backend.'),
+      '#states' => [
+        'visible' => [
+          ':input[name="backend"]' => ['value' => 'openy_activity_finder.solr_backend'],
+        ],
+        'required' => [
+          ':input[name="backend"]' => ['value' => 'openy_activity_finder.solr_backend'],
+        ],
+      ],
     ];
 
     $form['ages'] = [
       '#type' => 'textarea',
       '#title' => $this->t('Ages'),
       '#default_value' => $config->get('ages'),
-      '#description' => t('Ages mapping. One per line. "<number of months>,<age display label>". Example: "660,55+"'),
+      '#description' => $this->t('Ages mapping. One per line. "<number of months>,<age display label>". Example: "660,55+"'),
     ];
 
 
@@ -104,21 +149,21 @@ class SettingsForm extends ConfigFormBase {
       '#type' => 'textfield',
       '#title' => $this->t('Exclude category -- so we do not display Group Exercises'),
       '#default_value' => $config->get('exclude'),
-      '#description' => t('Provide ID of the Program Subcategory to exclude. You do not need to provide this if you use Daxko. Needed only for Solr backend.'),
+      '#description' => $this->t('Provide ID of the Program Subcategory to exclude. You do not need to provide this if you use Daxko. Needed only for Solr backend.'),
     ];
 
     $form['disable_search_box'] = [
       '#type' => 'checkbox',
       '#title' => $this->t('Disable Search Box'),
       '#default_value' => $config->get('disable_search_box'),
-      '#description' => t('When checked hides search text box (both for Activity Finder and Results page).'),
+      '#description' => $this->t('When checked hides search text box (both for Activity Finder and Results page).'),
     ];
 
     $form['disable_spots_available'] = [
       '#type' => 'checkbox',
       '#title' => $this->t('Disable Spots Available'),
       '#default_value' => $config->get('disable_spots_available'),
-      '#description' => t('When checked disables Spots Available feature on Results page.'),
+      '#description' => $this->t('When checked disables Spots Available feature on Results page.'),
     ];
 
     $form['hb_modal'] = [
@@ -221,6 +266,7 @@ class SettingsForm extends ConfigFormBase {
     $config = $this->config('openy_activity_finder.settings');
 
     $config->set('backend', $form_state->getValue('backend'))->save();
+    $config->set('index', $form_state->getValue('index'))->save();
     $config->set('ages', $form_state->getValue('ages'))->save();
     $config->set('exclude', $form_state->getValue('exclude'))->save();
     $config->set('disable_search_box', $form_state->getValue('disable_search_box'))->save();
