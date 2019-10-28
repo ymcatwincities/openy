070899763b2238a51dbd31c5f6f21edd584284dd
Vitalii Stepaniuk <vitalii.stepaniuk@gmail.com>
2019-10-28 18:13:35 +0200

    Temporarily frozen, for another experiment.

---
+++ themes/openy_themes/openy_carnation/webpack-entry.js
@@ -0,0 +1,5 @@
+import './src/scss/style.scss'
+import './src/js/**/*.js'
+
+// let files = glob.sync(path.resolve(__dirname) + './src/js/**/*.js')
+// this.logger.log(files)
--- themes/openy_themes/openy_carnation/webpack.config.js
+++ themes/openy_themes/openy_carnation/webpack.config.js
@@ -2,9 +2,14 @@ const SassLintPlugin = require('sass-lint-webpack')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const path = require('path')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+var entry = require('webpack-glob-entry')
+
+let smth = path.resolve(__dirname, './src/js/bootstrap.js')
+console.log(smth)
 
 module.exports = {
-    entry: './src/scss/style.scss',
+    //entry: entry('./src/scss/style.scss', './src/js/**/*.js'),
+    entry: './webpack-entry.js',
     plugins: [
         ///...
         new SassLintPlugin({
@@ -32,6 +37,35 @@ module.exports = {
 
     module: {
         rules: [
+            {
+                test: /.js/,
+                enforce: 'pre',
+                exclude: [
+                    /node_modules/,
+                    /webpack-entry.js/,
+                    /.\/src\/js\/bootstrap.js/,
+                    /jquery.matchHeight.js/,
+                    path.resolve(__dirname, './src/js/bootstrap.js'),
+                ],
+                use: [
+                    {
+                        loader: 'jshint-loader',
+                        options: {
+                            sourceMap: false,
+                            curly: true,
+                            eqeqeq: true,
+                            eqnull: true,
+                            browser: true,
+                            globals: {
+                                jQuery: true
+                            },
+                            ignores: [
+                                path.resolve(__dirname, './src/js/bootstrap.js'),
+                            ],
+                        }
+                    }
+                ]
+            },
             {
                 test: /\.s[ac]ss$/i,
                 use: [
