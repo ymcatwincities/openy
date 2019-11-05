0e6669d57048122dbbbb1b30c66db90aa2c1756c
redfox <redfoxxx.ua@gmail.com>
2019-11-05 13:53:02 +0200

    [OS-551] Convert front-end asset compiling engine down to a small NPM script

--- themes/openy_themes/openy_carnation/webpack-entry.js
+++ themes/openy_themes/openy_carnation/webpack-entry.js
@@ -1,6 +1,3 @@
 import './src/scss/style.scss'
-//import './src/js/**/*.js'
 
-require.context('./src/js/', true, /\.(js)$/im);
-// let files = glob.sync(path.resolve(__dirname) + './src/js/**/*.js')
-// this.logger.log(files)
+//require.context('./src/js/', true, /\.(js)$/im);
