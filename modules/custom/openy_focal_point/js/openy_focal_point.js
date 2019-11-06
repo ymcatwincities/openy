c50abf9f5d9ed1f0b8127fbcee8dd162dd46c9bf
redfox <redfoxxx.ua@gmail.com>
2019-11-06 12:35:08 +0200

    [OS-547] Update OpenY Focal Point related configs

--- modules/custom/openy_focal_point/js/openy_focal_point.js
+++ modules/custom/openy_focal_point/js/openy_focal_point.js
@@ -8,7 +8,6 @@
 
   Drupal.AjaxCommands.prototype.rerenderThumbnail = function (ajax, response, status) {
     var $image = $(response.selector);
-    console.log(response.selector, 'rerenderThumbnail');
     if ($image.length) {
       var src = $image.attr('src') + '1';
       $image.attr('src', src);
