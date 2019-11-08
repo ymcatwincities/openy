637123d7442793f444a84f029b3f94a99580b490
redfox <redfoxxx.ua@gmail.com>
2019-11-08 20:09:44 +0200

    Carnation refactoring: make css extract/map different to dev and build

--- themes/openy_themes/openy_carnation/webpack.common.js
+++ themes/openy_themes/openy_carnation/webpack.common.js
@@ -22,39 +22,5 @@ module.exports = {
   devtool: 'source-map',
   output: {
     sourceMapFilename: 'css/style.css.map',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: [
-          /node_modules/,
-          /jshint-loader/,
-          /webpack-entry.js/,
-        ],
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              url: false,
-              sourceMap: true,
-            }
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true,
-            }
-          },
-        ]
-      },
-    ]
   }
 };
