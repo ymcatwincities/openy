52d69873b69ad6823a3066b7ef5bfa761a8d4d0e
Oleksandr Kuksenko <oleksandrk@imagexmedia.com>
2019-12-02 17:02:45 +0200

    egym iwatch module whith banner_fixed paragraph

---
+++ modules/openy_features/openy_egym_iwatch/dist/main.js
@@ -0,0 +1,19 @@
+!function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}([
+/*!**************************!*\
+  !*** ./webpack-entry.js ***!
+  \**************************/
+/*! no exports provided */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is an entry point */function(e,t,n){"use strict";n.r(t);n(/*! ./src/scss/openy_egym_iwatch.scss */1);n(2)},
+/*!*****************************************!*\
+  !*** ./src/scss/openy_egym_iwatch.scss ***!
+  \*****************************************/
+/*! no static exports found */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,t,n){},
+/*!*******************************!*\
+  !*** ./src/js sync \.(js)$/i ***!
+  \*******************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,t){function n(e){var t=new Error("Cannot find module '"+e+"'");throw t.code="MODULE_NOT_FOUND",t}n.keys=function(){return[]},n.resolve=n,e.exports=n,n.id=2}]);
+//# sourceMappingURL=css/openy_egym_iwatch.css.map
---
+++ modules/openy_features/openy_egym_iwatch/webpack-entry.js
@@ -0,0 +1,2 @@
+import './src/scss/openy_egym_iwatch.scss';
+require.context('./src/js/', true, /\.(js)$/im);
