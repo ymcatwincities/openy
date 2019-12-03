e23e2861f2a5d6ca709c3385f33ec6ec18c39c7b
Oleksandr Kuksenko <oleksandrk@imagexmedia.com>
2019-12-03 11:41:05 +0200

    OS-643 replaced {theme_name} logic from rose html.html.twig with static "rose_based_theme" class

--- modules/openy_features/openy_egym_iwatch/dist/main.js
+++ modules/openy_features/openy_egym_iwatch/dist/main.js
@@ -1,19 +1,13 @@
-!function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}([
+!function(e){var t={};function r(n){if(t[n])return t[n].exports;var o=t[n]={i:n,l:!1,exports:{}};return e[n].call(o.exports,o,o.exports,r),o.l=!0,o.exports}r.m=e,r.c=t,r.d=function(e,t,n){r.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:n})},r.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},r.t=function(e,t){if(1&t&&(e=r(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var n=Object.create(null);if(r.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)r.d(n,o,function(t){return e[t]}.bind(null,o));return n},r.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return r.d(t,"a",t),t},r.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},r.p="",r(r.s=0)}([
 /*!**************************!*\
   !*** ./webpack-entry.js ***!
   \**************************/
 /*! no exports provided */
 /*! all exports used */
-/*! ModuleConcatenation bailout: Module is an entry point */function(e,t,n){"use strict";n.r(t);n(/*! ./src/scss/openy_egym_iwatch.scss */1);n(2)},
+/*! ModuleConcatenation bailout: Module is an entry point */function(e,t,r){"use strict";r.r(t);r(/*! ./src/scss/openy_egym_iwatch.scss */1)},
 /*!*****************************************!*\
   !*** ./src/scss/openy_egym_iwatch.scss ***!
   \*****************************************/
 /*! no static exports found */
-/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,t,n){},
-/*!*******************************!*\
-  !*** ./src/js sync \.(js)$/i ***!
-  \*******************************/
-/*! no static exports found */
-/*! all exports used */
-/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,t){function n(e){var t=new Error("Cannot find module '"+e+"'");throw t.code="MODULE_NOT_FOUND",t}n.keys=function(){return[]},n.resolve=n,e.exports=n,n.id=2}]);
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,t,r){}]);
 //# sourceMappingURL=css/openy_egym_iwatch.css.map
--- modules/openy_features/openy_egym_iwatch/webpack-entry.js
+++ modules/openy_features/openy_egym_iwatch/webpack-entry.js
@@ -1,2 +1,1 @@
 import './src/scss/openy_egym_iwatch.scss';
-require.context('./src/js/', true, /\.(js)$/im);
