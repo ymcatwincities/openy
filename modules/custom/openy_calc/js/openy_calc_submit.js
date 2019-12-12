02ebf0b73a0a2fb0127d9c0c6f8c6c0f71623d15
Oleksandr Kuksenko <oleksandrk@imagexmedia.com>
2019-12-12 17:11:08 +0200

    replaced setTimeout with better solution

--- modules/custom/openy_calc/js/openy_calc_submit.js
+++ modules/custom/openy_calc/js/openy_calc_submit.js
@@ -21,9 +21,15 @@
    */
   Drupal.behaviors.openy_calc_focus = {
     attach: function (context, settings) {
-      setTimeout(function() {
+      //After ajax repsonse rendered input[name="step-3"] button is focused, we need to wait for that.
+      var nextButton = $('input[name="step-3"]');
+      if(nextButton.length) {
+        nextButton.on('focus', function () {
+          $('a[role="tab"][aria-selected=true]').focus();
+        });
+      } else {
         $('a[role="tab"][aria-selected=true]').focus();
-      }, 1000);
+      }
     }
   };
 
