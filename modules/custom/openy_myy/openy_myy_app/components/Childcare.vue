15d4963b1d1da2655d8e7a840afcd5c421b5b8df
redfox <redfoxxx.ua@gmail.com>
2020-01-22 13:54:39 +0200

    MyY: Add item at childcare

--- modules/custom/openy_myy/openy_myy_app/components/Childcare.vue
+++ modules/custom/openy_myy/openy_myy_app/components/Childcare.vue
@@ -53,7 +53,8 @@
           </div>
           <div class="event_add_item row">
             <div class="col-myy-sm-12">
-              <i class="fa blue fa-calendar-plus-o"></i> <a href="#"><strong>Add Item</strong></a>
+              <i class="fa blue fa-calendar-plus-o"></i>
+              <a class="myy-modal__modal--myy-additem-link additem" role="button" href="#" v-on:click="populatePopupAdditem" data-toggle="modal" data-target=".myy-modal__modal--myy-additem"><strong>Add Item</strong></a>
             </div>
           </div>
         </div>
@@ -104,7 +105,7 @@
               </div>
               <div class="col-myy-sm-12 actions">
                 <button type="button" class="btn btn-primary close-action" data-dismiss="modal" aria-label="Close">NO, KEEP IT</button>
-                <button type="button" class="btn btn-primary text-uppercase cancel-action" data-dismiss="modal" @click="cancel(cancelPopup.content.order_number)">YES, CANCEL</button>
+                <button type="button" class="btn btn-primary text-uppercase cancel-action" data-dismiss="modal" @click="cancel(cancelPopup.content.order_number, cancelPopup.content.date)">YES, CANCEL</button>
               </div>
             </div>
           </div>
@@ -113,10 +114,57 @@
       </div>
     </div>
 
+    <!--modal > add item -->
+    <div class="modal fade myy-modal__modal myy-modal__modal--myy-additem" tabindex="-1" role="dialog" aria-labelledby="myy-modal__modal--myy-additem">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+
+          <div class="myy-modal__modal--header">
+            <h3>Add Item</h3>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><i class="fa fa-times" aria-hidden="true"></i></button>
+          </div>
+
+          <div v-if="addItemPopup.content.length > 0" class="myy-modal__modal--body">
+            <div class="col-myy-sm-12">
+              <div class="event_row">
+                <div class="event_head row">
+                  <div class="col-myy-sm-2">
+                    <span class="rounded_letter small blue">J</span>
+                  </div>
+                  <div class="col-myy-sm-10">
+                    <div class="program_name" v-if="addItemPopup.content.length > 0"><strong>{{ addItemPopup.content[0].program_name }}</strong></div>
+                    {{ addItemPopup.content[0].branch_id }}
+                    <div class="date"><strong>Dec 12-Dec 19</strong></div>
+                    <span class="weekdays">Mon - Fri</span>
+                  </div>
+                </div>
+                <div v-for="(item, index) in addItemPopup.content" v-bind:key="index" class="event_item row">
+                  <div class="col-myy-sm-2 no-padding-left text-center"><!--v-model="checked"--> <!--:value="getOption(checkbox)"-->
+                    <input type="checkbox" v-bind:checked="item.scheduled == 'Y'" v-model="addItemChecked" :value="item.order_number + '_' + item.date + '+' + item.type" :id="'checkbox-' + item.order_number + '_' + item.date + '+' + item.type" />
+                  </div>
+                  <div class="col-myy-sm-10 no-padding-left">
+                    <div class="date">{{ item.usr_day }}, {{ item.order_date }}</div>
+                    <div class="duration">{{ item.type }}</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-myy-sm-12 actions">
+              <button type="button" class="btn btn-primary close-action" data-dismiss="modal" aria-label="Close">Cancel</button>
+              <button type="button" class="btn btn-primary text-uppercase additem-action" :disabled="addItemPopup.disableScheduleButton" data-dismiss="modal" @click="scheduleItems()">Schedule</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
   </section>
 </template>
 
 <script>
+  import split from "../../../../../../../../libraries/vue/test/ssr/fixtures/split";
+
   export default {
     data () {
       return {
@@ -129,6 +177,11 @@
           denyCancel: false,
           content: {}
         },
+        addItemPopup: {
+          content: {},
+          disableScheduleButton:false
+        },
+        addItemChecked: [],
       }
     },
     methods: {
@@ -161,6 +214,44 @@
           component.loading = false;
         });
       },
+      scheduleItems: function() {
+        let component = this;
+
+        var newScheduled = {};
+        for (var i in component.data) {
+          newScheduled[component.data[i].order_number + '_' + component.data[i].date + '+' + component.data[i].type] = 'N';
+        }
+        for (var j in newScheduled) {
+          for (var k in component.addItemChecked) {
+            if (component.addItemChecked[k] == j) {
+              newScheduled[j] = 'Y';
+            }
+          }
+        }
+        var dataToSend = {};
+        for (var l in newScheduled) {
+          var id = l.split('_')[0];
+          if (typeof dataToSend[id] == 'undefined') {
+            dataToSend[id] = '';
+          }
+          dataToSend[id] = dataToSend[id] + ',' + l.split('_')[1] + '+' + newScheduled[l];
+        }
+        for (var order_id in dataToSend) {
+          let url = component.baseUrl + 'myy-model/childcare/additem/' + order_id + '}/' + dataToSend[order_id];
+
+          component.loading = true;
+          jQuery.ajax({
+            url: url,
+            xhrFields: {
+              withCredentials: true
+            }
+          }).done(function(data) {
+          });
+        }
+        // @todo: Reload main content with runAjaxRequest??
+        component.loading = false;
+        component.runAjaxRequest();
+      },
       populatePopupCancel: function(index) {
         // Check if there only 1 item so user doesn't allow to cancel it.
         if (this.data.length == 1) {
@@ -168,6 +259,26 @@
         }
         this.cancelPopup.content = this.data[index];
       },
+      populatePopupAdditem: function() {
+        if (this.addItemChecked.length === 0) {
+          for (var i in this.data) {
+            if (this.data[i].scheduled == 'Y') {
+              this.addItemChecked.push(this.data[i].order_number + '_' + this.data[i].date + '+' +this.data[i].type);
+            }
+          }
+        }
+        this.addItemPopup.content = this.data;
+      },
+    },
+    watch: {
+      addItemChecked: function(newValue, oldValue) {
+        if (newValue.length === 0) {
+          this.addItemPopup.disableScheduleButton = true;
+        }
+        else {
+          this.addItemPopup.disableScheduleButton = false;
+        }
+      }
     },
     mounted: function() {
       let component = this;
@@ -319,5 +430,63 @@
       }
     }
   }
+  .myy-modal__modal--myy-additem {
+    .modal-dialog {
+      max-width: 340px;
+    }
+    .col-myy-sm-12 {
+      padding-left: 10px;
+      padding-right: 10px;
+    }
+    .event_row {
+      .event_head {
+        padding: 10px 0;
+        line-height: 18px;
+        margin-right: -10px;
+        margin-left: -10px;
+      }
+      .event_item {
+        padding: 10px 0;
+        margin: 10px 0;
+        &:last-child {
+          border-bottom: none;
+        }
+        .date {
+          margin-left: 0;
+        }
+      }
+    }
+    .actions {
+      background-color: #f2f2f2;
+      margin-top: 15px;
+      padding-top: 15px;
+      padding-bottom: 15px;
+      .close-action {
+        border: 2px solid #0060AF;
+        border-radius: 4px;
+        color: #0060AF;
+        font-family: "Cachet Medium", sans-serif;
+        font-size: 18px;
+        height: 50px;
+        line-height: 50px;
+        padding: 0 20px;
+        text-transform: uppercase;
+      }
+      .additem-action {
+        background-color: #92278F;
+        border: 2px solid #92278F;
+        border-radius: 4px;
+        color: #FFF;
+        float: right;
+        font-family: "Cachet Medium", sans-serif;
+        font-size: 18px;
+        height: 50px;
+        line-height: 50px;
+        padding: 0 20px;
+        text-transform: uppercase;
+      }
+    }
+  }
+
 </style>
 
