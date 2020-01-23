110e3192e8a8cb3c2ad2af40b8dacd2697be5232
Oleksandr Kuksenko <oleksandrk@imagexmedia.com>
2020-01-23 18:56:06 +0200

    OS-681 google translate link duplicate

--- modules/custom/openy_gtranslate/js/openy_gtranslate.js
+++ modules/custom/openy_gtranslate/js/openy_gtranslate.js
@@ -1,46 +1,18 @@
 /**
  * @file openy_gtranslate.js
  */
-(function ($, Drupal, drupalSettings) {
-
-    "use strict";
-
-    Drupal.behaviors.openy_gtranslate = {
-        attach: function (context, settings) {
-            setTimeout(function () {
-                var langSelect = $('.goog-te-menu-frame').first();
-
-                $('nav .navbar-nav li.language > a').on('click', function (e, context) {
-                    e.preventDefault();
-                    langSelect.show();
-                    langSelect.addClass('open');
-                    return false;
-                });
-
-                $('body').on('click', function (e, context) {
-                    if (langSelect.hasClass('open')) {
-                        langSelect.hide();
-                        langSelect.removeClass('open');
-                    }
-                });
-            }, 100);
-        }
-    };
-
-    Drupal.behaviors.openy_gtranslate_mobile = {
-        attach: function (context, settings) {
-          var el = $('#google_translate_element');
-
-          // Emulate mobile width.
-          if ($(window).width() < 768) {
-            setTimeout(function() {
-              $(".navbar-toggler, .navbar-toggle").on('click', function () {
-                $('.navbar-nav > li.language a').hide();
-                el.removeClass('hidden-xs').appendTo('.navbar-nav > li.language');
-              });
-            }, 100);
-          }
-        }
-    };
-
-}(jQuery, Drupal, drupalSettings));
+function googleTranslateElementInit() {
+  var elem = jQuery('.openy-google-translate:visible').get(0);
+  if (elem === undefined) {
+    console.log('Placeholder for google translate widget not found.');
+  }
+  else {
+    new google.translate.TranslateElement(
+      {
+        pageLanguage: drupalSettings.path.currentLanguage,
+        layout: google.translate.TranslateElement.InlineLayout.VERTICAL,
+      },
+      elem
+    );
+  }
+}
