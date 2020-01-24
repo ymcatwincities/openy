5dbd211d5ac2d6012f892d863b7c5e976d51b614
Oleksandr Kuksenko <oleksandrk@imagexmedia.com>
2020-01-24 15:12:28 +0200

    OS-681 fix function scope for openy_gtranslate.js

--- modules/custom/openy_gtranslate/js/openy_gtranslate.js
+++ modules/custom/openy_gtranslate/js/openy_gtranslate.js
@@ -1,18 +1,20 @@
 /**
  * @file openy_gtranslate.js
  */
-function googleTranslateElementInit() {
-  var elem = jQuery('.openy-google-translate:visible').get(0);
-  if (elem === undefined) {
-    console.log('Placeholder for google translate widget not found.');
-  }
-  else {
-    new google.translate.TranslateElement(
-      {
-        pageLanguage: drupalSettings.path.currentLanguage,
-        layout: google.translate.TranslateElement.InlineLayout.VERTICAL,
-      },
-      elem
-    );
-  }
-}
+(function ($, Drupal, drupalSettings) {
+  Drupal.googleTranslateElementInit = function() {
+    var elem = $('.openy-google-translate:visible').get(0);
+    if (elem === undefined) {
+      console.log('Placeholder for google translate widget not found.');
+    }
+    else {
+      new google.translate.TranslateElement(
+        {
+          pageLanguage: drupalSettings.path.currentLanguage,
+          layout: google.translate.TranslateElement.InlineLayout.VERTICAL,
+        },
+        elem
+      );
+    }
+  };
+}(jQuery, Drupal, drupalSettings));
