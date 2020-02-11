1454e4befa5e37a643aabcac391f7d8bbb1ca1aa
Alexandr Prykhodko <alexandr.prykhodko@fivejars.com>
2020-02-11 17:17:20 +0200

    [YGSSLA-135]: Add address for locations to SchedulesLocBlock

--- modules/custom/openy_repeat/src/Plugin/Block/RepeatSchedulesLocBlock.php
+++ modules/custom/openy_repeat/src/Plugin/Block/RepeatSchedulesLocBlock.php
@@ -19,14 +19,19 @@ class RepeatSchedulesLocBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
-    $sql = "SELECT DISTINCT nd.title as location 
+    $sql = "SELECT DISTINCT nd.title as location,
+            fla.field_location_address_address_line1 as address,
+            fla.field_location_address_locality as locality
             FROM {node} n
             INNER JOIN node__field_session_location l ON n.nid = l.entity_id AND l.bundle = 'session'
+            LEFT JOIN node__field_location_address fla ON fla.entity_id = l.field_session_location_target_id
             INNER JOIN node_field_data nd ON l.field_session_location_target_id = nd.nid
-            WHERE n.type = 'session'";
+            WHERE n.type = 'session'
+            ORDER BY location";
+
     $connection = \Drupal::database();
     $query = $connection->query($sql);
-    $locations = $query->fetchCol();
+    $locations = $query->fetchAll();
 
     return [
       '#theme' => 'openy_repeat_schedule_locations',
