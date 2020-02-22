cf9b5a70baf1773001633f03b5366679312767ea
Andriy Prokopenko <andprk@gmail.com>
2020-02-22 13:49:38 +0200

    [OYS-79] Move code to openy_ckeditor module

--- themes/openy_themes/openy_rose/scripts/openy_rose.js
+++ themes/openy_themes/openy_rose/scripts/openy_rose.js
@@ -1,6 +1,6 @@
 /**
  * @file
- * OpenY Rose JavaScript logic.
+ * Open Rose JS.
  */
 
 (function ($) {
@@ -12,7 +12,6 @@
         active: false,
         collapsible: true
       });
-
     }
   };
 
@@ -257,20 +256,6 @@
         }
         else {
           $(this).attr('aria-expanded', 'false');
-        }
-      });
-    }
-  };
-
-  /**
-   * Fill ckeditor table cell padding with value for cellpadding.
-   */
-  Drupal.behaviors.ckeditorTablePadding = {
-    attach: function (context, settings) {
-      $("table", context).each(function () {
-        var padding = $(this).attr("cellpadding");
-        if (padding !== 0) {
-          $(this).find("td").css("padding", padding);
         }
       });
     }
