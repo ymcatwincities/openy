929bfe473815186680a54b571b6af8a12dafa156
Andrii Tiupa <andrey@imagexmedia.com>
2020-03-24 15:09:32 +0200

    Camp Finder: Backport from YGBW (extend for Activity Finder)

--- modules/custom/openy_activity_finder/src/Plugin/Block/ActivityFinderBlock.php
+++ modules/custom/openy_activity_finder/src/Plugin/Block/ActivityFinderBlock.php
@@ -4,7 +4,15 @@ namespace Drupal\openy_activity_finder\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Cache\Cache;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\Query\QueryFactory;
+use Drupal\Core\Path\AliasManagerInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\openy_activity_finder\OpenyActivityFinderSolrBackend;
+use Drupal\node\Entity\Node;
+use Drupal\node\NodeInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a 'Activity Finder' block.
@@ -15,19 +23,104 @@ use Drupal\openy_activity_finder\OpenyActivityFinderSolrBackend;
  *   category = @Translation("Paragraph Blocks")
  * )
  */
-class ActivityFinderBlock extends BlockBase {
+class ActivityFinderBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The configuration factory.
+   *
+   * @var ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * The entity query factory.
+   *
+   * @var QueryFactory
+   */
+  protected $entityQuery;
+
+  /**
+   * The alias manager that caches alias lookups based on the request.
+   *
+   * @var AliasManagerInterface
+   */
+  protected $aliasManager;
+
+  /**
+   * The route match.
+   *
+   * @var RouteMatchInterface
+   */
+  protected $routeMatch;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    ConfigFactoryInterface $config_factory,
+    QueryFactory $entity_query,
+    AliasManagerInterface $alias_manager,
+    RouteMatchInterface $route_match
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->configFactory = $config_factory;
+    $this->entityQuery = $entity_query;
+    $this->aliasManager = $alias_manager;
+    $this->routeMatch = $route_match;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('config.factory'),
+      $container->get('entity.query'),
+      $container->get('path.alias_manager'),
+      $container->get('current_route_match')
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
-    $config = \Drupal::service('config.factory')->get('openy_activity_finder.settings');
+    $config = $this->configFactory->get('openy_activity_finder.settings');
     $backend_service_id = $config->get('backend');
     $backend = \Drupal::service($backend_service_id);
-    $node = \Drupal::routeMatch()->getParameter('node');
+    $node = $this->routeMatch->getParameter('node');
     $alias = '';
     if ($node instanceof NodeInterface) {
-      $alias = \Drupal::service('path.alias_manager')->getAliasByPath('/node/' . $node->id());
+      $alias = $this->aliasManager->getAliasByPath('/node/' . $node->id());
+    }
+
+    $locationsMapping = [];
+    if ($backend_service_id == 'openy_daxko2.openy_activity_finder_backend') {
+      $openy_daxko2_config = $this->configFactory->get('openy_daxko2.settings');
+      if (!empty($openy_daxko2_config->get('locations'))) {
+        $nids = $this->entityQuery
+          ->get('node')
+          ->condition('type', ['branch', 'camp', 'facility'], 'IN')
+          ->condition('status', 1)
+          ->sort('title', 'ASC')
+          ->execute();
+        $locations = Node::loadMultiple($nids);
+        $config_rows = explode("\n", $openy_daxko2_config->get('locations'));
+        foreach ($config_rows as $row) {
+          $line = explode(', ', $row);
+          foreach ($locations as $nid => $location) {
+            if (isset($line[1]) && $line[1] == $location->getTitle()) {
+              $locationsMapping[$nid] = $line[0];
+            }
+          }
+        }
+      }
     }
 
     return [
@@ -39,11 +132,13 @@ class ActivityFinderBlock extends BlockBase {
       '#categories_type' => $backend->getCategoriesType(),
       '#activities' => $backend->getCategories(),
       '#locations' => $backend->getLocations(),
+      '#expanderSectionsConfig' => $config->getRawData(),
       '#attached' => [
         'drupalSettings' => [
           'activityFinder' => [
             'alias' => $alias,
             'is_search_box_disabled' => $config->get('disable_search_box'),
+            'locationsNidToDaxkoIdMapping' => $locationsMapping,
           ],
         ],
       ],
