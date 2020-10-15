b4a0fe655f7e5e746bc2bdd766f995a570f63225
NightHunterSV <nighthuntersv@gmail.com>
2020-10-15 14:14:45 +0300

    Use DI for the configs overrider service.

--- modules/openy_features/openy_prgf/src/Config/OpenyParagraphOverrides.php
+++ modules/openy_features/openy_prgf/src/Config/OpenyParagraphOverrides.php
@@ -5,6 +5,7 @@ namespace Drupal\openy_prgf\Config;
 use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Config\ConfigFactoryOverrideInterface;
 use Drupal\Core\Config\StorageInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
 
 /**
  * OpenY Paragraphs configuration override.
@@ -12,11 +13,28 @@ use Drupal\Core\Config\StorageInterface;
 class OpenyParagraphOverrides implements ConfigFactoryOverrideInterface {
 
   /**
+   * The module handler to invoke the alter hook.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  /**
+   * Creates the OpenyParagraphOverrides object.
+   *
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
+   *   The module handler.
+   */
+  public function __construct(ModuleHandlerInterface $module_handler) {
+    $this->moduleHandler = $module_handler;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function loadOverrides($names) {
     $overrides = [];
-    if (\Drupal::service('module_handler')->moduleExists('entity_clone')) {
+    if ($this->moduleHandler->moduleExists('entity_clone')) {
       $overrides['entity_clone.settings'] = [
         'form_settings' => [
           'paragraph' => [
