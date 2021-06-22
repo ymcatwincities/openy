d2bfa92f15442cc1bd274c191e641811134fa423
Andrii Podanenko <podarokua@gmail.com>
2021-06-22 13:50:57 +0300

    decouple: Remove openy_node_alert

--- modules/openy_features/openy_node/modules/openy_node_alert/js/openy-alerts/src/stores/store.prod.js
+++
@@ -1,8 +0,0 @@
-import { applyMiddleware, createStore } from 'redux';
-import thunk from 'redux-thunk';
-
-import reducer from '../reducers/index';
-
-const middleware = [thunk];
-
-export default createStore(reducer, {}, applyMiddleware(...middleware));
